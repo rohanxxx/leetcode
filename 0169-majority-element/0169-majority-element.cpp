456dd833fc96a606fdbf659808fc30224f6ee818
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-06-24 01:03:50 -0400

    Time: 23 ms (22.26%), Space: 27.9 MB (12.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,16 +6,11 @@ public:
         for(int i = 0; i < nums.size(); i++){
             mp[nums[i]]++;
             if(maxCount < mp[nums[i]]){
-                cout << "nums[" << i << "]: " << nums[i] << endl;
                 maxCount = mp[nums[i]];
                 majorityElement = nums[i];
             }
         }
-        cout << "majorityElement: " << majorityElement << endl;
         if(mp[majorityElement] > nums.size()/2) return majorityElement;
-        for(auto it = mp.begin(); it != mp.end(); it++){
-            cout << "it->first: " << it->first << ", " << "it->second: " << it->second << endl;
-        }
         return -1;
     }
 };
