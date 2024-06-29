216e2588e7a63b23f23300521a0ff655bee46f10
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-06-29 14:23:18 -0400

    Time: 75 ms (55.72%), Space: 100.6 MB (94.07%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -10,29 +10,17 @@ public:
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (grid[r][c] == 1) {
-                    if (r == 0) {
-                        up = 0;
-                    } else {
-                        up = grid[r-1][c];
-                    }
+                    if (r == 0) up = 0;
+                    else up = grid[r-1][c];
                     
-                    if (c == 0) {
-                        left = 0;
-                    } else {
-                        left = grid[r][c-1];
-                    }
+                    if (c == 0) left = 0;
+                    else left = grid[r][c-1];
                     
-                    if (r == rows-1) {
-                        down = 0;
-                    } else {
-                        down = grid[r+1][c];
-                    }
+                    if (r == rows-1) down = 0;
+                    else down = grid[r+1][c];
                     
-                    if (c == cols-1) {
-                        right = 0;
-                    } else {
-                        right = grid[r][c+1];
-                    }
+                    if (c == cols-1) right = 0;
+                    else right = grid[r][c+1];
                     
                     result += 4 - (up + left + right + down);
                 }
