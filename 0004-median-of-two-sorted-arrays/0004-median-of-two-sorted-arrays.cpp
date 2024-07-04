dd943ff05f398441c5a9cdbda01bcfb113cdc87d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-04 00:37:42 -0400

    Time: 33 ms (20.78%), Space: 96 MB (11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,11 +5,13 @@ public:
         for(auto& num2: nums2) nums.push_back(num2);
 
         sort(nums.begin(), nums.end());
+        
         //odd case
         if(nums.size()%2){
             return static_cast<double>(nums[nums.size()/2]);
         }
 
+        //even case
         return (static_cast<double>(nums[nums.size()/2])+nums[nums.size()/2-1])/2;
     }
 };
