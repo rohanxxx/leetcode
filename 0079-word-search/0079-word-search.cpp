d095ceca852a95889b2a34c4224330e2c22e061e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-07 13:21:54 -0400

    Time: 273 ms (72.1%), Space: 9.6 MB (80.82%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int ROWS;
-    int COLS;
+    int ROWS, COLS;
     vector<vector<char>> boardCopy;
 
     bool backtrack(int row, int col, const string& word, int index){
         if(index >= word.length()) return true;
         if(row < 0 || row == ROWS || col < 0 || col == COLS || boardCopy[row][col] != word[index]) return false;
-        bool res = false;
+        
         boardCopy[row][col] = '#';
-        int rowOffset[4] = {0, 1, 0, -1};
-        int colOffset[4] = {1, 0, -1, 0};
+
+        int rowOffset[4] = {0,1,0,-1};
+        int colOffset[4] = {1,0,-1,0};
+
+        bool res = false;
         for(int i = 0; i < 4; i++){
-            res = backtrack(row + rowOffset[i], col+colOffset[i], word, index+1);
-            if(res) break;
+            res = backtrack(row+rowOffset[i], col+colOffset[i], word, index+1);
+            if (res) break;
         }
         boardCopy[row][col] = word[index];
         return res;
@@ -22,11 +24,13 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         boardCopy = board;
         ROWS = board.size(), COLS = board[0].size();
+
         for(int row = 0; row < ROWS; row++){
-            for(int column = 0; column < COLS; column++){
-                if(backtrack(row, column, word, 0)) return true;
+            for(int col = 0; col < COLS; col++){
+                if(backtrack(row, col, word, 0)) return true;
             }
         }
+
         return false;
     }
 };
