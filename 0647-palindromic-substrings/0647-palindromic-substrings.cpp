8f4b30c06b6666c51751be7c01e2d039915b70ad
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-08 14:26:57 -0400

    Time: 524 ms (14.55%), Space: 7.4 MB (85.18%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,19 @@
 class Solution {
-    bool isPalindrome(const string& s, int lo, int hi) {
-        while (lo < hi) {
-            if (s[lo] != s[hi])
-                return false;
-
-            ++lo;
-            --hi;
+public:
+    bool isPalinedrom(int lo, int hi, const string& s){
+        while(lo <= hi){
+            if(s[lo] != s[hi]) return false;
+            lo++; hi--;
         }
-
         return true;
     }
-
- public:
     int countSubstrings(string s) {
-        int ans = 0;
-
-        for (int lo = 0; lo < s.size(); ++lo)
-            for (int hi = lo; hi < s.size(); ++hi)
-                ans += isPalindrome(s, lo, hi);
-
-        return ans;
+        int countPalinedrome = 0;
+        for(int lo = 0; lo < s.length(); lo++){
+            for(int hi = lo; hi < s.length(); hi++){
+                countPalinedrome += isPalinedrom(lo, hi, s);
+            }
+        }
+        return countPalinedrome;
     }
 };
