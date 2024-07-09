10edf48f8c8bad39f923834cdb1814dbe755cab0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-09 00:43:57 -0400

    Time: 12 ms (46.22%), Space: 16.5 MB (41.54%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -14,7 +14,9 @@ public:
         if(withoutDup.size()==1) return gain[withoutDup.back()];
         
         sort(withoutDup.begin(), withoutDup.end());
+       
         vector<int> dp(withoutDup.size(), 0);
+        
         dp[0] = gain[withoutDup[0]];
         if(withoutDup[0] == withoutDup[1]-1) dp[1] = max(dp[0], gain[withoutDup[1]]);
         else dp[1] = dp[0] + gain[withoutDup[1]];
