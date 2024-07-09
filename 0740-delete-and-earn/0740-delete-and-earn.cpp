8845a53b3a13cc5233144129939d4b50cc3d6c16
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-09 00:43:14 -0400

    Time: 4 ms (90.47%), Space: 16.5 MB (41.54%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -12,22 +12,15 @@ public:
         }
 
         if(withoutDup.size()==1) return gain[withoutDup.back()];
+        
         sort(withoutDup.begin(), withoutDup.end());
         vector<int> dp(withoutDup.size(), 0);
         dp[0] = gain[withoutDup[0]];
         if(withoutDup[0] == withoutDup[1]-1) dp[1] = max(dp[0], gain[withoutDup[1]]);
         else dp[1] = dp[0] + gain[withoutDup[1]];
 
-        cout << "withoutDup.size(): " << withoutDup.size() << endl;
-        for(auto &d: withoutDup){
-            cout << d << " ";
-        }
-        cout << endl;
         for(int i = 2; i < withoutDup.size(); i++){
             if(withoutDup[i-1] == withoutDup[i]-1){
-                cout << "line 24" << endl;
-                cout << "i: " << i << endl;
-                cout << "gain[withoutDup[i]]: " << gain[withoutDup[i]] << endl; 
                 dp[i] = max(dp[i-1], dp[i-2]+gain[withoutDup[i]]);
             }
             else{
