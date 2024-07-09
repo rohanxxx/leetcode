0427fc3d7ffa0ef604a0ddc5dc5fb18edce3fd10
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-09 00:42:30 -0400

    Time: 35 ms (7.36%), Space: 16.6 MB (41.54%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+
+        if (nums.size() == 1) return nums.back();
+        vector<int> withoutDup;
+        unordered_map<int, int> gain;
+
+        for(auto &num: nums){
+            if(!gain[num]) withoutDup.push_back(num);
+            gain[num] += num;
+        }
+
+        if(withoutDup.size()==1) return gain[withoutDup.back()];
+        sort(withoutDup.begin(), withoutDup.end());
+        vector<int> dp(withoutDup.size(), 0);
+        dp[0] = gain[withoutDup[0]];
+        if(withoutDup[0] == withoutDup[1]-1) dp[1] = max(dp[0], gain[withoutDup[1]]);
+        else dp[1] = dp[0] + gain[withoutDup[1]];
+
+        cout << "withoutDup.size(): " << withoutDup.size() << endl;
+        for(auto &d: withoutDup){
+            cout << d << " ";
+        }
+        cout << endl;
+        for(int i = 2; i < withoutDup.size(); i++){
+            if(withoutDup[i-1] == withoutDup[i]-1){
+                cout << "line 24" << endl;
+                cout << "i: " << i << endl;
+                cout << "gain[withoutDup[i]]: " << gain[withoutDup[i]] << endl; 
+                dp[i] = max(dp[i-1], dp[i-2]+gain[withoutDup[i]]);
+            }
+            else{
+                dp[i] = dp[i-1]+gain[withoutDup[i]];
+            }
+        }
+        return dp.back();
+    }
+};
