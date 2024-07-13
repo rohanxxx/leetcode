8ac62a3906437f9c7167735db28f2028c6a4a8e0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-13 18:12:57 -0400

    Time: 3 ms (43.81%), Space: 11.7 MB (14.79%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -10,11 +10,13 @@ public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         unordered_map<string, unordered_map<string, double>> graph;
 
+        string dividend, divisor;
+        double quotient;
         // Step 1: Build the graph from the equations
         for (int i = 0; i < equations.size(); i++) {
-            string dividend = equations[i][0];
-            string divisor = equations[i][1];
-            double quotient = values[i];
+            dividend = equations[i][0];
+            divisor = equations[i][1];
+            quotient = values[i];
 
             graph[dividend][divisor] = quotient;
             graph[divisor][dividend] = 1 / quotient;
@@ -22,13 +24,11 @@ public:
 
         vector<double> results;
         for (int i = 0; i < queries.size(); i++) {
-            string dividend = queries[i][0];
-            string divisor = queries[i][1];
+            dividend = queries[i][0];
+            divisor = queries[i][1];
 
-            if (graph.find(dividend) == graph.end() || graph.find(divisor) == graph.end())
-                results.push_back(-1.0);
-            else if (dividend == divisor)
-                results.push_back(1.0);
+            if (graph.find(dividend) == graph.end() || graph.find(divisor) == graph.end()) results.push_back(-1.0);
+            else if (dividend == divisor) results.push_back(1.0);
             else {
                 unordered_set<string> visited;
                 results.push_back(backtrackEvaluate(graph, dividend, divisor, 1.0, visited));
@@ -45,16 +45,16 @@ private:
         double ret = -1.0;
 
         auto neighbors = graph[currNode];
-        if (neighbors.find(targetNode) != neighbors.end()) {
-            ret = accProduct * neighbors[targetNode];
-        } else {
+        if (neighbors.find(targetNode) != neighbors.end()) ret = accProduct * neighbors[targetNode];
+        else {
             for (auto& pair : neighbors) {
                 string nextNode = pair.first;
-                if (visited.find(nextNode) != visited.end())
-                    continue;
+                
+                if (visited.find(nextNode) != visited.end()) continue;
+                
                 ret = backtrackEvaluate(graph, nextNode, targetNode, accProduct * pair.second, visited);
-                if (ret != -1.0)
-                    break;
+                
+                if (ret != -1.0) break;
             }
         }
 
