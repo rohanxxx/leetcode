e6b9a740a277a4b64aa045cbee89fb3699c25c83
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-13 21:36:03 -0400

    Time: 0 ms (100%), Space: 17 MB (5.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -6,27 +6,18 @@ public:
         first = stack.top(); stack.pop();
 
         int new_int = 0;
-        if(sign == "+"){
-            new_int = first+second;
-        }
-        if(sign == "-"){
-            new_int = first-second;
-        }
-        if(sign == "*"){
-            new_int = first*second;
-        }
-        if(sign == "/"){
-            new_int = first/second;
-        }
+        if(sign == "+") new_int = first+second;
+        if(sign == "-") new_int = first-second;
+        if(sign == "*") new_int = first*second;
+        if(sign == "/") new_int = first/second;
+        
         return new_int;
     }
     int evalRPN(vector<string>& tokens) {
         if(tokens.size() < 2) return stoi(tokens[0]);
         stack<int> stack;
-        int ans = 0;
         for(auto& token: tokens){  
             if(token == "+" || token == "-" || token == "*" || token == "/"){
-                // ans = calculate(stack, token);
                 stack.push(calculate(stack, token));
                 continue;
             }
