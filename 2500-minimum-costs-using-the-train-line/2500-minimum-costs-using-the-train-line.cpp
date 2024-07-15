f0f922e9dee5a0e6bc05019f0f16fab30022ea54
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-15 15:37:05 -0400

    Time: 170 ms (16.24%), Space: 119.1 MB (30.77%) - LeetHub

--- 2500-minimum-costs-using-the-train-line/2500-minimum-costs-using-the-train-line.cpp
+++ 2500-minimum-costs-using-the-train-line/2500-minimum-costs-using-the-train-line.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<long long> minimumCosts(vector<int>& regular, vector<int>& express, int expressCost) {
-        int size = regular.size()+1;
+        int size = regular.size();
         
         long prevRegularLaneCost = 0;
         long prevExpressLaneCost = expressCost;
         vector<long long> minCost;
 
-        for(int i = 1; i < size; i++){
-            long regLaneCost = regular[i-1]+min(prevRegularLaneCost, prevExpressLaneCost);
-            long regExpressLaneCost = express[i-1]+min(expressCost+prevRegularLaneCost, prevExpressLaneCost);
+        for(int i = 0; i < size; i++){
+            long regLaneCost = regular[i]+min(prevRegularLaneCost, prevExpressLaneCost);
+            long regExpressLaneCost = express[i]+min(expressCost+prevRegularLaneCost, prevExpressLaneCost);
             
             minCost.push_back(min(regLaneCost, regExpressLaneCost));
             
