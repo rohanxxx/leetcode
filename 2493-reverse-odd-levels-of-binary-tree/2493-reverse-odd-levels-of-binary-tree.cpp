0e7f4ce403ab765a6b735e281f82e5f89a7b0b45
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-18 17:37:49 -0400

    Time: 161 ms (32.42%), Space: 77.8 MB (16.21%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,35 +1,36 @@
-//Please upvote if it helps :)
-class Solution 
-{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) 
-    {
+    TreeNode* reverseOddLevels(TreeNode* root) {
         if(root == NULL) return root;
-        
-        queue<TreeNode*> q; q.push(root);
-        
+        queue<TreeNode*>q; q.push(root);
         vector<int> nodeValues;
-        
-        int level=0;
-        
-        while(!q.empty())
-        {
+        int level = 0;
+        while(!q.empty()){
             int qSize = q.size();
             vector<int> temp;
             for(int i = 0; i < qSize; i++){
-                TreeNode* node = q.front(); q.pop();
-                
+                TreeNode* node = q.front();
+                q.pop();
                 if(level%2) node->val = nodeValues[qSize-i-1];
-                
                 if(node->left){
                     q.push(node->left);
                     temp.push_back(node->left->val);
                 }
-                if(node->right) {
+                if(node->right){
                     q.push(node->right);
                     temp.push_back(node->right->val);
                 }
-                
             }
             level++;
             nodeValues = temp;
