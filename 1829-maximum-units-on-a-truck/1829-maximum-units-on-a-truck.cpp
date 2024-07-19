cb7a8c9b3f52a0fc11a1b4895d1729a878fa728e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-18 23:56:44 -0400

    Time: 31 ms (41.3%), Space: 19.9 MB (48.53%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
@@ -6,17 +6,17 @@ public:
         }
 
         sort(boxTypes.begin(), boxTypes.end());
-        reverse(boxTypes.begin(), boxTypes.end());
+        // reverse(boxTypes.begin(), boxTypes.end());
         
-        for(auto& box: boxTypes){
-            cout << "[" << box[0] << "," << box[1] << "] ";
-        }
-        cout << endl;
+        // for(auto& box: boxTypes){
+            // cout << "[" << box[0] << "," << box[1] << "] ";
+        // }
+        // cout << endl;
         
         int totalUnit = 0;
-        for(auto& box: boxTypes){
-            int numBoxes = box[1];
-            int unit = box[0];
+        for(int i = boxTypes.size()-1; i >= 0; i--){
+            int numBoxes = boxTypes[i][1];
+            int unit = boxTypes[i][0];
             if(numBoxes <= truckSize){
                 totalUnit += (numBoxes*unit);
                 truckSize -= numBoxes;
