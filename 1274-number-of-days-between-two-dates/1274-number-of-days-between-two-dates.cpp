ea03ebeb8087cf419ae1bb84d7d97a2f3c0977ba
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-23 17:42:25 -0400

    Time: 0 ms (100%), Space: 7.8 MB (13.46%) - LeetHub

--- 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.cpp
+++ 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    bool leapYear(int y){
-        if(y % 4 == 0 && y%100 != 0) return true;
-        if (y%400 == 0) return true;
-        return false; 
+    bool leapYear(int year){
+        if(year%4 == 0 && year%100 != 0) return true;
+        if(year%400 == 0) return true;
+        return false;
     }
     int daysInMonth(int m, int y){
         if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) return 31;
         if(m == 2){
             if(leapYear(y)) return 29;
-            else return 28;
+            return 28;
         }
-        
         return 30;
-    
     }
-    int date_to_int(string date){
-        int y = stoi(date.substr(0,4));
-        int m = stoi(date.substr(5,2));
-        int d = stoi(date.substr(8,2));
-        
-        int dayCount = 0;
+    int dateToInt(string s){
+        int year = stoi(s.substr(0,4));
+        int month = stoi(s.substr(5,2));
+        int day = stoi(s.substr(8,2));
 
-        for(int i = 1971; i < y; i++){
-            if(leapYear(i)) dayCount += 366;
+        int dayCount = 0;
+        for(int y = 1971; y < year; y++){
+            if(leapYear(y)) dayCount += 366;
             else dayCount += 365;
         }
-
-        for(int i = 1; i < m; i++){
-            dayCount += daysInMonth(i, y);
+        for(int m = 0; m < month; m++){
+            dayCount += daysInMonth(m, year);
         }
 
-        return dayCount+d;
+        dayCount += day;
+        return dayCount;
     }
     int daysBetweenDates(string date1, string date2) {
-        return abs(date_to_int(date2) - date_to_int(date1));
+        return abs(dateToInt(date1) - dateToInt(date2)); 
     }
 };
