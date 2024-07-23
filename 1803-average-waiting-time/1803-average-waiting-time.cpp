ee1c5439c4abeb7e915e84774c72fe73bf522fe7
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-23 14:59:43 -0400

    Time: 160 ms (31.31%), Space: 76.7 MB (31.22%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double prefixSum = customers[0][0]+customers[0][1];
-        cout << "prefixSum: " << prefixSum << endl;
         double waitTime = customers[0][1];
-        cout << "waitTime: " << waitTime << endl;
+
         for(int i = 1; i < customers.size(); i++){
             // prefixSum += customers[i][1];
             prefixSum = max(double(customers[i][0]), prefixSum)+customers[i][1];
-            cout << "prefixSum: " << prefixSum << endl;
             waitTime += (prefixSum-customers[i][0]);
-            cout << "waitTime: " << waitTime << endl;
         }
-        cout << "waitTime: " << waitTime << endl;
         double avgTime = waitTime/customers.size();
         return avgTime;
     }
