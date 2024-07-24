3b3681d8f89288233f7bb1a2189456740614a73b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-24 15:06:02 -0400

    Time: 85 ms (53.61%), Space: 43.9 MB (60.7%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,43 +1,40 @@
 class Solution {
-public:    
+public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int, int>> adj[101];
-        // Build the adjacency list
-        for (vector<int> time : times) {
-            int source = time[0];
-            int dest = time[1];
-            int travelTime = time[2];
-            adj[source].push_back({travelTime, dest});
+        //build the graph here
+        for(auto& t: times){
+            int source = t[0];
+            int dest = t[1];
+            int time = t[2];
+            adj[source].push_back({time, dest});
         }
 
-        vector<int> signalReceivedAt(n + 1, INT_MAX);
-        // BFS(signalReceivedAt, k);
+        vector<int> signalAt(n+1, INT_MAX);
+
         queue<int> q; q.push(k);
-        
-        signalReceivedAt[k] = 0;
-        
-        while (!q.empty()) {
-            int currNode = q.front(); 
-            q.pop();
-            
-            // Broadcast the signal to adjacent nodes
-            for (pair<int, int> edge : adj[currNode]) {
+
+        signalAt[k] = 0;
+
+        while(!q.empty()){
+            int curNode = q.front(); q.pop();
+            for(pair<int, int>edge: adj[curNode]){
                 int time = edge.first, neighborNode = edge.second;
-                int arrivalTime = signalReceivedAt[currNode] + time;
-                if (signalReceivedAt[neighborNode] > arrivalTime) {
-                    signalReceivedAt[neighborNode] = arrivalTime;
+                int arrivalTime = signalAt[curNode] + time;
+                if(arrivalTime < signalAt[neighborNode]){
+                    signalAt[neighborNode] = arrivalTime;
                     q.push(neighborNode);
-                }
+                } 
             }
         }
-        
+
         int answer = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            answer = max(answer, signalReceivedAt[i]);
+        for(int i = 1; i <= n; i++){
+            answer = max(answer, signalAt[i]);
         }
-        
-        // INT_MAX signifies atleat one node is unreachable
+
         if(answer == INT_MAX) return -1;
+
         return answer;
     }
 };
