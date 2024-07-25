853f5d0cb21446dad268293475f471123cd1d776
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-24 22:31:18 -0400

    Time: 0 ms (100%), Space: 14 MB (19.87%) - LeetHub

--- 1682-most-visited-sector-in-a-circular-track/1682-most-visited-sector-in-a-circular-track.cpp
+++ 1682-most-visited-sector-in-a-circular-track/1682-most-visited-sector-in-a-circular-track.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> frequency(n+1, 0);
         int maxVisited = 0; 
         for(int i = 0; i < rounds.size()-1; i++){
-            // frequency[rounds[i]]++;
             maxVisited = max(maxVisited, ++frequency[rounds[i]]);
             if(rounds[i+1]-rounds[i] > 1){
                 for(int j = rounds[i]+1; j < rounds[i+1]; j++) maxVisited = max(maxVisited,++frequency[j]);
@@ -17,10 +16,7 @@ public:
             }
         }
 
-        // frequency[rounds.back()]++;
         maxVisited = max(maxVisited,++frequency[rounds.back()]);
-        // int maxVisited = 0; 
-        // for(auto& f: frequency) maxVisited = max(f, maxVisited);
 
         vector<int> ans;
         for(int i = 1; i < frequency.size(); i++){
