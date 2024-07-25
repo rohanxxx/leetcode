15fec19ee4f63aa49e36b5b9fa20044abef37d56
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-24 22:30:55 -0400

    Time: 8 ms (25.83%), Space: 14.1 MB (19.87%) - LeetHub

--- 1682-most-visited-sector-in-a-circular-track/1682-most-visited-sector-in-a-circular-track.cpp
+++ 1682-most-visited-sector-in-a-circular-track/1682-most-visited-sector-in-a-circular-track.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     vector<int> mostVisited(int n, vector<int>& rounds) {
         vector<int> frequency(n+1, 0);
+        int maxVisited = 0; 
         for(int i = 0; i < rounds.size()-1; i++){
-            frequency[rounds[i]]++;
+            // frequency[rounds[i]]++;
+            maxVisited = max(maxVisited, ++frequency[rounds[i]]);
             if(rounds[i+1]-rounds[i] > 1){
-                for(int j = rounds[i]+1; j < rounds[i+1]; j++) frequency[j]++;
+                for(int j = rounds[i]+1; j < rounds[i+1]; j++) maxVisited = max(maxVisited,++frequency[j]);
             }
             else{
                 if(rounds[i+1]-rounds[i] <= rounds[i]){
-                    for(int j = rounds[i]+1; j <= n; j++) frequency[j]++;
-                    for(int j = 1; j < rounds[i+1]; j++) frequency[j]++;
+                    for(int j = rounds[i]+1; j <= n; j++) maxVisited = max(maxVisited,++frequency[j]);
+                    for(int j = 1; j < rounds[i+1]; j++) maxVisited = max(maxVisited,++frequency[j]);
                 }
             }
         }
 
-        frequency[rounds.back()]++;
-        
-        int maxVisited = 0; 
-        for(auto& f: frequency) maxVisited = max(f, maxVisited);
+        // frequency[rounds.back()]++;
+        maxVisited = max(maxVisited,++frequency[rounds.back()]);
+        // int maxVisited = 0; 
+        // for(auto& f: frequency) maxVisited = max(f, maxVisited);
 
         vector<int> ans;
         for(int i = 1; i < frequency.size(); i++){
