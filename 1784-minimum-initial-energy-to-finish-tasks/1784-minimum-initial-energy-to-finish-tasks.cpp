c2a5ab79708664a4f6931b80015abff27bd5082b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-25 21:51:31 -0400

    Time: 212 ms (94.51%), Space: 97.1 MB (48.72%) - LeetHub

--- 1784-minimum-initial-energy-to-finish-tasks/1784-minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/1784-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool static compare(vector<int>& a, vector<int>& b){
-        return (a[1]-a[0]) > (b[1]-b[0]);
+        return(a[1] - a[0]) > (b[1] - b[0]);
     }
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(), tasks.end(), compare);
-
-        int energyLeft = 0, ans = 0;
+        int ans = 0;
+        int energyLeft = 0, answer = 0;
         for(auto& t: tasks){
-            int actual = t[0];
-            int minimum = t[1];
-
+            int actual = t[0], minimum = t[1];
             if(energyLeft < minimum){
-                ans+= minimum-energyLeft;
+                ans += (minimum - energyLeft);
                 energyLeft = minimum - actual;
                 continue;
             }
-
             energyLeft -= actual;
         }
+
         return ans;
     }
 };
