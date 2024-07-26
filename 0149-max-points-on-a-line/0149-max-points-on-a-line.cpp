303dd0b2b0a0454a06c80fc2a7657dc27a5cd74f
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-26 12:17:58 -0400

    Time: 38 ms (48.17%), Space: 24.3 MB (14.34%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -10,12 +10,12 @@ public:
             unordered_map<double, int> mp;
             for(int j = 0; j < points.size(); j++){
                 if(j != i){
-                    mp[atan2(points[j][1]-points[i][1], points[j][0]-points[i][0])]++;
+                    result = max(result, ++mp[atan2(points[j][1]-points[i][1], points[j][0]-points[i][0])]+1);
                 }
             }
-            for(auto& [h, count]: mp){
+            /*for(auto& [h, count]: mp){
                 result = max(result, count+1);
-            }
+            }*/
         }
 
         return result;
