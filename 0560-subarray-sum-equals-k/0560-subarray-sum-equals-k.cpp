37f9521418e3444cbc711d2c81c8340c29add96c
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-29 21:34:25 -0400

    Time: 63 ms (75.47%), Space: 45.5 MB (48.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> umap;
-        int prefix_sum = 0, total_sub_arr = 0, diff = 0;
-        umap[prefix_sum]++;
-        
+        unordered_map<int, int> mp;
+
+        int prefixSum = 0, diff = INT_MIN, subArrCount = 0;
+
+        mp[prefixSum] = 1;
+
         for(int i = 0; i < nums.size(); i++){
-            prefix_sum += nums[i];
-            diff = prefix_sum - k;
-            cout << "prefix_sum: " << prefix_sum << endl;
-            cout << "diff: " << diff << endl;
-            
-            if(umap.find(diff) != umap.end()){
-                total_sub_arr += umap[diff];
-                cout << "total_sub_arr: " << total_sub_arr << endl;
+            prefixSum += nums[i];
+            diff = prefixSum - k;
+            if(mp.find(diff) != mp.end()){
+                subArrCount += mp[diff];
             }
-            
-            umap[prefix_sum]++;
+            mp[prefixSum]++;
         }
-        
-        return total_sub_arr;
-        
+
+        return subArrCount;
     }
 };
