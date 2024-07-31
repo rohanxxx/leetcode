22021aa4c6a0425759e8a0dedafdaa07ebf29ca7
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-07-30 23:26:06 -0400

    Time: 46 ms (35.36%), Space: 35.6 MB (7.33%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -9,6 +9,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
             int remainder = sum % k;
+            if(remainder < 0) remainder += k;
             if(mp.find(remainder) != mp.end()) count += mp[remainder];
             mp[remainder]++;
         }
