09b13831325c2864f809746f7b8b2bf41cb91b3d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-08-07 01:08:27 -0400

    Time: 138 ms (56.37%), Space: 105.9 MB (16.69%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -29,9 +29,6 @@ public:
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            cout << "levelSum: " << levelSum << endl;
-            // maxLevelSum = max(levelSum, maxLevelSum);
-            // mp[levelSum] = levelCount;
 
             if(maxLevelSum < levelSum){
                 maxLevelSum = levelSum;
