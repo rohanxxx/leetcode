af5301548db1c3e94471aa67af6dbe3566817220
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-08-13 23:58:57 -0400

    Time: 22 ms (12.97%), Space: 17.7 MB (27.29%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> tree(n);
+        //bulding the graph
+        for(auto &edge: edges){
+            tree[edge[0]].push_back(edge[1]);
+            tree[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_map<int, int> parent;
+        parent[0] = -1;
+
+        stack<int> stack;
+        stack.push(0);
+
+        while(!stack.empty()){
+            int node = stack.top(); stack.pop();
+            for(auto nextNode: tree[node]){
+                //detects the parentNode
+                if(parent[node] == nextNode) continue;
+                //detects the cycle
+                if(parent.find(nextNode) != parent.end()) return false;
+
+                //dfs
+                stack.push(nextNode);
+                parent[nextNode] = node;
+            }
+        }
+
+        return parent.size() == n;
+    }
+};
