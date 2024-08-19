eb06e52ea047464e589f247db45cfd9f69d28302
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-08-19 15:46:10 -0400

    Time: 63 ms (10.51%), Space: 33 MB (29.6%) - LeetHub

--- 1101-parallel-courses/1101-parallel-courses.cpp
+++ 1101-parallel-courses/1101-parallel-courses.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     int dfs(int node, vector<vector<int>>& graph, vector<int>& visited){
         if(visited[node] != 0) return visited[node];
-        else visited[node] = -1;
+        else {
+            cout << "line 6 node: " << node << endl;
+            visited[node] = -1;
+        }
 
         int maxLength = 1;
         for(auto nextNode: graph[node]){
             int length = dfs(nextNode, graph, visited);
-            if(length == -1) return -1;
+            if(length == -1) {
+                cout << "line 14 node: " << node << endl;
+                return -1;
+            }
             maxLength = max(maxLength, length+1);
         }
 
@@ -27,7 +33,10 @@ public:
         int maxLength = 1;
         for(int i = 1; i < n+1; i++){
             int length = dfs(i, graph, visited);
-            if(length == -1) return -1;
+            if(length == -1) {
+                cout << "line 37 node: " << i << endl;
+                return -1;
+            }
             maxLength = max(maxLength, length);
         }
 
