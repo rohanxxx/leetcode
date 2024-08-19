bdcdd2b2e4de46e6117e5e5971f0b64b2c9519d7
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-08-19 15:12:15 -0400

    Time: 56 ms (24.43%), Space: 32.8 MB (32.51%) - LeetHub

---
+++ 1101-parallel-courses/1101-parallel-courses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dfs(int node, vector<vector<int>>& graph, vector<int>& visited){
+        if(visited[node] != 0) return visited[node];
+        else visited[node] = -1;
+
+        int maxLength = 1;
+        for(auto nextNode: graph[node]){
+            int length = dfs(nextNode, graph, visited);
+            if(length == -1) return -1;
+            maxLength = max(maxLength, length+1);
+        }
+
+        visited[node] = maxLength;
+        return maxLength;
+    }
+
+    int minimumSemesters(int n, vector<vector<int>>& relations) {
+        vector<vector<int>> graph(n+1);
+        //build the graph
+        for(auto relation: relations){
+            graph[relation[0]].push_back(relation[1]);
+        }
+
+        vector<int> visited(n+1);
+
+        int maxLength = 1;
+        for(int i = 1; i < n+1; i++){
+            int length = dfs(i, graph, visited);
+            if(length == -1) return -1;
+            maxLength = max(maxLength, length);
+        }
+
+        return maxLength;
+    }
+};
