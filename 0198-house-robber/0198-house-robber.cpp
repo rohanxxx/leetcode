f834dcca0539b83670e67056382af5ceb66d83a2
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-02 18:14:22 -0400

    Time: 0 ms (100%), Space: 9.5 MB (56.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,13 +10,13 @@ public:
         sum[0] = nums[0];
         sum[1] = max(nums[1], nums[0]);
         
-        int maxSum = max(sum[0], sum[1]);
+        int tempMaxSum = max(sum[0], sum[1]);
 
         for(int i = 2; i < n; i++){
-            sum[i] = max(nums[i]+sum[i-2], maxSum);
-            maxSum = sum[i];
+            sum[i] = max(nums[i]+sum[i-2], tempMaxSum);
+            tempMaxSum = sum[i];
         }
 
-        return maxSum;
+        return sum[n-1];
     }
 };
