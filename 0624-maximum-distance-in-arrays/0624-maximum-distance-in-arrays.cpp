7a4e04ba62a4b3fab2529d2893914f04fc0b0ccc
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-03 20:55:43 -0400

    Time: 225 ms (65.76%), Space: 107.8 MB (72.38%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i < arrays.size(); i++){
             if(i == 0){
                 if(arrays[i].size() > 0){
-                    minSoFar = arrays[i][0];
+                    minSoFar = arrays[i].front();
                     maxSoFar = arrays[i].back();
                 }
                 continue;
@@ -14,7 +14,7 @@ public:
 
             int temp1, temp2;
             if(arrays[i].size() > 0) {
-                temp1 = arrays[i][0];
+                temp1 = arrays[i].front();
                 maxDistance = max(maxDistance, abs(maxSoFar - temp1));
                 temp2 = arrays[i].back();
                 maxDistance = max(maxDistance, abs(temp2 - minSoFar)); 
