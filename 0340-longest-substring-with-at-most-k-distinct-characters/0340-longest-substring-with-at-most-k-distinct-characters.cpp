22da9e07ad488ff462c52ab5abc9bc72d827e223
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-04 22:15:17 -0400

    Time: 12 ms (54.36%), Space: 12.8 MB (7.94%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int strSize = s.size();
+        if(strSize < k+1) return strSize;
+
+        map <char, int> hashmap;
+
+        int left = 0, right = 0;
+        int longest_len = 0;
+        while(right < strSize){
+            hashmap[s[right]] = right;
+            right++;
+            
+            if(hashmap.size() == k+1){
+                int del_index = INT_MAX;
+                for(pair<char,int> element: hashmap){
+                    del_index = min(del_index, element.second);
+                }
+                hashmap.erase(s[del_index]);
+                left = del_index+1;
+            }
+
+            longest_len = max(longest_len, right-left);
+            // cout << "s[right]: " << s[right] << endl;
+        }
+        return longest_len;
+    }
+};
