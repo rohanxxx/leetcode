9cc6590519c73b40ebf141a8b775638a3f374a0c
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-09 00:25:58 -0400

    Time: 66 ms (5.38%), Space: 26.3 MB (5.38%) - LeetHub

---
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findLonelyPixel(vector<vector<char>>& picture) {
+
+        int n = picture.size();
+        int m = picture[0].size();
+
+        unordered_map<int, unordered_set<int>> row;
+        unordered_map<int, unordered_set<int>> column;
+
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < m; c++){
+                if(picture[r][c] == 'B'){
+                    row[r].insert(c);
+                    column[c].insert(r);
+                }
+            }
+        }
+
+        int lonelyPixel = 0;
+        for(int r = 0; r < n; r++){
+            // cout << "row[r].size(): " << row[r].size() << endl;
+            for(int c = 0; c < m; c++){
+                if(picture[r][c] == 'B'){
+                    if(row[r].size() == 1 && column[c].size() == 1) lonelyPixel++;
+                }
+            }
+        }
+
+        return lonelyPixel;
+    }
+};
