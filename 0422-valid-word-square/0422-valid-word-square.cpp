4938fb1219f35b74897b6bd281c2074a7e27c527
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-08 23:15:13 -0400

    Time: 7 ms (76.16%), Space: 12.8 MB (8.14%) - LeetHub

---
+++ 0422-valid-word-square/0422-valid-word-square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int cols = 0;
+        int rows = words.size();
+        vector<string> newWords;
+        
+        for (auto& word : words) {
+            cols = max(cols, (int)word.size());
+        }
+
+        if (cols != words[0].size() || rows != cols) {
+            return false;
+        }
+
+        for (int col = 0; col < cols; ++col) {
+            string newWord;
+            for (int row = 0; row < rows; ++row) {
+                if (col < words[row].size()) {
+                    newWord += words[row][col];
+                }
+            }
+            newWords.push_back(newWord);
+        }
+        return words == newWords;
+    }
+};
