a2c4a7e714c80a0de20088b926ca37407afbf699
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-12 21:51:04 -0400

    Time: 6 ms (9.52%), Space: 11.6 MB (94.64%) - LeetHub

---
+++ 0369-plus-one-linked-list/0369-plus-one-linked-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* plusOne(ListNode* head) {
+        // sentinel head
+        ListNode* sentinel = new ListNode(0);
+        sentinel->next = head;
+        ListNode* notNine = sentinel;
+
+        // find the rightmost not-nine digit
+        while (head != nullptr) {
+            if (head->val != 9) notNine = head;
+            head = head->next;
+        }
+        // increase this rightmost not-nine digit by 1
+        notNine->val++;
+        notNine = notNine->next;
+        // set all the following nines to zeros
+        while (notNine != nullptr) {
+            notNine->val = 0;
+            notNine = notNine->next;
+        }
+
+        delete notNine;
+        
+        if (sentinel->val != 0) {
+            return sentinel;
+        } else {
+            return sentinel->next;
+        }
+    }
+};
