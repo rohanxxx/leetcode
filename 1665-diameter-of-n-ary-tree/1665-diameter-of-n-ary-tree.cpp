b9b97e148a05b0ae39f4ae190e1801bc3e3ca3a0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-15 22:41:39 -0400

    Time: 12 ms (74.46%), Space: 15 MB (17.93%) - LeetHub

--- 1665-diameter-of-n-ary-tree/1665-diameter-of-n-ary-tree.cpp
+++ 1665-diameter-of-n-ary-tree/1665-diameter-of-n-ary-tree.cpp
@@ -43,13 +43,13 @@ public:
             return secondHigh+1;
         }
 
-        if(pq.size() == 1){
+        // if(pq.size() == 1){
             int high = pq.top(); pq.pop();
             maxDiameter = max(maxDiameter, high);
             return high+1;
-        }
+        // }
 
-        return 0;
+        // return 0;
     }
 
     int diameter(Node* root) {
