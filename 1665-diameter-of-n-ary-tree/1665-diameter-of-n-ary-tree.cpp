28b733bc2ad9bd77ed1ec7190be072207526b009
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-09-15 22:39:26 -0400

    Time: 14 ms (52.72%), Space: 15.1 MB (13.59%) - LeetHub

---
+++ 1665-diameter-of-n-ary-tree/1665-diameter-of-n-ary-tree.cpp
@@ -0,0 +1,68 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDiameter;
+    int dfs(Node* root){
+        // cout << "root->val: " << root->val << endl;
+        if(root->children.empty()) return 1;
+        
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        
+        for(auto& child: root->children){
+            // cout << "child->val: " << child->val << endl;
+            int dept = dfs(child);
+            // cout << "dept: " << dept << endl;
+            pq.push(dept);
+            if(pq.size()>2){
+                pq.pop();
+            }
+        }
+        // cout << "pq.size(): " << pq.size() << endl;
+        /*while (!pq.empty()) {
+            std::cout << pq.top() << " "; // Prints the smallest element
+            pq.pop();
+        }*/
+    cout << endl;
+        if(pq.size() == 2){
+            int firstHigh = pq.top(); pq.pop();
+            int secondHigh = pq.top(); pq.pop();
+            // cout << "firstHigh: " << firstHigh << endl;
+            // cout << "secondHigh: " << secondHigh << endl;
+            maxDiameter = max(maxDiameter, firstHigh+secondHigh);
+            return secondHigh+1;
+        }
+
+        if(pq.size() == 1){
+            int high = pq.top(); pq.pop();
+            maxDiameter = max(maxDiameter, high);
+            return high+1;
+        }
+
+        return 0;
+    }
+
+    int diameter(Node* root) {
+        maxDiameter = 0;
+        dfs(root);
+        return maxDiameter;
+    }
+};
