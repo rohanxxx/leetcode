8848739bac183f2973c45f6fe6c78c851d594f45
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-10-10 16:05:15 -0400

    Time: 387 ms (10.28%), Space: 0B (100%) - LeetHub

---
+++ 3287-loan-types/3287-loan-types.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT DISTINCT l1.user_id
+FROM Loans l1
+JOIN Loans l2
+ON l1.user_id = l2.user_id
+WHERE l1.loan_type = 'Refinance'
+AND l2.loan_type = 'Mortgage'
+ORDER BY l1.user_id ASC;
