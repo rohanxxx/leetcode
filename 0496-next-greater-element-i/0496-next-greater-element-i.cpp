900a5fdeb6c6934ef239669f2d966100f688b46e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-10-14 15:09:41 -0400

    Time: 0 ms (100%), Space: 12.4 MB (43.68%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        
+        for (int i = 0; i < nums2.size(); i++){
+            mp[nums2[i]] = i;
+        }
+        
+        for(auto n: nums1){
+            int startIndex = mp[n];
+            bool ifBreak = false;
+            for(int i = startIndex; i < nums2.size(); i++){
+                if(n < nums2[i]){
+                    // cout << "n: " << n << endl;
+                    // cout << "nums2[i]: " << nums2[i] << endl; 
+                    ans.push_back(nums2[i]);
+                    ifBreak = true;
+                    break;
+                }
+            }
+            if(ifBreak == false){
+                ans.push_back(-1);
+            }
+        }
+
+        return ans;
+    }
+};
