af4895f2cc136aed247263b25b94fce6e0f79c6d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-10-21 00:51:12 -0400

    Time: 2 ms (98.6%), Space: 16.2 MB (40.12%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    struct compare{
-        bool operator() (const int &a, const int &b){
-            return a > b;
-        }
-    };
-    
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        
-        //sorts the intervals
         sort(intervals.begin(), intervals.end());
         
-        //create the custom class minQue
-        priority_queue<int, vector<int>, compare> minQue;
+        priority_queue<int, vector<int>, greater<int>> minQue;
         
-        for(int i = 0; i < intervals.size(); i++){
-            if(!minQue.empty()){ 
-                if(minQue.top() <= intervals[i][0]){
-                    minQue.pop();
-                }
+        for(auto interval: intervals){
+            if(!minQue.empty()){
+                if(minQue.top() <= interval[0]) minQue.pop();
             }
-            minQue.push(intervals[i][1]);
-        }
-        /*
-        for(int i = 0; i < minQue.size(); i++){
-            cout << minQue.top() << " ";
-            minQue.pop();
+            minQue.push(interval[1]);
         }
 
-        cout << minQue.top() << " ";
-
-        cout << endl;
-        */
         return minQue.size();
     }
 };
