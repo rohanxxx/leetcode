254b3a90c5dac57ddafed80c68d523adcd858b12
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-10-23 19:38:42 -0400

    Time: 210 ms (28.07%), Space: 53.6 MB (13.04%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -1,50 +1,47 @@
 class Solution {
 public:
-    int maximumDetonation(vector<vector<int>>& bombs) {
-        unordered_map<int, vector<int>> graph;
-        int n = bombs.size();
+    int dfs(int i, unordered_map<int, vector<int>>& graph){
+        unordered_set<int> visited;
+        stack<int> stack;
+        stack.push(i);
+        visited.insert(i);
         
-        // Build the graph
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
+        while(!stack.empty()){
+            int cur = stack.top();
+            stack.pop();
+            for(int n: graph[cur]){
+                if(visited.find(n) == visited.end()){
+                    visited.insert(n);
+                    stack.push(n);
                 }
+            }
+        }
+
+        return visited.size();
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        unordered_map<int, vector<int>> graph;
+        
+        // builds the graph
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j == i) continue;
                 int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
                 int xj = bombs[j][0], yj = bombs[j][1];
 
-                // Create a path from node i to node j, if bomb i detonates bomb j.
-                if ((long long)ri * ri >= (long long)(xi - xj) * (xi - xj) + (long long)(yi - yj) * (yi - yj)) {
+                if((long long) ri*ri >= (long long) (xi-xj) * (xi-xj) + (long long)(yi-yj)*(yi-yj)){
                     graph[i].push_back(j);
                 }
+
             }
         }
-        
+
         int answer = 0;
-        for (int i = 0; i < n; i++) {
-            int x = dfs(i, graph);
-            answer = max(answer, x);
+        for(int i = 0; i < n; i++){
+            answer = max(answer, dfs(i, graph));
         }
-        
+
         return answer;
-    }
-    
-private:
-    int dfs(int i, unordered_map<int, vector<int>>& graph) {
-        stack<int> stk;
-        unordered_set<int> visited;
-        stk.push(i);
-        visited.insert(i);
-        while (!stk.empty()) {
-            int cur = stk.top();
-            stk.pop();
-            for (int neib : graph[cur]) {
-                if (visited.find(neib) == visited.end()) {
-                    visited.insert(neib);
-                    stk.push(neib);
-                }
-            }
-        }
-        return visited.size();
     }
 };
