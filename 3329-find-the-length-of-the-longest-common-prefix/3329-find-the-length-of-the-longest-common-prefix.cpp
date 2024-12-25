46e092d0facb1a9f3e379772b37c59f9e7480835
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-12-25 16:28:54 -0500

    Time: 160 ms (67.14%), Space: 135.5 MB (72.07%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> arr1Prefixes;
+        unordered_set<int> prefixes;
         for(int val: arr1){
-            while(!arr1Prefixes.count(val) && val > 0){
-                arr1Prefixes.insert(val);
+            while(!prefixes.count(val) && val > 0){
+                prefixes.insert(val);
                 val /= 10;
             }
         }
-        
-        int longestPrefix = 0;
 
+        int longestPrefixCount = 0;
         for(int val: arr2){
-            while(!arr1Prefixes.count(val) && val > 0){
+            // it will fail when the val is found
+            while(!prefixes.count(val) && val > 0){
                 val /= 10;
             }
+
+            // if val is found then execute this
             if(val > 0){
                 string str = to_string(val);
-                // longestPrefix = max(longestPrefix, str.length());
-                if (longestPrefix < str.length()){
-                    longestPrefix = str.length();
-                }
+                longestPrefixCount = max(longestPrefixCount, (int)str.length());
             }
         }
-        return longestPrefix;
+
+        return longestPrefixCount;
     }
 };
