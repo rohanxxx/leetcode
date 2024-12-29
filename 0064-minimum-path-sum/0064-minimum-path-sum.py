f012dd7d58e644bb6117abba0d1a817412118649
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-12-28 22:23:09 -0500

    Time: 15 ms (75.62%), Space: 14.3 MB (17.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -14,15 +14,15 @@ class Solution(object):
 
         dp[m-1][n-1] = grid[m-1][n-1]
         for i in range(n-2, -1, -1):
-            dp[m-1][i] += dp[m-1][i+1]
+            dp[m-1][i] += (dp[m-1][i+1]+grid[m-1][i])
         for i in range(m-2, -1, -1):
-            dp[i][n-1] += dp[i+1][n-1]
+            dp[i][n-1] += (dp[i+1][n-1]+grid[i][n-1])
 
         # goes bottom to up
         for i in range(m-2, -1, -1):
             # goes right to left:
             for j in range(n-2, -1, -1):
-                dp[i][j] = min(dp[i][j+1], dp[i+1][j])
+                dp[i][j] = min(dp[i][j+1], dp[i+1][j])+grid[i][j]
 
         return dp[0][0]
         
