3b68e0f5596a5aa309c8acd9d82624112cc0f7eb
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2024-12-29 22:37:09 -0500

    Time: 3 ms (34.3%), Space: 14.7 MB (24.4%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int overall_xor = 0;
+        unordered_map<int, int> freq;
         for (int n : nums) {
-            overall_xor ^= n;
+            freq[n]++;
         }
 
-        int first_group_xor = 0;
-        int second_group_xor = 0;
-
-        int bit_pos_dif = 0;
-        while (((overall_xor >> bit_pos_dif) & 1) != 1) {
-            bit_pos_dif++;
-        }
-
-        for (int num : nums) {
-            if (((num >> bit_pos_dif) & 1) == 1) {
-                first_group_xor ^= num;
-            } else {
-                second_group_xor ^= num;
+        vector<int> res;
+        for (auto& pair : freq) {
+            if (pair.second == 1) {
+                res.push_back(pair.first);
             }
         }
 
-        return {first_group_xor, second_group_xor};
+        return res;
     }
 };
