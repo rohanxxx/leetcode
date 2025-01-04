fbe29584ffc124b2c4a8b1aae29a46a9108cc4e3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-04 08:42:18 -0500

    Time: 11 ms (60.4%), Space: 11.4 MB (42.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,58 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-     // Count the occurrences of each character in string t
-        unordered_map<char, int> m;
-        for (int i = 0; i < t.size(); i++) {
-            m[t[i]]++;
-        }
-        
-        int i = 0;                // Left pointer of the sliding window
-        int j = 0;                // Right pointer of the sliding window
-        
-        int counter = t.size();   // Number of characters in t that must be present in the current window
-        
-        int minStart = 0;         // Start index of the minimum window
-        int minLength = INT_MAX;   // Length of the minimum window
-        
-        while (j < s.size()) {
-            // If the character in s is required for the window, decrease the counter
-            if (m[s[j]] > 0) {
-                counter--;
-            }
+        unordered_map<char, int> mp;
+        for(char& c: t) mp[c]++;
+
+        int left = 0, right = 0;
+
+        int counter = t.length();
+
+        int startIndex = 0;
+        int minLen = INT_MAX;
+
+        int size = s.length();
+        while(right < size){
+            if(mp[s[right]] > 0) counter--;
             
-            // Decrease the count of the character in the window
-            m[s[j]]--;
+            mp[s[right]]--;
+            right++;
             
-            // Move the right pointer to expand the window
-            j++;
-            
-            // When a valid window is found, move the left pointer to find a smaller window
-            while (counter == 0) {
-                // Update the minimum window information if the current window is smaller
-                if (j - i < minLength) {
-                    minStart = i;
-                    minLength = j - i;
+            while(counter == 0){
+                if(right-left < minLen){
+                    startIndex = left;
+                    minLen = right-left;
                 }
-                
-                // Increase the count of the character going out of the window
-                m[s[i]]++;
-                
-                // When a required character is present in the window, increase the counter
-                if (m[s[i]] > 0) {
-                    counter++;
-                }
-                
-                // Move the left pointer to shrink the window
-                i++;
+
+                mp[s[left]]++;
+                if(mp[s[left]] > 0) counter++;
+
+                left++;
             }
-            
+
         }
-        
-        // Check if a valid minimum window was found
-        if (minLength != INT_MAX) {
-            return s.substr(minStart, minLength);
-        }
+
+        if(minLen != INT_MAX) return s.substr(startIndex, minLen);
         return "";
     }
 };
