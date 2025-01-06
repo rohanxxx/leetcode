87067a87d40dfbdc075dcf08a87c2766e6b98ce3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-06 15:40:37 -0500

    Time: 0 ms (100%), Space: 21.9 MB (29.72%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,27 +11,22 @@
  */
 class Solution {
 public:
-    
-    bool inorderTraversal(TreeNode* root, long int& rootVal){
-        if(root == NULL)
+    bool validate(TreeNode* root, TreeNode* low, TreeNode* high) {
+        // Empty trees are valid BSTs.
+        if (root == nullptr) {
             return true;
-        
-        if(!inorderTraversal(root->left, rootVal))
+        }
+
+        // The current node's value must be between low and high.
+        if ((low != nullptr and root->val <= low->val) or (high != nullptr and root->val >= high->val)) {
             return false;
-        
-        if(rootVal >= root->val)
-            return false;
-        
-        rootVal = root->val;
-        
-        if(!inorderTraversal(root->right, rootVal))
-            return false;
-        
-        return true;
+        }
+
+        // The left and right subtree must also be valid.
+        return validate(root->right, root, high) and validate(root->left, low, root);
     }
-    
+
     bool isValidBST(TreeNode* root) {
-        long int rootVal = LONG_MIN;
-        return inorderTraversal(root, rootVal);
+        return validate(root, nullptr, nullptr);
     }
 };
