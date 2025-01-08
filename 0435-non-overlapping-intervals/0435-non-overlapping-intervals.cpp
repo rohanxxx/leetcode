5ac584af94fba1fee05878ee11d172a033e84ebe
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-08 15:51:36 -0500

    Time: 98 ms (23.44%), Space: 106.7 MB (20.36%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,3 +1,4 @@
+
 bool compareY(vector<int>& a, vector<int>& b){
     return a[1] < b[1];
 }
@@ -5,16 +6,11 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), compareY);
-        // sort(intervals.begin(), intervals.end());
-        for(auto& i: intervals){
-            cout << "[" << i[0] << "," << i[1] << "],";
-        }
-        cout << "]"<< endl;
         int maxRange = INT_MIN;
         int removeOperations = 0;
-        for(auto interval: intervals){
-            int x = interval[0];
-            int y = interval[1];
+        for(auto i: intervals){
+            int x = i[0];
+            int y = i[1];
             if(x >= maxRange) maxRange = y;
             else removeOperations++;
         }
