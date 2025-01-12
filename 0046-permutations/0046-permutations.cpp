e776ea9ebdccc4cbf3b81209767dca737ac53a59
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-11 21:59:27 -0500

    Time: 0 ms (100%), Space: 11 MB (29.85%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -7,13 +7,15 @@ public:
             return;
         }
 
-        for(int i = 0; i < nums.size(); i++){
-            if(find(current.begin(), current.end(), nums[i]) == current.end()){
-                current.push_back(nums[i]);
+        for(int& num: nums){
+            // if not found then push it to the current vector
+            if(find(current.begin(), current.end(), num) == current.end()){
+                current.push_back(num);
                 backtrack(nums, current);
                 current.pop_back();
             }
         }
+        return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> current;
