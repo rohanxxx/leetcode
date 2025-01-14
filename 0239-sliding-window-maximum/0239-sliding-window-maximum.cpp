a1bf52143a76ba7c4d2b3051bff32ca5c1598c8e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-14 03:21:10 -0500

    Time: 16 ms (89.16%), Space: 139.2 MB (38.07%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,16 +4,22 @@ public:
         deque<int> dq;
         vector<int> ans;
 
+        // initialize case here
         for(int i = 0; i < k; i++){
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) dq.pop_back();
+            while(!dq.empty() && nums[i] > nums[dq.back()]){
+                dq.pop_back();
+            }
             dq.push_back(i);
         }
 
         ans.push_back(nums[dq.front()]);
 
         for(int i = k; i < nums.size(); i++){
-            if(dq.front() == i-k) dq.pop_front();
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) dq.pop_back();
+            if(k == i-dq.front()) dq.pop_front();
+            while(!dq.empty() && nums[i] > nums[dq.back()]){
+                dq.pop_back();
+            }
+
             dq.push_back(i);
             ans.push_back(nums[dq.front()]);
         }
