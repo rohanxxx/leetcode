4f7e28120cff42ca246c6051a9f53fb209ae1afb
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-25 00:06:47 -0500

    Time: 0 ms (100%), Space: 10 MB (43.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.length() > s2.length()) return false;
-        
-        vector<int> ans;
+        if(s2.length() < s1.length()) return false;
         vector<int> s1map(26, 0), s2map(26, 0);
-
-        int len = s1.length();
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < s1.length(); i++) {
             s1map[s1[i]-'a']++;
             s2map[s2[i]-'a']++;
         }
-
         if(s1map == s2map) return true;
-
-        for(int i = len; i < s2.length(); i++){
-            int index = i-len;
+        for(int i = s1.length(); i < s2.length(); i++){
+            int index = i - s1.length();
+            
             s2map[s2[i]-'a']++;
             s2map[s2[index]-'a']--;
             if(s2map == s1map) return true;
