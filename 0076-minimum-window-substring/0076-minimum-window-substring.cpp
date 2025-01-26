83fe7cd0a69cf92f27344917e4eee0358d8a64cd
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-25 22:50:19 -0500

    Time: 11 ms (54.19%), Space: 11.4 MB (81.1%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,36 +2,25 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         unordered_map<char, int> mp;
-        for(char& c: t) mp[c]++;
+        for(auto& c: t) mp[c]++;
 
-        int left = 0, right = 0;
+        int minLen = INT_MAX, count = t.length();
+        int left = 0, right = 0, startIndex = 0;
 
-        int counter = t.length();
-
-        int startIndex = 0;
-        int minLen = INT_MAX;
-
-        int size = s.length();
-        while(right < size){
-            if(mp[s[right]] > 0) counter--;
+        int sSize = s.length();
+        while(right < sSize){
+            if(mp[s[right]] > 0) count--;
             
-            mp[s[right]]--;
-            right++;
-            
-            while(counter == 0){
+            mp[s[right++]]--;
+            while(count == 0){
                 if(right-left < minLen){
                     startIndex = left;
                     minLen = right-left;
                 }
-
                 mp[s[left]]++;
-                if(mp[s[left]] > 0) counter++;
-
-                left++;
+                if(mp[s[left++]] > 0) count++;
             }
-
         }
-
         if(minLen != INT_MAX) return s.substr(startIndex, minLen);
         return "";
     }
