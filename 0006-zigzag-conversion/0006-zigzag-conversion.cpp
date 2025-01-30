305cabcb2f24ffc5fda2a407472dd49bd55b9357
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-01-30 00:59:27 -0500

    Time: 0 ms (100%), Space: 10.8 MB (92.89%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,33 +3,32 @@ public:
     string convert(string s, int numRows) {
         if(numRows == 1) return s;
         if(s.length() < numRows) return s;
-        
-        string answer = "";
 
         int maxJump = (numRows-2)+numRows;
         int j1 = maxJump;
         int j2 = 0;
-        
+
+        string ans = "";
         for(int i = 0; i < numRows; i++){
             int index = i;
-            
-            answer += s[index];
+            ans += s[index];
             while(j1 >= 0 && j2 <= maxJump){
                 if(j1){
                     index += j1;
                     if(index > s.length()-1) break;
-                    answer += s[index];
+                    ans += s[index];
                 }
-                
                 if(j2){
                     index += j2;
                     if(index > s.length()-1) break;
-                    answer += s[index];
+                    ans += s[index];
                 }
             }
+
             j1 -= 2;
             j2 += 2;
         }
-        return answer;
+
+        return ans;
     }
 };
