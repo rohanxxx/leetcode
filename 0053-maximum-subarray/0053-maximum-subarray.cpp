2ff8a587cf050d7f16a5389bb41e9028c565abbf
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-06 18:12:56 -0500

    Time: 8 ms (5.06%), Space: 71.9 MB (17.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,9 @@ public:
         for(int num: nums){
             sum += num;
             maxSum = max(maxSum, sum);
-            if(sum < 0) sum = 0;
+            if(sum < 0){
+                sum = 0;
+            }
         }
         return maxSum;
     }
