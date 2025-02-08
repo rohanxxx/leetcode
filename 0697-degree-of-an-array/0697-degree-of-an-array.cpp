63533af0e67cae87e4f3af2f4be12427382cea22
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-08 14:02:14 -0500

    Time: 16 ms (30.77%), Space: 29.6 MB (61.69%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -4,7 +4,6 @@ public:
         int maxDegree = 0;
         unordered_map<int, int> freq, mp;
         for(int num: nums){
-            // freq[num]++;
             maxDegree = max(maxDegree, ++freq[num]);
         }
         int left = 0, minLen = INT_MAX;
@@ -12,8 +11,8 @@ public:
             mp[nums[right]]++;
             while(mp[nums[right]] == maxDegree){
                 minLen = min(minLen, right-left+1);
-                mp[nums[left]]--;
-                left++;
+                mp[nums[left++]]--;
+                // left++;
             }
         }
         return minLen;
