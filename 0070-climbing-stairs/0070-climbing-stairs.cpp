b9bb1a4849b42b7aa178b9bb9ace5a869bb992b2
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-10 18:28:32 -0500

    Time: 0 ms (100%), Space: 7.7 MB (87.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        
-        int prev1 = 1;
-        int prev2 = 2;
-        int answer = 0;
-        
-        for(int i = 3; i <= n; i++){
-            answer = prev1+prev2;
-            prev1 = prev2;
-            prev2 = answer;
+        if(n == 1) return 1;
+        int ans = 0;
+        int prev1 = 1, prev2 = 1;
+        for(int i = 2; i <= n; i++){
+            ans = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = ans;
         }
-
-        return answer;
+        return ans;
     }
 };
