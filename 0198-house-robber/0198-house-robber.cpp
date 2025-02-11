f85be88e095bf11cbecefee01866e049e3c20dc8
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-10 20:51:18 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int len = nums.size();
-        
-        if(len == 1) return nums.back();
-        if(len == 2) return max(nums[len-1], nums[len-2]);
-        
-        vector<int> dp(len, 0);
-        dp[0] = nums[0]; dp[1] = max(nums[1], dp[0]);
-        for(int i = 2; i < len; i++){
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+
+        vector<int> dp0, dp1;
+        dp0.push_back(nums[0]);
+        dp0.push_back(max(nums[1], nums[0]));
+
+        for(int i = 0; i < len-1; i++){
+            int size = dp0.size();
+            dp0.push_back(max(dp0[size-1], dp0[size-2]+nums[i]));
         }
-        return dp.back();
+
+        dp1.push_back(nums[1]);
+        dp1.push_back(max(nums[1], nums[2]));
+
+        for(int i = 1; i < len; i++){
+            int size = dp1.size();
+            dp1.push_back(max(dp1[size-1], dp1[size-2]+nums[i]));
+        }
+
+        return max(dp1.back(), dp0.back());
     }
 };
