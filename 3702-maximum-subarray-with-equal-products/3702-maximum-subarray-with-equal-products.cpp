744299662e852cf8a876ff052bea2a4b2c283525
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-11 20:33:28 -0500

    Time: 2 ms (55.08%), Space: 28 MB (84.64%) - LeetHub

--- 3702-maximum-subarray-with-equal-products/3702-maximum-subarray-with-equal-products.cpp
+++ 3702-maximum-subarray-with-equal-products/3702-maximum-subarray-with-equal-products.cpp
@@ -4,24 +4,16 @@ public:
         int maxLen = 0;
         for(int i = 0; i < nums.size(); i++){
             int product = 1;
-            int gcdVal = nums[i];
-            int lcmVal = nums[i];
-
-            int maxLcm = 1e9;
-
+            int gcdVal = nums[i], lcmVal = nums[i];
             for(int j = i; j < nums.size(); j++){
-                product = product*(long long)nums[j];
-                if(product > maxLcm){
-                    break;
-                }
-                gcdVal = gcd(gcdVal, (long long)nums[j]);
-                lcmVal = lcm(lcmVal, (long long)nums[j]);
+                product *= (long long)nums[j];
+                gcdVal = gcd(gcdVal, nums[j]);
+                lcmVal = lcm(lcmVal, nums[j]);
                 if(product == (gcdVal*lcmVal)){
                     maxLen = max(maxLen, j-i+1);
                 }
             }
         }
-
         return maxLen;
     }
 };
