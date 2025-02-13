ee63f06c02ed47f35198f53dfa388362a5bf0176
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-13 13:09:43 -0500

    Time: 4 ms (71.05%), Space: 133.2 MB (52.63%) - LeetHub

--- 3694-minimum-total-operations/3694-minimum-total-operations.cpp
+++ 3694-minimum-total-operations/3694-minimum-total-operations.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size(), minOpsCount = 0;
-        for(int i = 1, prev = nums[0]; i < n; i++){
-            if(nums[i] != prev){
-                prev = nums[i];
+        for(int i = 1; i < n; i++){
+            if(nums[i] != nums[i-1]){
                 minOpsCount++;
             }
         }
