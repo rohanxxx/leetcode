db3216ab65048b38b23c5e9b7451ff5d7b4a979b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-13 20:50:47 -0500

    Time: 552 ms (5.14%), Space: 34.6 MB (51.06%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        vector<int> result;
+        unordered_map<int, int> mp;
+        for(int i = 0; (i+k-1) < nums.size(); i++){
+            if(i == 0){
+                for(int j = 0; j < k; j++){
+                    mp[nums[j]]++;
+                }
+            }
+            if(i != 0){
+                mp[nums[i-1]]--;
+                mp[nums[i+(k-1)]]++;
+            }
+
+            priority_queue<pair<int, int>> pq;
+            for(auto pair: mp){
+                cout << "pair.first: " << pair.first << endl;
+                cout << "pair.second: " << pair.second << endl;
+                pq.push({pair.second, pair.first});
+            }
+
+            int sum = 0;
+            for(int j = 0; j < x && !pq.empty(); j++){
+                pair<int,int> p = pq.top();
+                for(int k = 0; k < p.first; k++){
+                    sum += p.second;
+                }
+                pq.pop();
+            }
+            result.push_back(sum);
+        }
+        return result;
+    }
+};
