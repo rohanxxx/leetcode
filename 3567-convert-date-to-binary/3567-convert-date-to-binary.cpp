3b5bd4b50304d998c98bdfe8bf6d579100ee910d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-14 22:27:36 -0500

    Time: 0 ms (100%), Space: 8.6 MB (79.84%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    string getBinaryString(bitset<32>& bits){
+    string getBinaryString(bitset<16>& bits){
         string s = bits.to_string();
         int index = s.find('1');
         return s.substr(index);
     }
     string convertDateToBinary(string date) {
-        bitset<32> year(stoi(date.substr(0, 4)));
-        bitset<32> month(stoi(date.substr(5, 2)));
-        bitset<32> day(stoi(date.substr(8, 2)));
+        bitset<16> year(stoi(date.substr(0, 4)));
+        bitset<16> month(stoi(date.substr(5, 2)));
+        bitset<16> day(stoi(date.substr(8, 2)));
 
         string ans = "";
         ans += getBinaryString(year);
