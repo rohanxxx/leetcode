b76905dacdf41b7d5879316c669b2d65e7a07677
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-22 15:06:08 -0500

    Time: 0 ms (100%), Space: 12.4 MB (60.84%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -1,35 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    int minDistance = INT_MAX;
+    TreeNode* prevValue;
+    void inorderTraversal(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        inorderTraversal(root->left);
+        // Find the difference with the previous value if it is there.
+        if (prevValue != NULL) {
+            minDistance = min(minDistance, root->val - prevValue->val);
+        }
+        prevValue = root;
+        inorderTraversal(root->right);
+    }
+    
     int minDiffInBST(TreeNode* root) {
-        if(root == NULL) return -1;
-        queue<TreeNode*> q; q.push(root);
-        vector<int> node;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                TreeNode* temp = q.front(); q.pop();
-                node.push_back(temp->val);
-                if(temp->left != NULL) q.push(temp->left);
-                if(temp->right != NULL) q.push(temp->right);
-            }
-        }
-
-        int minDif = INT_MAX;
-        sort(node.begin(), node.end());
-        for(int i = 1; i < node.size(); i++){
-            minDif = min(minDif, node[i]-node[i-1]);
-        }
-        return minDif;
+        inorderTraversal(root);
+        return minDistance;
     }
 };
