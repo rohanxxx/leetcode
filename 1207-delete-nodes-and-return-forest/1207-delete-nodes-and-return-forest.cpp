d8846454b2a65baf60b3a1e3e943cdfa0d698c2d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-22 23:41:15 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -1,37 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        
-        if (!root) return {};
-
-        unordered_set<int> toDeleteSet(to_delete.begin(), to_delete.end());
         vector<TreeNode*> forest;
-
-        queue<TreeNode*> nodesQueue;
-        nodesQueue.push(root);
-
-        while (!nodesQueue.empty()) {
-            TreeNode* currentNode = nodesQueue.front();
-            nodesQueue.pop();
-
-            if (currentNode->left) {
-                nodesQueue.push(currentNode->left);
-                if (toDeleteSet.find(currentNode->left->val) != toDeleteSet.end()) currentNode->left = NULL;
+        if(!root) return forest;
+        
+        unordered_set<int> deleteNodes(to_delete.begin(), to_delete.end());
+        
+        queue<TreeNode*> q; q.push(root);
+        while(!q.empty()){
+            TreeNode* currentNode = q.front(); q.pop();
+            if(currentNode->left){
+                q.push(currentNode->left);
+                if(deleteNodes.find(currentNode->left->val) != deleteNodes.end()){
+                    currentNode->left = NULL;
+                }
             }
-
-            if (currentNode->right) {
-                nodesQueue.push(currentNode->right);
-                if (toDeleteSet.find(currentNode->right->val) != toDeleteSet.end()) currentNode->right = NULL;
+            if(currentNode->right){
+                q.push(currentNode->right);
+                if(deleteNodes.find(currentNode->right->val) != deleteNodes.end()){
+                    currentNode->right = NULL;
+                }
             }
-
-            if (toDeleteSet.find(currentNode->val) != toDeleteSet.end()) {
-                if (currentNode->left) forest.push_back(currentNode->left);
-                if (currentNode->right) forest.push_back(currentNode->right);
+            if(deleteNodes.find(currentNode->val) != deleteNodes.end()){
+                forest.push_back(currentNode->left);
+                forest.push_back(currentNode->right);
             }
         }
-
-        if (toDeleteSet.find(root->val) == toDeleteSet.end()) forest.push_back(root);
-
+        if(deleteNodes.find(root->val) == deleteNodes.end()){
+            forest.push_back(root);
+        }
         return forest;
     }
 };
