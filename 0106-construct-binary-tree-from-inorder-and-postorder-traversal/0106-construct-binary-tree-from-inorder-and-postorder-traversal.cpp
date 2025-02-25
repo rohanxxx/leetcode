f230a0c91fb3152af0736793d52fb5f73a2254f7
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-25 00:43:34 -0500

    Time: 3 ms (71.01%), Space: 27.5 MB (58.51%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,35 +1,28 @@
 class Solution {
-    int post_idx;
-    vector<int> postorder;
-    vector<int> inorder;
-    unordered_map<int, int> idx_map;
+    int postorderIndex;
+    unordered_map<int, int> mp;
 
 public:
-    TreeNode* helper(int in_left, int in_right) {
-        // if there are no elements to construct subtrees
-        if (in_left > in_right) return NULL;
+    TreeNode* helper(vector<int>& postorder, vector<int>& inorder, int left, int right) {
+        if (left > right) return NULL;
         // pick up post_idx element as a root
-        int root_val = postorder[post_idx];
+        int root_val = postorder[postorderIndex];
         TreeNode* root = new TreeNode(root_val);
         // root splits inorder list
         // into left and right subtrees
-        int index = idx_map[root_val];
+        int index = mp[root_val];
         // recursion
-        post_idx--;
-        // build the right subtree
-        root->right = helper(index + 1, in_right);
-        // build the left subtree
-        root->left = helper(in_left, index - 1);
+        postorderIndex--;
+        root->right = helper(postorder, inorder, index + 1, right);
+        root->left = helper(postorder, inorder, left, index - 1);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        this->postorder = postorder;
-        this->inorder = inorder;
-        // start from the last postorder element
-        post_idx = postorder.size() - 1;
-        // build a hashmap value -> its index
-        int idx = 0;
-        for (int val : inorder) idx_map[val] = idx++;
-        return helper(0, inorder.size() - 1);
+        int n = postorder.size();
+        postorderIndex = n - 1;
+        for (int i = 0; i < n; i++) {
+            mp[inorder[i]] = i;
+        }
+        return helper(postorder, inorder, 0, n - 1);
     }
 };
