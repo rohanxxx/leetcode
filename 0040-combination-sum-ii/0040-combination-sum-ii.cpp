f1bf54dd2acb36241b918e80671a43aa8ff7a8f6
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-02-28 23:07:53 -0500

    Time: 4 ms (27.19%), Space: 14.1 MB (61.68%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> tempList;
+        sort(candidates.begin(), candidates.end());
+        backtrack(result, tempList, candidates, target, 0);
+        return result;
+    }
+
+private:
+    void backtrack(vector<vector<int>>& answer, vector<int>& tempList, vector<int>& candidates, int totalLeft, int index) {
+        if (totalLeft < 0) return;
+        if (totalLeft == 0) {
+            answer.push_back(tempList);
+            return;
+        } 
+        for (int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+                tempList.push_back(candidates[i]);
+                backtrack(answer, tempList, candidates, totalLeft - candidates[i], i + 1);
+                tempList.pop_back();
+        }
+    }
+};
