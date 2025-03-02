80d67ea5238e450e74c1f7cbd3ac5f1baf482c07
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-02 05:55:42 -0500

    Time: 143 ms (43.08%), Space: 155 MB (26.15%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
-        // Sort based on min task processing time or min task index.
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> nextTask;
-
-        // Store task enqueue time, processing time, index.
-        vector<array<int, 3>> sortedTasks;
-        for (int i = 0; i < tasks.size(); ++i) {
+        vector<vector<int>> sortedTasks;
+        for(int i = 0; i < tasks.size(); i++){
             sortedTasks.push_back({tasks[i][0], tasks[i][1], i});
         }
-
         sort(sortedTasks.begin(), sortedTasks.end());
-        vector<int> tasksProcessingOrder;
 
-        long long currTime = 0;
-        int taskIndex = 0;
+        vector<int> ans;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        // Stop when no tasks are left in array and heap.
-        while (taskIndex < tasks.size() || !nextTask.empty()) {
-            if (nextTask.empty() && currTime < sortedTasks[taskIndex][0]) {
-                // When the heap is empty, try updating currTime to next
-                // task's enqueue time.
-                currTime = sortedTasks[taskIndex][0];
+        int index = 0;
+        long long curTime = 0;
+
+        while(index < tasks.size() || !pq.empty()){
+            if(pq.empty() && curTime < sortedTasks[index][0]){
+                curTime = sortedTasks[index][0];
             }
-
-            // Push all the tasks whose enqueueTime <= currtTime into the heap.
-            while (taskIndex < sortedTasks.size() && currTime >= sortedTasks[taskIndex][0]) {
-                nextTask.push({sortedTasks[taskIndex][1], sortedTasks[taskIndex][2]});
-                ++taskIndex;
+            while(index < sortedTasks.size() && sortedTasks[index][0] <= curTime ){
+                pq.push({sortedTasks[index][1], sortedTasks[index][2]});
+                index++;
             }
+            auto [processingTime, idx] = pq.top();
+            pq.pop();
 
-            auto [processTime, index] = nextTask.top();
-            nextTask.pop();
-
-            // Complete this task and increment currTime.
-            currTime += processTime;
-            tasksProcessingOrder.push_back(index);
+            curTime += processingTime;
+            ans.push_back(idx);
         }
 
-        return tasksProcessingOrder;
+        return ans;
     }
 };
