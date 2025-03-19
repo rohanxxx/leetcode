c6f3b36a435cc2dbe36957038f71f96a1d17766c
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-19 17:21:55 -0400

    Time: 4 ms (65.35%), Space: 15.6 MB (28.38%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,62 +9,57 @@
  */
 class Solution {
 public:
-    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track, TreeNode* target) {
-    queue<TreeNode*> queue;
-    queue.push(root);
-    while (!queue.empty()) {
-        TreeNode* current = queue.front();
-        queue.pop();
-        if (current->left) {
-            parent_track[current->left] = current;
-            queue.push(current->left);
-        }
-        if (current->right) {
-            parent_track[current->right] = current;
-            queue.push(current->right);
-        }
-    }
-}
-
-public:
-vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-    unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
-    markParents(root, parent_track, target);
-
-    unordered_map<TreeNode*, bool> visited;
-    queue<TreeNode*> queue;
-    queue.push(target);
-    visited[target] = true;
-    int curr_level = 0;
-    while (!queue.empty()) {
-        /* Second BFS to go up to K level from target node and using our hashtable info */
-        int size = queue.size();
-        if (curr_level++ == k) break;
-        for (int i = 0; i < size; i++) {
-            TreeNode* current = queue.front();
-            queue.pop();
-            if (current->left && !visited[current->left]) {
-                queue.push(current->left);
-                visited[current->left] = true;
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track){
+        queue<TreeNode*> q; q.push(root);
+        while(!q.empty()){
+            TreeNode* current = q.front(); q.pop();
+            if(current->left){
+                parent_track[current->left] = current;
+                q.push(current->left);
             }
-            if (current->right && !visited[current->right]) {
-                queue.push(current->right);
-                visited[current->right] = true;
-            }
-            if (parent_track[current] && !visited[parent_track[current]]) {
-                queue.push(parent_track[current]);
-                visited[parent_track[current]] = true;
+            if(current->right){
+                parent_track[current->right] = current;
+                q.push(current->right);
             }
         }
     }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*, TreeNode*> parent_track;
+        markParents(root, parent_track);
 
-    vector<int> result;
-    while (!queue.empty()) {
-        TreeNode* current = queue.front(); 
-        queue.pop();
-        result.push_back(current->val);
+        unordered_map <TreeNode*, bool> visited;
+        queue<TreeNode*> q; q.push(target);
+        
+        visited[target] = true;
+
+        int curLevel = 0;
+        while(!q.empty()){
+            int size = q.size();
+            if(curLevel++ == k){
+                break;
+            }
+            for(int i = 0; i < size; i++){
+                TreeNode* current = q.front(); q.pop();
+                if(current->left && !visited[current->left]){
+                    q.push(current->left);
+                    visited[current->left] = true;
+                }
+                if(current->right && !visited[current->right]){
+                    q.push(current->right);
+                    visited[current->right] = true;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]){
+                    q.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+
+        vector<int> result;
+        while(!q.empty()){
+            TreeNode* current = q.front(); q.pop();
+            result.push_back(current->val);
+        }
+        return result;
     }
-
-    return result;
-}
 };
