1667b48f9353d5373bca0d445da079ebae0a50f0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-19 17:41:53 -0400

    Time: 6 ms (40.04%), Space: 15.4 MB (51.49%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,29 +9,30 @@
  */
 class Solution {
 public:
-    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track){
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent_track){
         queue<TreeNode*> q; q.push(root);
         while(!q.empty()){
             TreeNode* current = q.front(); q.pop();
             if(current->left){
-                parent_track[current->left] = current;
                 q.push(current->left);
+                parent_track[current->left] = current;
             }
             if(current->right){
-                parent_track[current->right] = current;
                 q.push(current->right);
+                parent_track[current->right] = current;
             }
+            
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         unordered_map<TreeNode*, TreeNode*> parent_track;
         markParents(root, parent_track);
 
-        unordered_map <TreeNode*, bool> visited;
-        queue<TreeNode*> q; q.push(target);
+        unordered_map<TreeNode*, bool> visited;
         
+        queue<TreeNode*> q; q.push(target);
         visited[target] = true;
-
+        
         int curLevel = 0;
         while(!q.empty()){
             int size = q.size();
@@ -42,15 +43,15 @@ public:
                 TreeNode* current = q.front(); q.pop();
                 if(current->left && !visited[current->left]){
                     q.push(current->left);
-                    visited[current->left] = true;
+                    visited[current->left] = true;   
                 }
                 if(current->right && !visited[current->right]){
                     q.push(current->right);
-                    visited[current->right] = true;
+                    visited[current->right] = true;   
                 }
                 if(parent_track[current] && !visited[parent_track[current]]){
                     q.push(parent_track[current]);
-                    visited[parent_track[current]] = true;
+                    visited[parent_track[current]] = true;   
                 }
             }
         }
