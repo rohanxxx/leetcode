9afa1f9cb375871e733f140dfa4e7c097b22f5d0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-20 00:21:35 -0400

    Time: 28 ms (40.34%), Space: 21.1 MB (73.31%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -12,29 +12,21 @@ class Solution {
 public:
     int getImportance(vector<Employee*> employees, int id) {
         unordered_map<int, Employee*> mp;
-
-        //creates the tree
-        for(auto& e: employees){
+        for(auto e: employees){
             mp[e->id] = e;
         }
 
-        //pushes the employee importance id
-        queue<Employee*> q;
-        q.push(mp[id]);
+        queue<Employee*> q; q.push(mp[id]);
 
         int totalImportance = 0;
-        //all the branches of the queue gets executed here
         while(!q.empty()){
             Employee* e = q.front(); q.pop();
-
-            //summing up the importance here
             totalImportance += e->importance;
             
             int size = e->subordinates.size();
-            //pushes the employee subID's to the queue
             for(int i = 0; i < size; i++){
-                int subordinateID = e->subordinates[i];
-                q.push(mp[subordinateID]);
+                int subOrdinateId = e->subordinates[i];
+                q.push(mp[subOrdinateId]);
             }
         }
         return totalImportance;
