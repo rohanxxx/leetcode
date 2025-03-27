dcd7055ee083efd3ebfcd08d4cd82257bbde4863
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-27 02:45:07 -0400

    Time: 28 ms (33.85%), Space: 28.2 MB (67.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int func(int i, int target, vector<int>& nums, vector<vector<int>>& dp){
-        if(i == 0){
-            if(target % nums[0] == 0) return dp[i][target] = 1;
-            else return dp[i][target] = 0;
-        }
-        if(dp[i][target] != -1) return dp[i][target];
-        int notTake = func(i-1, target, nums, dp);
-        int take = 0;
-        if(nums[i] <= target){
-            take = func(i, target-nums[i], nums, dp);
-        }
-        return dp[i][target] = notTake+take;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return func(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        for(int t = 0; t <= amount; t++){
+            if(t % coins[0] == 0){
+                dp[0][t] = 1;
+            }
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= amount; t++){
+                int notTake = dp[i-1][t];
+                int take = 0;
+                if(coins[i] <= t){
+                    take = dp[i][t-coins[i]];
+                }
+                // dp[i][t] = notTake+take;
+                if((long)notTake+take >= (long)INT_MAX){
+                    dp[i][t] = 0;
+                }
+                else{
+                    dp[i][t] = notTake+take;
+                }
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
