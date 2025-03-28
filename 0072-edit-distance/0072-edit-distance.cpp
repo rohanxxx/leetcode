14a10814fda838b508f57b7ba30e9a8037f0fb4c
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-28 16:07:03 -0400

    Time: 12 ms (23.51%), Space: 13.2 MB (30.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,7 +14,22 @@ public:
     }
     int minDistance(string s1, string s2) {
         int n = s1.length(), m = s2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return func(n, m, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                    continue;
+                }
+                int deleteOp = 1+dp[i-1][j];
+                int insertOp = 1+dp[i][j-1];
+                int replaceOp = 1+dp[i-1][j-1];
+                dp[i][j] = min({deleteOp, insertOp, replaceOp});   
+            }
+        }
+        // return func(n, m, s1, s2, dp);
+        return dp[n][m];
     }
 };
