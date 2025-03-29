94bee684a17dc7c781acbaa7931577197fe2acdb
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-03-29 04:33:13 -0400

    Time: 1 ms (47.13%), Space: 16.2 MB (33.84%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int sold = INT_MIN, held = INT_MIN, reset = 0;
-
-        for (int price : prices) {
-            int preSold = sold;
-            cout << "preSold: " << preSold << endl;
-            sold = held + price;
-            cout << "sold: " << sold << endl;
-            held = max(held, reset - price);
-            cout << "held: " << held << endl;
-            reset = max(reset, preSold);
-            cout << "reset: " << reset << endl << endl;
+    int func(int i, int buy, vector<int>& prices, vector<vector<int>>& dp){
+        if(i >= prices.size()) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+        int profit = 0;
+        if(buy){
+            profit = max(func(i+1, 0, prices, dp) - prices[i], func(i+1, 1, prices, dp));
         }
-
-        return max(sold, reset);
+        else{
+            profit = max(prices[i] + func(i+2, 1, prices, dp), func(i+1, 0, prices, dp));
+        }
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return func(0, 1, prices, dp);
     }
 };
