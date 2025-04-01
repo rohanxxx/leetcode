fe3af0968daf38767869d47adb17f43c95bf4f65
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-01 16:55:09 -0400

    Time: 87 ms (93.22%), Space: 13.8 MB (57.45%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -16,7 +16,20 @@ public:
         int n = a.size();
         a.push_back(1);
         a.insert(a.begin(), 1);
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return f(1, n, a, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) continue;
+                int maxi = INT_MIN;
+                for(int ind = i; ind <= j; ind++) {
+                    int cost = a[i - 1] * a[ind] * a[j + 1] +
+                            dp[i][ind - 1] + dp[ind + 1][j];
+                    maxi = max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        // return f(1, n, a, dp);
+        return dp[1][n];
     }
 };
