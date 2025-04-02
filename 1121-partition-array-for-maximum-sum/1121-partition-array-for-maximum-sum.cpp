cb0bc68be852296e6e57ac0646fa3a19b9d84066
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-02 14:14:09 -0400

    Time: 5 ms (62.8%), Space: 12.4 MB (85.08%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    int func(int index, int& k, int& n, vector<int>& arr, vector<int>& dp){
-        if(index == n) return 0;
-        if(dp[index] != -1) return dp[index];
-        int maxi = INT_MIN;
-        int len = 0, maxElement = INT_MIN;
-        for(int i = index; i < min(n, index+k); i++){
-            len++;
-            maxElement = max(arr[i], maxElement);
-            int sum = maxElement*len + func(i+1, k, n, arr, dp);
-            maxi = max(maxi, sum);
-        }
-        return dp[index] = maxi;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
         vector<int> dp(n+1, 0);
@@ -22,12 +9,11 @@ public:
             for(int i = index; i < min(n, index+k); i++){
                 len++;
                 maxElement = max(arr[i], maxElement);
-                int sum = maxElement*len + func(i+1, k, n, arr, dp);
+                int sum = maxElement*len + dp[i+1];
                 maxi = max(maxi, sum);
             }
             dp[index] = maxi;
         }
         return dp[0];
-        // return func(0, k, n, arr, dp);
     }
 };
