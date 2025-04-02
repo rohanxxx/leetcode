6246c4db3e7757975e2a5c8c72eff90c07d855a5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-02 12:57:21 -0400

    Time: 1750 ms (17.78%), Space: 9.4 MB (77.15%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int i, int j, string& s){
+        // int i = 0, j = s.length();
+        while(i <= j){
+            if(s[i] != s[j]) return false;
+            i++; j--;
+        }
+        return true;
+    }
+    int func(int i, string& s, vector<int>& dp){
+        if(i == s.length()) return 0;
+        if(dp[i] != -1) return dp[i];
+        int minCost = INT_MAX;
+        for(int j = i; j < s.length(); j++){
+            if(isPalindrome(i, j, s)){
+                int cost = 1+func(j+1, s, dp);
+                minCost = min(minCost, cost);
+            }
+        }
+        return dp[i] = minCost;
+    }
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> dp(n, -1);
+        return func(0, s, dp)-1;
+    }
+};
