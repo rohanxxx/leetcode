d44f8c02792c88273b1aa2782555536c1eb231a5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-10 18:41:05 -0400

    Time: 0 ms (100%), Space: 8.6 MB (34.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int func(int n, vector<int>& dp){
-        if(n == 1){
-            return dp[n] = 1;
-        }
-        if(n == 2){
-            return dp[n] = 2;
+        if(n == 1 || n == 2){
+            return dp[n] = n;
         }
         if(dp[n] != -1){
             return dp[n];
