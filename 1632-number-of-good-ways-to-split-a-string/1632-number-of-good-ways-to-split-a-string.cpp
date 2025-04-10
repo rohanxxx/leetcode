f65a7de7dd949cc5cf210f48cb653042f7cf78cb
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-09 21:13:05 -0400

    Time: 10 ms (62.81%), Space: 11.5 MB (64.81%) - LeetHub

--- 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.cpp
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int numSplits(string s) {
-        int count = 0;
         unordered_map<char, int> leftMap, rightMap;
-        for(char c: s) leftMap[c]++;
-        for(int i = s.length()-1; i >= 0; i--){
-            leftMap[s[i]]--;
-            rightMap[s[i]]++;
-            
-            if(leftMap[s[i]] == 0) leftMap.erase(s[i]);
-            if((int)leftMap.size() == (int)rightMap.size()) count++;
+        for(char c: s){
+            rightMap[c]++;
         }
 
-        return count;
+        int goodSplitCount = 0;
+        for(char c: s){
+            leftMap[c]++;
+            rightMap[c]--;
+            if(rightMap[c] == 0){
+                rightMap.erase(c);
+            }
+            if(leftMap.size() == rightMap.size()){
+                goodSplitCount++;
+            }
+        }
+
+        return goodSplitCount;
     }
 };
