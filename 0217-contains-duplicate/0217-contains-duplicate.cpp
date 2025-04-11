afb12a65094a01dce20e3cd8de3df80e02d59219
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-11 16:46:28 -0400

    Time: 31 ms (63.72%), Space: 73.6 MB (35.84%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int num: nums){
+            if(mp[num]++) return true;
+            // mp[num]++;
+        }
+        return false;
+    }
+};
