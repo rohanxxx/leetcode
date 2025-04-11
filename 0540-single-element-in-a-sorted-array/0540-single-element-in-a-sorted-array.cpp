5e6708d1e967ebfe7e04e16a3af5795f2ec5d735
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-11 18:09:06 -0400

    Time: 0 ms (100%), Space: 26.2 MB (33.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,8 +3,7 @@ public:
     int binarySearch(int left, int right, int& n, vector<int>& nums){
         if(left > right) return -1;
         int mid = (right+left)/2;
-        bool lc = false;
-        bool rc = false;
+        bool lc = false, rc = false;
         if(mid-1 >= 0){ 
             if(nums[mid-1] != nums[mid]) lc = true;
         }
@@ -13,18 +12,19 @@ public:
             if(nums[mid] != nums[mid+1]) rc = true;
         }
         else rc = true;
-        if(lc == true && rc == true) {
-            // cout << "mid: " << mid << endl;
-            return mid;
-        }
+        if(lc == true && rc == true) return mid;
+        
         int checkLeft = binarySearch(left, mid-1, n, nums);
+        
         if(checkLeft > -1) return checkLeft;
+        
         int checkRight = binarySearch(mid+1, right, n, nums);
+        
         return max(checkLeft, checkRight);
     }
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
+        // if(n == 1) return nums[0];
         int index = binarySearch(0, n-1, n, nums);
         if(index != -1) return nums[index];
         return -1;
