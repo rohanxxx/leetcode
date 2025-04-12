d806b01218def6b9827cd4cdac8c9cdd0c52e646
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-12 14:49:34 -0400

    Time: 0 ms (100%), Space: 17.4 MB (31.11%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -13,6 +13,7 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(k == 0) return head;
         if(head == NULL) return NULL;
+        
         int n = 0;
         ListNode* temp = head;
         while(temp){
@@ -20,25 +21,16 @@ public:
             temp = temp->next;
         }
         vector<ListNode*> arr(n);
-        int i = 0;
         temp = head;
-        // cout << "temp->val: " << temp->val << endl;
-        while(temp){
+        for(int i = 0; i < n; i++){
             int index = (i+k) % n;
-            // if(temp == NULL) cout << "line 25" << endl;
             arr[index] = temp;
-            // cout << "index: " << index;
-            // cout << " arr[index]: " << arr[index]->val << endl;
             temp = temp->next;
-            i++;
         }
-        for(int j = 0; j < n-1; j++){
-            // cout << arr[j]->val << " ";
-            arr[j]->next = arr[j+1];
+        for(int i = 0; i < n-1; i++){
+            arr[i]->next = arr[i+1];
         }
-        // cout << endl;
         arr[n-1]->next = NULL;
-
         return arr[0];
     }
 };
