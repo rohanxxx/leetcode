75114eee6ddd4ee5a2f27029e1b582ac7596d8cf
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-12 16:16:26 -0400

    Time: 22 ms (70.77%), Space: 88.3 MB (7.06%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        // priority_queue<int, vector<int>, greater<int>> minQ;
+        int maxElement = INT_MIN;
+        for(int num: candies){
+            maxElement = max(maxElement, num);
+        }
+
+        int maxPile = 0;
+        int left = 1, right = maxElement;
+        while(left <= right){
+            int i = (left+right)/2;
+            long long child = k;
+            for(int num: candies){
+                child -= (num/i);
+                if(child <= 0){
+                    maxPile = max(maxPile, i);
+                    break;
+                }
+            }
+
+            if(child <= 0) left = i+1;
+            else right = i-1;
+        }
+
+        return maxPile;
+    }
+};
