5640fca42090aab203459600274934fdff1d7df0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-12 13:59:04 -0400

    Time: 5 ms (12.63%), Space: 9.4 MB (28.51%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,14 +4,14 @@ public:
         if(s.length() != t.length()) return false;
         unordered_map<char, int> mp1, mp2;
         for(int i = 0; i < s.length(); i++){
-            if(mp1.find(s[i]) != mp1.end()){
+            if(mp1[s[i]]){
                 if(mp1[s[i]] != t[i]) return false;
             }
             else{
                 mp1[s[i]] = t[i];
             }
 
-            if(mp2.find(t[i]) != mp2.end()){
+            if(mp2[t[i]]){
                 if(mp2[t[i]] != s[i]) return false;
             }
             else{
