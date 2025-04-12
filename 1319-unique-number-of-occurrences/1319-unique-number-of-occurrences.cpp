5057dec0849738879736b8c5ff46bc2db843825e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-12 01:30:48 -0400

    Time: 0 ms (100%), Space: 12.1 MB (28.18%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> mp;
-        for(auto& num: arr){
-            mp[num]++;
-        }
+        unordered_map<int, int> freq;
+        for(int num: arr) freq[num]++;
 
-        vector<int> v;
-        for(auto it= mp.begin(); it != mp.end(); it++){
-            v.push_back(it->second);
-        }
-        sort(v.begin(), v.end());
-        for(int i = 1; i < v.size(); i++){
-            if(v[i] == v[i-1]) return false;
+        unordered_map<int, int> mp;
+        for(auto f: freq){
+            if(mp[f.second]) return false;
+            mp[f.second]++;
         }
         return true;
     }
