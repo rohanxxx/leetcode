776b6f4bac928add10f5a0bcc720c4d9d5290db7
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-12 23:43:28 -0400

    Time: 0 ms (100%), Space: 20.7 MB (5.92%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,26 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-  public:
-  ListNode* removeElements(ListNode* head, int val) {
-    ListNode* sentinel = new ListNode(0);
-    sentinel->next = head;
-
-    ListNode *prev = sentinel, *curr = head, *toDelete = nullptr;
-    while (curr != nullptr) {
-      if (curr->val == val) {
-        prev->next = curr->next;
-        toDelete = curr;
-      } else prev = curr;
-
-      curr = curr->next;
-
-      if (toDelete != nullptr) {
-        delete toDelete;
-        toDelete = nullptr;
-      }
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL) return NULL;
+        queue<ListNode*> q;
+        ListNode* temp = head;
+        while(temp){
+            if(temp->val != val){
+                // cout << "temp->val: " << temp ->val << endl;
+                q.push(temp);
+            }
+            temp = temp->next;
+        }
+        if(q.empty()) return NULL;
+        head = q.front();
+        ListNode* cur;
+        while(!q.empty()){
+            cur = q.front(); q.pop();
+            if(!q.empty()){
+                cur->next = q.front();
+            }
+        }
+        cur->next = NULL;
+        return head;
     }
-
-    ListNode *ret = sentinel->next;
-    delete sentinel;
-    return ret;
-  }
 };
