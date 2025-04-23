ef9cf1fb67965f07d57235473040c1fe6bc7103e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-22 20:33:09 -0400

    Time: 51 ms (29.23%), Space: 57.8 MB (10.89%) - LeetHub

--- 0305-number-of-islands-ii/0305-number-of-islands-ii.cpp
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.cpp
@@ -1,76 +1,72 @@
-class UnionFind {
-private:
-    vector<int> parent, rank;
-    int count;
-
-public:
-    UnionFind(int size) {
-        parent.resize(size, -1);
-        rank.resize(size, 0);
-        count = 0;
-    }
-
-    void addLand(int x) {
-        if (parent[x] >= 0) return;
-        parent[x] = x;
-        count++;
-    }
-
-    bool isLand(int x) {
-        if (parent[x] >= 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    int numberOfIslands() { return count; }
-
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    void union_set(int x, int y) {
-        int xset = find(x), yset = find(y);
-        
-        if (xset == yset) {
-            return;
-        } else {
-            parent[yset] = xset;
-            rank[xset]++;
-        }
-        count--;
-    }
-};
-
 class Solution {
 public:
+    class DSU{
+    public:
+        vector<int> parent;
+        vector<int> size;
+
+        DSU(int n){
+            parent.resize(n+1);
+            size.resize(n+1);
+            for(int i = 0; i < n+1; i++){
+                parent[i] = i; size[i] = 1;
+            }
+        }
+
+        int findParent(int node){
+            if(node == parent[node]) return node;
+            return parent[node] = findParent(parent[node]);
+        }
+
+        void unionBySize(int node1, int node2){
+            int p1 = findParent(node1);
+            int p2 = findParent(node2);
+            if(p1 == p2) return;
+            if(size[p1] < size[p2]){
+                parent[p1] = parent[p2];
+                size[p2] += size[p1];
+            }
+            else{
+                parent[p2] = parent[p1];
+                size[p1] = size[p2];
+            }
+        }
+    };
+
     vector<int> numIslands2(int m, int n, vector<vector<int>>& positions) {
-        int x[] = {-1, 1, 0, 0};
-        int y[] = {0, 0, -1, 1};
-        UnionFind dsu(m * n);
-        vector<int> answer;
+        DSU dsu(m*n);
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        for (auto& position : positions) {
-            int landPosition = position[0] * n + position[1];
-            dsu.addLand(landPosition);
+        int count = 0;
+        vector<int> ans;
+        vector<vector<int>> dir = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(auto node: positions){
+            int r = node[0];
+            int c = node[1];
+            if(visited[r][c] == true){
+                ans.push_back(count);
+                continue;
+            }
+            visited[r][c] = true;
+            count++;
 
-            for (int i = 0; i < 4; i++) {
-                int neighborX = position[0] + x[i];
-                int neighborY = position[1] + y[i];
-                int neighborPosition = neighborX * n + neighborY;
-                // If neighborX and neighborY correspond to a point in the grid and there is a land
-                // at that point, then merge it with the current land.
-                if (neighborX >= 0 && neighborX < m && neighborY >= 0 && neighborY < n &&
-                    dsu.isLand(neighborPosition)) {
-                    dsu.union_set(landPosition, neighborPosition);
+            for(auto d: dir){
+                int adjr = r+d[0];
+                int adjc = c+d[1];
+                if(adjr >= 0 && adjr < m && adjc >= 0 && adjc < n){
+                    if(visited[adjr][adjc] == true){
+                        int curNodeNo = r * n + c;
+                        int adjNodeNo = adjr * n + adjc;
+                        if(dsu.findParent(curNodeNo) != dsu.findParent(adjNodeNo)){
+                            count--;
+                            dsu.unionBySize(curNodeNo, adjNodeNo);
+                        }
+                    }
                 }
             }
-            answer.push_back(dsu.numberOfIslands());
+
+            ans.push_back(count);
         }
-        return answer;
+        return ans;
     }
 };
