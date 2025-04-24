b5dd16195cb882ff017c5207615ecdaafa3e71d5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-24 00:16:32 -0400

    Time: 4 ms (5.73%), Space: 11.9 MB (28.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
+    int func(int r, int c, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+        if(r == 0 && c == 0 && matrix[r][c] != 1) return 1;
+        if(r < 0 || c < 0) return 0;
+        
+        if(dp[r][c] != -1) return dp[r][c];
+        
+        if(matrix[r][c] == 1) return 0;
+        
+        int upPath = func(r-1, c, matrix, dp);
+        int leftPath = func(r, c-1, matrix, dp);
+        
+        return dp[r][c] = upPath+leftPath; 
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m));
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(i == 0 && j == 0 && obstacleGrid[i][j] != 1){
-                    dp[i][j] = 1;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < m; c++){
+                if(r == 0 && c == 0 && obstacleGrid[r][c] != 1) {
+                    dp[r][c] = 1;
                     continue;
                 }
-                if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+                if(obstacleGrid[r][c] == 1) {
+                    dp[r][c] = 0; 
                     continue;
                 }
                 int up = 0, left = 0;
-                if(i - 1 >= 0) up = dp[i-1][j];
-                if(j - 1 >= 0) left = dp[i][j-1];
-                dp[i][j] = up+left;
+                if(r-1 >= 0) up = dp[r-1][c];
+                if(c-1 >= 0) left = dp[r][c-1];
+                dp[r][c] = up + left;
             }
         }
         return dp[n-1][m-1];
