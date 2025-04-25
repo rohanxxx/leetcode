d46d21be0dce756ebef90b76692b3b4a2f601ba4
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-25 16:28:32 -0400

    Time: 77 ms (17.4%), Space: 13.4 MB (93.8%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -3,14 +3,16 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        //vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        vector<vector<int>> cur(m, vector<int>(m, 0));
+        vector<vector<int>> front(m, vector<int>(m, 0));
         //our one of the primary base case is ready
         //tc O(M*M) + O(N*M*M*9)
         //sc 0(N*M*M) -> improve the space complexity to O(M*M); 
         for(int j1 = 0; j1 < m; j1++){
             for(int j2 = 0; j2 < m; j2++){
-                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
-                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                if(j1 == j2) front[j1][j2] = grid[n-1][j1];
+                else front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
             }
         }
         //O(N*M*M*9)
@@ -28,16 +30,17 @@ public:
                                 val = INT_MIN;
                             }
                             else{
-                                val += dp[i+1][j1+dl1][j2+dl2];
+                                val += front[j1+dl1][j2+dl2];
                             }
-                            dp[i][j1][j2] = max(dp[i][j1][j2], val);
+                            cur[j1][j2] = max(cur[j1][j2], val);
                         }
                     }
                     //dp[i][j1][j2];
                 }
             }
+            front = cur;
         }
         
-        return dp[0][0][m-1];
+        return front[0][m-1];
     }
 };
