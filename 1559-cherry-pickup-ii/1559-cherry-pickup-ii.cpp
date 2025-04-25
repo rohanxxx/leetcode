9b464f856cf6da23f5781698c51b61250e9375b8
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-25 16:26:09 -0400

    Time: 106 ms (7.48%), Space: 21.1 MB (27.59%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
-    int func(int i, int j1, int j2, int& r, int& c, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
-        if(j1 < 0 || j2 < 0 || j1 >= c || j2 >= c){
-            return INT_MIN;
-        }
-        if(i == r-1){
-            if(j1 == j2) return dp[i][j1][j2] = grid[i][j1];
-            return dp[i][j1][j2] = grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2] != -1){
-            return dp[i][j1][j2];
-        }
-
-        for(int dl1 = -1; dl1 <= 1; dl1++){
-            for(int dl2 = -1; dl2 <= 1; dl2++){
-                int sum = 0;
-                
-                if(j1 == j2) sum = grid[i][j1];
-                else sum = grid[i][j1] + grid[i][j2]; 
-                
-                int val = func(i+1, j1+dl1, j2+dl2, r, c, grid, dp);
-                if(val == INT_MIN) val = 0;
-
-                dp[i][j1][j2] = max(dp[i][j1][j2], sum+val);
-            }
-        }
-        return dp[i][j1][j2];
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
-        return func(0, 0, m-1, n, m, grid, dp);
-        // return dp[][0][m-1];
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        //our one of the primary base case is ready
+        //tc O(M*M) + O(N*M*M*9)
+        //sc 0(N*M*M) -> improve the space complexity to O(M*M); 
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        //O(N*M*M*9)
+        for(int i = n-2; i >= 0; i--){
+            for(int j1 = 0; j1 < m; j1++){
+                for(int j2 = 0; j2 < m; j2++){
+                    for(int dl1 = -1; dl1 <= 1; dl1++){
+                        for(int dl2 = -1; dl2 <= 1; dl2++){
+                            int val;
+                            
+                            if(j1 == j2) val = grid[i][j1];
+                            else val = grid[i][j1] + grid[i][j2]; 
+                            
+                            if(j1+dl1 < 0 || j1+dl1 >= m || j2+dl2 < 0 || j2+dl2 >= m){
+                                val = INT_MIN;
+                            }
+                            else{
+                                val += dp[i+1][j1+dl1][j2+dl2];
+                            }
+                            dp[i][j1][j2] = max(dp[i][j1][j2], val);
+                        }
+                    }
+                    //dp[i][j1][j2];
+                }
+            }
+        }
+        
+        return dp[0][0][m-1];
     }
 };
