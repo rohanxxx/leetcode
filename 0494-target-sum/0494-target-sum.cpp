168482256f3396488afc8776fcce37aa0eb01788
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-25 18:26:32 -0400

    Time: 16 ms (53.04%), Space: 25.6 MB (30.27%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int totalSum;
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        totalSum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(n+1, vector<int>(2 * totalSum + 1, -1));
-        return func(nums, 0, 0, target, dp);
-    }
-
-    int func(vector<int>& nums, int i, int currentSum, int target, vector<vector<int>>& dp) {
-        if (i == nums.size()) {
-            if (currentSum == target) return 1;
+    int func(int i, int currentSum, int target, vector<int>& nums, vector<vector<int>>& dp){
+        if(i == nums.size()){
+            if(currentSum == target) return 1;
             else return 0;
-        } 
-        
-        if (dp[i][currentSum + totalSum] != -1) {
-            return dp[i][currentSum + totalSum];
         }
-
-        int add = func(nums, i + 1, currentSum + nums[i], target, dp);
-        int subtract = func(nums, i + 1, currentSum - nums[i], target, dp);
-
-        return dp[i][currentSum + totalSum] = add + subtract;
+        if(dp[i][currentSum+totalSum] != -1) return dp[i][currentSum+totalSum];
+        
+        int plus = func(i+1, currentSum+nums[i], target, nums, dp);
+        int minus = func(i+1, currentSum-nums[i], target, nums, dp);
+        
+        return dp[i][currentSum+totalSum] = plus + minus;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        totalSum = 0;
+        for(int num: nums){
+            totalSum += num;
+        }
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(1+totalSum*2, -1));
+        return func(0, 0, target, nums, dp);
     }
 };
