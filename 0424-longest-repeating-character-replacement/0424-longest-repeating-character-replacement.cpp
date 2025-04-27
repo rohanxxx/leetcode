5fe5b6799a8733c6162a6d8a7de5cce9af40c347
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-27 17:49:24 -0400

    Time: 0 ms (100%), Space: 10.9 MB (42.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left = 0, maxLen = 0, maxFreq = 0;
-        
+        int n = s.length(), left, maxFreq, maxLen;
+        left = maxFreq = maxLen = 0;
         vector<int> map(26, 0);
-        for(int right = 0; right < s.length(); right++){
-            maxFreq = max(maxFreq, ++map[s[right] - 'A']);
-            while(((right-left+1)-maxFreq) > k){
-                map[s[left++]-'A']--; //maxFreq = 0;
-                //for(int mp: map) maxFreq = max(maxFreq, mp);
+        for(int right = 0; right < n; right++){
+            map[s[right]-'A']++;
+            maxFreq = max(maxFreq, map[s[right]-'A']);
+            int curWindow = right-left+1;
+            while(curWindow-maxFreq > k){
+                map[s[left]-'A']--;
+                left++;
+                curWindow = right-left+1;
             }
-
-            if((right-left+1)-maxFreq <= k){ 
-                maxLen = max(maxLen, right-left+1);
+            curWindow = right-left+1;
+            if(curWindow-maxFreq <= k){
+                maxLen = max(maxLen, curWindow);
             }
         }
         return maxLen;
