0795705d4ca0c5f2afb796e21a2437d0de10e459
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-29 02:42:14 -0400

    Time: 4 ms (53.22%), Space: 18.9 MB (19%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -8,19 +8,17 @@ public:
             graph[it[1]].push_back(it[0]);
         }
         int countComponents = 0;
-        queue<pair<int, int>> q;
+        queue<int> q;
         for(int i = 0; i < n; i++){
             if(visited[i] == 0){
                 countComponents++;
-                q.push({i, -1});
+                q.push(i);
                 while(!q.empty()){
-                    int node = q.front().first;
-                    int parent = q.front().second;
-                    q.pop();
+                    int node = q.front(); q.pop();
                     for(auto adjNode: graph[node]){
                         if(visited[adjNode] == 0){
                             visited[adjNode] = 1;
-                            q.push({adjNode, parent});
+                            q.push(adjNode);
                         }
                     }
                 }
