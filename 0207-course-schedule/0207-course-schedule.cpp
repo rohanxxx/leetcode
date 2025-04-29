3db54b1e28a871296f3fc3ac1f5568b30e71f756
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-29 01:21:47 -0400

    Time: 0 ms (100%), Space: 20.2 MB (34.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,33 +2,31 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
+        vector<int> indegree(n);
         vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0);
-        // build the graph here
-        for(vector<int> prereq: prerequisites){
-            indegree[prereq[0]]++;
-            graph[prereq[1]].push_back(prereq[0]);
+        for(auto it: prerequisites){
+            indegree[it[0]]++;
+            graph[it[1]].push_back(it[0]);
         }
-        queue<int> q;
+        
+        int count = 0;
+        queue<int>q;
         for(int i = 0; i < n; i++){
             if(indegree[i] == 0){
-                // cout << "i: " << i << endl;
                 q.push(i);
             }
         }
-        // vector<int> topoSort;
-        int count = 0;
+
         while(!q.empty()){
-            int node = q.front(); q.pop();
-            // topoSort.push_back(node);
             count++;
-            for(int nextNode: graph[node]){
-                if(--indegree[nextNode] == 0){
-                    q.push(nextNode);
+            int node = q.front(); q.pop();
+            for(int adjNode: graph[node]){
+                if(--indegree[adjNode] == 0){
+                    q.push(adjNode);
                 }
             }
         }
-        // if(topoSort.size() == n) return true;
+
         return count == n;
     }
 };
