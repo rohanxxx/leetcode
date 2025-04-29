24668cb3d955ffecb8893da71df723fe21473274
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-29 02:41:11 -0400

    Time: 6 ms (40.19%), Space: 18.9 MB (19%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph, int node, vector<bool>& visited){
-        if(!visited[node]){
-            visited[node] = true;
-            for(int i = 0; i < graph[node].size(); i++){
-                dfs(graph, graph[node][i], visited);
-            }
-        }
-    }
-
     int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> visited(n);
         vector<vector<int>> graph(n);
-        for(int i = 0; i < edges.size(); i++){
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+        for(auto it: edges){
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
         }
-
-        int count = 0;
-        vector<bool> visited(n);
+        int countComponents = 0;
+        queue<pair<int, int>> q;
         for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                count++;
-                visited[i] = true;
-                
-                for(int j = 0; j < graph[i].size(); j++){
-                    dfs(graph, graph[i][j], visited);
+            if(visited[i] == 0){
+                countComponents++;
+                q.push({i, -1});
+                while(!q.empty()){
+                    int node = q.front().first;
+                    int parent = q.front().second;
+                    q.pop();
+                    for(auto adjNode: graph[node]){
+                        if(visited[adjNode] == 0){
+                            visited[adjNode] = 1;
+                            q.push({adjNode, parent});
+                        }
+                    }
                 }
             }
         }
-
-        return count;
+        return countComponents;
     }
 };
