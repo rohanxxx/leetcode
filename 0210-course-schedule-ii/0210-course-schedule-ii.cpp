948bc74788072beb926f1e971de044b39249e097
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-29 01:31:04 -0400

    Time: 12 ms (9.92%), Space: 18.9 MB (27.09%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<int> indegree(n, 0);
+        vector<int> indegree(n);
         vector<vector<int>> graph(n);
-        for(auto prereq: prerequisites){
-            indegree[prereq[0]]++;
-            graph[prereq[1]].push_back(prereq[0]);
+        for(auto it: prerequisites){
+            indegree[it[0]]++;
+            graph[it[1]].push_back(it[0]);
         }
-
         queue<int> q;
         for(int i = 0; i < n; i++){
             if(indegree[i] == 0) q.push(i);
@@ -18,14 +17,13 @@ public:
         while(!q.empty()){
             int node = q.front(); q.pop();
             topoSort.push_back(node);
-            for(int nextNode: graph[node]){
-                if(--indegree[nextNode] == 0){
-                    q.push(nextNode);
+            for(int adjNode: graph[node]){
+                if(--indegree[adjNode] == 0){
+                    q.push(adjNode);
                 }
             }
         }
-        if((int)topoSort.size() == n) return topoSort;
-        
-        return {};
+        if(topoSort.size() != n) return {};
+        return topoSort;
     }
 };
