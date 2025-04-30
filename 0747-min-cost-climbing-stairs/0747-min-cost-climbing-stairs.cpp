b7916e82ade031f0a81a29cb5d6fa5f89142486e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-29 23:15:31 -0400

    Time: 0 ms (100%), Space: 17.6 MB (50.76%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -22,18 +22,18 @@ public:
         if(n == 2) return min(cost[0], cost[1]);
         
         vector<int> dp(n+1, 0);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int prev0 = cost[0];
+        int prev1 = cost[1];
         for(int i = 2; i <= n; i++){
-            int take1 = dp[i-1];
-            int take2 = dp[i-2];
+            int take1 = prev0;
+            int take2 = prev1;
             
             int curCost;
             if(i == n) curCost = 0;
             else curCost = cost[i];
-
-            dp[i] = curCost + min(take1, take2);
+            prev0 = prev1;
+            prev1 = curCost + min(take1, take2);
         }
-        return dp[n];
+        return prev1;
     }
 };
