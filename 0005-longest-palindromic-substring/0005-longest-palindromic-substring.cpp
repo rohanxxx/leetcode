b1359e25aa4808da74543665c70a4c4646224a87
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-30 00:03:41 -0400

    Time: 19 ms (60.36%), Space: 18.7 MB (50.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,24 +3,24 @@ public:
     /*
     bababdddddba
     */
-    bool isPalindrom(int left, int right, string& s){
-        while(left <= right){
-            if(s[left] != s[right]) return false;
-            left++; right--;
+    string isPalindrom(int left, int right, string& s){
+        while(left >= 0 && right < s.length() && s[left] == s[right]){
+            left--; right++;
         }
-        return true;
+        return s.substr(left+1, right-1-left+1-1);
     }
 
     string longestPalindrome(string s) {
         int n = s.length();
         string ans = "";
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(isPalindrom(i, j, s)){
-                    if(ans.length() < j-i+1){
-                        ans = s.substr(i, (j-i+1));
-                    }
-                }
+            string odd = isPalindrom(i, i, s);
+            if(ans.size() < odd.size()){
+                ans = odd;
+            }
+            string even = isPalindrom(i,i+1, s);
+            if(ans.size() < even.size()){
+                ans = even;
             }
         }
 
