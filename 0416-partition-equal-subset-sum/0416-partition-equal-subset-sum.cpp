b6ec80cf3636e7a5ea42b2796e125ac9315e9a43
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-30 02:55:13 -0400

    Time: 141 ms (30.83%), Space: 110.6 MB (14.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
+    bool func(vector<int>& nums, vector<vector<int>>& dp, int i, int target){
+        if(target == 0) return true;
+        if(i == 0) return (nums[i] == target);
+        if(dp[i][target] != -1) return dp[i][target];
+        
+        bool notTake = func(nums, dp, i-1, target);
+        bool take = false;
+
+        if(nums[i] <= target){
+            take = func(nums, dp, i-1, target-nums[i]);
+        }
+
+        return dp[i][target] = take || notTake;
+    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), sum = 0;
-        //O(N)
+        
         for(int i = 0; i < n; i++) sum += nums[i];
         if(sum % 2 == 1) return false;
         
         int target = sum / 2;
-        //vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
-        //O(target+1) ->space complexity
-        vector<int> prev(target+1, 0);
-        vector<int> cur(target+1, 0);
-        //O(N)
-        for(int i = 0; i < n; i++){
-            prev[0] = true;
-        }
-        if(nums[0] <= target){
-            prev[nums[0]] = true;
-        }
-        
-        //O(N*(target+1))
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j <= target; j++){
-                bool notTake = prev[j];
-                bool take = false;
-                if(nums[i] <= j){
-                    take = prev[j-nums[i]];
-                }
-                cur[j] = take || notTake;
-            }
-            prev = cur;
-        }
-
-        return prev[target];
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        return func(nums, dp, n-1, target);
     }
 };
