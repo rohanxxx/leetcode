9aa8d3c6cd28e35d9ef79d0071d75437ea9bae48
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-01 00:37:40 -0400

    Time: 3 ms (8.31%), Space: 9.4 MB (8.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int func(int i, int j, vector<vector<int>>& dp){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i == 0 || j == 0){
-            return 0;
-        }
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = 1+func(i, j-1, dp) + func(i-1, j, dp);
+        int left = func(i, j-1, dp);
+        int up = func(i-1, j, dp);
+        return dp[i][j] = left+up;
     }
     int uniquePaths(int m, int n) {
-        if(m == 1 and n == 1) return 1;
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return func(m-1, n-1, dp)+1;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                int left = 0, up = 0;
+                if(j-1 >= 0) left = dp[i][j-1];
+                if(i-1 >= 0) up = dp[i-1][j];
+                dp[i][j] = left+up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
