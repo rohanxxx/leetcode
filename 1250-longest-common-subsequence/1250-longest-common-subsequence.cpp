5f3d7892896b4d0bb9c3314e6cb15329291bcb27
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-01 02:17:43 -0400

    Time: 33 ms (31.82%), Space: 26.7 MB (77.2%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int func(int i, int j, string& text1, string& text2, vector<vector<int>>& dp){
-        if(i == 0 || j == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(text1[i-1] == text2[j-1]) return dp[i][j] = 1 + func(i-1, j-1, text1, text2, dp);
-        return dp[i][j] = max(func(i-1, j, text1, text2, dp), func(i, j-1, text1, text2, dp));
+    int func(int i, int j, string& t1, string& t2){
+        if(i == 0 || j == 0){
+            return 0;
+        }
+        
+        if(t1[i-1] == t2[j-1]) return 1 + func(i-1, j-1, t1, t2);
+        int notTake1 = func(i-1, j, t1, t2);
+        int notTake2 = func(i, j-1, t1, t2);
+        return max({notTake1, notTake2});
     }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-        
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        
-        return func(n, m, text1, text2, dp);
+    int longestCommonSubsequence(string t1, string t2) {
+        int n = t1.length();
+        int m = t2.length();
+        //vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> prev(m+1, 0);
+        for(int i = 1; i <= n; i++){
+            vector<int> cur(m+1, 0);
+            for(int j = 1; j <= m; j++){
+                if(t1[i-1] == t2[j-1]) {
+                    cur[j] = 1 + prev[j-1];
+                    continue;
+                }
+                int notTake1 = prev[j];
+                int notTake2 = cur[j-1];
+                cur[j] = max({notTake1, notTake2});
+            }
+            prev = cur;
+        }
+        return prev[m];
     }
 };
