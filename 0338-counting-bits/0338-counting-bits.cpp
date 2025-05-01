f07c095c653deec264ed151db9901cecc3e88c4b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-04-30 23:58:17 -0400

    Time: 7 ms (5.66%), Space: 11.2 MB (30.18%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        //SC: O(n)
         vector<int> ans;
+        //TC: O(n*32)
         for(int j = 0; j <= n; j++){
             int count = 0;
             for(int i = 0; i < 32; i++){
