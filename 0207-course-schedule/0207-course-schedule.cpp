e253118f269081523b252f23cfc1ba9d1721c292
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-02 23:41:09 -0400

    Time: 7 ms (51.97%), Space: 20 MB (50.1%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
+/*
+    a is dependent on b. in order to take course a we should always take course b first
+    indegree vector:    0 1
+                        0 1
+    BFS 
+    first we would push the indegree's that has 0 dependencies
+    queue: 0
+    once we pop the queue and get 0 we mark it visited then we go to the outdegree node which we 
+    traverse 
+    like this 
+    for(i->adjList){
+        before we push it to queue 
+        we are going to decrease 1 by 1 and then if it is 0 the indegree then we push it to the queue
+    }
+*/
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<int> indegree(n);
+        vector<int> indegrees(n, 0);
+        //build the graph
         vector<vector<int>> graph(n);
         for(auto it: prerequisites){
-            indegree[it[0]]++;
             graph[it[1]].push_back(it[0]);
+            indegrees[it[0]]++;
         }
-        
-        int count = 0;
-        queue<int>q;
+
+        queue<int> q;
         for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
+            if(indegrees[i] == 0) q.push(i);
         }
 
         while(!q.empty()){
-            count++;
+            n--;
             int node = q.front(); q.pop();
-            for(int adjNode: graph[node]){
-                if(--indegree[adjNode] == 0){
+            for(auto adjNode: graph[node]){
+                if(--indegrees[adjNode] == 0){
                     q.push(adjNode);
                 }
             }
         }
 
-        return count == n;
+        return n==0;
     }
 };
