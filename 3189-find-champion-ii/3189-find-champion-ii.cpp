4a5d5b2f00b83649b8647e4589c9509b3e7d1b23
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-04 15:27:50 -0400

    Time: 49 ms (16.74%), Space: 104.9 MB (23.01%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
+    //Total TC: O(N+E)
+    //Total SC: O(N)
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<bool> inDegreeNodes(n);
-        for(auto& edge: edges){
-            inDegreeNodes[edge[1]] = true;
+        //SC: O(N)
+        vector<int> indegree(n);
+        //O(E)
+        for(auto it: edges){
+            indegree[it[1]]++;
         }
 
-        int champion;
-        int count = 0;
-
+        int champion = -1;
+        int championCount = 0;
+        //O(N)
         for(int i = 0; i < n; i++){
-            if(!inDegreeNodes[i]){
+            if(indegree[i] == 0){
                 champion = i;
-                count++;
+                championCount++;
             }
         }
+        
+        //O(1)
+        if(championCount == 1){
+            return champion;
+        }
 
-        if(count == 1) return champion;
         return -1;
     }
 };
