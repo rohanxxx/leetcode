dd43f2095b7fca296a7d6def02c4c6a35426fd48
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-05 23:16:46 -0400

    Time: 10 ms (77.85%), Space: 17.3 MB (67.57%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,17 +7,42 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+ /*
+ root = [3,5,1,6,2,0,8,null,null,7,4]
+                                3
+                            5       1
+                        6       2 0     8
+                              7  4
+p = 5, q = 1
+
+
+if(left && right) return curNode;
+if(root == p || root == q) return root;
+return NULL;
+
+root = [3,5,1,6,2,0,8,null,null,7,4]
+                            3
+                        5       1
+                    6     2   0   8
+                        7   4
+p = 5, q = 4
+
+ */
 class Solution {
 public:
-    TreeNode* dfs(TreeNode* node, TreeNode* p, TreeNode* q){
-        if(!node) return NULL;
-        TreeNode* left = dfs(node->left, p, q);
-        TreeNode* right = dfs(node->right, p, q);
-
-        if(left && right) return node;
-        if(node == p || node == q) return node;
-        if(left) return left;
-        if(right) return right;
+    TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == NULL) return NULL;
+        TreeNode* leftTree = dfs(root->left, p, q);
+        TreeNode* rightTree = dfs(root->right, p, q);
+        if((p != NULL && root->val == p->val) || (q != NULL && root->val == q->val)){
+            return root;
+        }
+        if(leftTree && rightTree) {
+            return root;
+        }
+        if(leftTree) return leftTree;
+        if(rightTree) return rightTree;
         return NULL;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
