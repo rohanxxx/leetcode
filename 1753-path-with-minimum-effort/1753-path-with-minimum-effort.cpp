8af8f5cb045b0b8732a39aaf36ab9a2958c272f5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-09 17:27:40 -0400

    Time: 260 ms (8.56%), Space: 69.4 MB (8.02%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -15,25 +15,33 @@
     {maxEffort, r, c}
     priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq
 */
+
+//O(NlogN+4E)
+//O(N*M + N*M + 8)
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();         //row size
         int m = heights[0].size();      //col size
-        
-        //if(n == 1 && m == 1) return 0;
 
+        //SC:O(N*M)
+        //TC:O(N*M)
         vector<vector<int>> effort(n, vector<int>(m, INT_MAX));
 
+        //SC: O(N*M)
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({0, 0, 0}); effort[0][0] = 0;
 
+        //SC: O(8)
         vector<vector<int>> dir = {{-1,0}, {1,0}, {0,-1}, {0, 1}};
+
+        //O(N)
         while(!pq.empty()){
             int minEffortSoFar = pq.top()[0];
             int curRow = pq.top()[1];
             int curCol = pq.top()[2];
 
+            //O(logN)
             pq.pop();
             //chcek if we reached out destination
             if(curRow == n-1 && curCol == m-1){
@@ -41,6 +49,7 @@ public:
                 return minEffortSoFar;
             }
 
+            //(O(4E))
             for(auto d: dir){
                 int adjr = curRow + d[0];
                 int adjc = curCol + d[1];
