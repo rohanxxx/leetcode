237095fb6829f9681e26bcdda54f37521bb364d5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-09 23:46:28 -0400

    Time: 663 ms (19.09%), Space: 295.7 MB (14.24%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,50 @@
+/*
+*/
+
+class Solution {
+public:
+    //vector<int> ans;
+    void merge(vector<int>& nums, int left, int mid, int right){
+        int p1 = left;
+        int p2 = mid+1;
+        vector<int> temp;
+        while(p1 <= mid && p2 <= right){
+            if(nums[p1] <= nums[p2]){
+                temp.push_back(nums[p1]);
+                p1++;
+            }
+            else{
+                temp.push_back(nums[p2]);
+                p2++;
+            }
+        }
+
+        while(p1 <= mid){
+            temp.push_back(nums[p1]);
+            p1++;
+        }
+        while(p2 <= right){
+            temp.push_back(nums[p2]);
+            p2++;
+        }
+
+
+        //we are transferring all the sorted cells to the original array
+        for(int i = left; i <= right; i++){
+            nums[i] = temp[i-left];
+        }
+    }
+    void mergeSort(vector<int>& nums, int left, int right){
+        if(left >= right) return;
+        int mid = (left+right)/2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid+1, right);
+        merge(nums, left, mid, right);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        //ans.resize(n);
+        mergeSort(nums, 0, n-1);
+        return nums;
+    }
+};
