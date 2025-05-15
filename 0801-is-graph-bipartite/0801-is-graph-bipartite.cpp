b404dee20d98494158488066e452979c28c6e33d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-14 20:49:43 -0400

    Time: 0 ms (100%), Space: 17.6 MB (19.25%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,33 +1,74 @@
+/*
+graph is undirected
+                            0      1    2       3
+        Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
+        Output: false
+
+        0 - 1
+        | \ | 
+        3 - 2
+
+        0 - 1
+        |   | 
+        3 - 2
+
+        0, 0
+        pair<nodeValue, mark>
+        queue<pair<int, int>>
+
+        q-> (0,0) pop, (1,1)pop, (2,1) (3,1) 
+
+        if(0 is visited) then check if(0 is mapped the opposit of 1's mapping) or else it is false
+        else q.push()
+*/
 class Solution {
 public:
-    bool bfs(vector<vector<int>>& graph, vector<int>& color, int node){
-        color[node] = 0;
-        queue<int> q; q.push(node);
+    bool bfs(int node, vector<int>& visited, vector<int>& map, vector<vector<int>>& graph){
+        queue<pair<int, int>> q;
+
+        //push node 0 to q
+        q.push({node, map[node]});
+        visited[node] = 1;
+
+        //then do bfs traversal and checking if it bipartite
+        //TC: O(N)
         while(!q.empty()){
-            // int parentNodeColor = color[node];
-            int curNode = q.front(); q.pop();
-            int parentNodeColor = color[curNode];
-            for(int nextNode: graph[curNode]){
-                if(color[nextNode] == -1){
-                    color[nextNode] = !parentNodeColor;
-                    q.push(nextNode);
+            int curNode = q.front().first;
+            int curMark = q.front().second;
+            q.pop();
+
+            for(auto adjNode: graph[curNode]){
+                //if not visited
+                if(visited[adjNode] == 0){
+                    visited[adjNode] = 1;
+                    map[adjNode] = (!curMark);
+                    q.push({adjNode, !curMark});
                 }
-                else if(color[nextNode] == parentNodeColor){
+                else if(visited[adjNode] == 1 && map[adjNode] == curMark){
                     return false;
                 }
             }
         }
+
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
+        //declare queue, vector visited, vector<int> map
         int n = graph.size();
-        vector<int> color(n,-1);
+        //edge case
+        if(n == 0) return true;
+        vector<int> visited(n, 0);  //0 means false
+        vector<int> map(n, 0);      //initially everything is marked 0
         for(int i = 0; i < n; i++){
-            if(color[i] != -1) continue;
-            if(!bfs(graph, color, i)){
-                return false;
+            int node = i;
+            if(visited[node] == 0){
+                visited[node] = 1; //means true
+                if(bfs(node, visited, map, graph) == false){
+                    return false;
+                }
             }
         }
+
         return true;
     }
 };
