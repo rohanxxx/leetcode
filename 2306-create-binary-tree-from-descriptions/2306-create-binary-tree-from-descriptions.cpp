74e4189860a1c20f9a70ab3c49152f453843c153
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-16 16:04:00 -0400

    Time: 283 ms (29.48%), Space: 301.6 MB (22.89%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -9,39 +9,75 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ /*
+    Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+    Output: [50,20,80,15,17,19]
+
+                          50
+                         /  \
+                       20   80
+                       /\   /
+                     15  17 19
+
+    unordered_map<int, TreeNode*> tree;
+    map[20] = new TreeNode(20) -> left = new TreeNode(15)               15 17 20
+    map[15] = treeNode(15)                                      
+    map[17] = treeNode(17)                                              
+    map[50] = if the value 20 doesn't exist then create new TreeNode(20) otherwise point it to map[20] 20 has 
+
+    if(!hasparent[map[id]]) return the map[id];
+
+    return NULL?
+ */
+
+ /*
+ */
+
+void connectTree(int& parent, int& child, int& isLeft, unordered_map<int, TreeNode*>& treeMap){
+    if(treeMap.find(child) == treeMap.end()){
+        treeMap[child] = new TreeNode(child);
+    }
+    if(isLeft == 1){
+        treeMap[parent]->left = treeMap[child];
+    }
+    else{
+        treeMap[parent]->right = treeMap[child];
+    }
+} 
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> mp;
-        unordered_map<int, bool> hasParent;
+        //first we will construct the tree
+        //SC: O(N+N) = O(2N) = O(N)
+        unordered_map<int, TreeNode*> treeMap;
+        unordered_map<int, int> hasParent;
 
-        for(auto d: descriptions){
-            int parentVal = d[0];
-            int childVal = d[1];
-            int isLeft = d[2];
-
-            TreeNode* parentNode = NULL;
-            TreeNode* childNode = NULL;
-
-            if(mp.find(parentVal) == mp.end()) parentNode = new TreeNode(parentVal);
-            else parentNode = mp[parentVal];
-
-            if(mp.find(childVal) == mp.end()) childNode = new TreeNode(childVal);
-            else childNode = mp[childVal];
-
-            if(isLeft) parentNode->left = childNode;
-            else parentNode->right = childNode;
-
-            hasParent[childVal] = true;
-            mp[childVal] = childNode;
-            mp[parentVal] = parentNode;
+        //TC: O(N)
+        for(auto it: descriptions){
+            int parent = it[0];
+            int child = it[1];
+            int isLeft = it[2];
+            hasParent[child] = 1;
+            //if it already doesn't exist create a new treeNode
+            if(treeMap.find(parent) == treeMap.end()){
+                treeMap[parent] = new TreeNode(parent);
+                connectTree(parent, child, isLeft, treeMap);
+            }
+            else{
+                connectTree(parent, child, isLeft, treeMap);
+            }
         }
-
-        TreeNode* rootNode = NULL;
-        for(auto& [nodeVal, node]: mp){
-            if(!hasParent[nodeVal]) rootNode = node;
+        TreeNode* root = NULL;
+        //here we will identify the parent
+        //TC: O(N)
+        for(auto it: descriptions){
+            int parent = it[0];
+            if(hasParent[parent] == 0){
+                root = treeMap[parent];
+                return root;
+            }
         }
-
-        return rootNode;
+        return root;
     }
 };
