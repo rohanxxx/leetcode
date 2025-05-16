4e6436a66b11c38691d0a9882bde973cc0071d01
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-16 18:06:37 -0400

    Time: 19 ms (32.08%), Space: 38.7 MB (26.79%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -4,10 +4,10 @@ public:
         int totalCount = 0;
         int currSum = 0;
         unordered_map<int, int> frequency;
-
+        frequency[0] = 1;
         for(int i = 0; i < nums.size(); i++){
             currSum += nums[i];
-            if(currSum == goal) totalCount++;
+            //if(currSum == goal) totalCount++;
             if(frequency.find(currSum-goal) != frequency.end()){
                 totalCount += frequency[currSum-goal];
             }
