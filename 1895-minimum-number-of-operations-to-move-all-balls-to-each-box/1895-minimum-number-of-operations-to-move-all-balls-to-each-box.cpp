62d81498891ecda7bb90ba5ece01e0d4e775d348
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-18 15:32:48 -0400

    Time: 2 ms (67.88%), Space: 12.6 MB (23.03%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -23,17 +23,25 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> answer(boxes.length());
-        for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
-            // If the current box contains a ball, calculate the number of moves
-            // for every box.
-            if (boxes[currentBox] == '1') {
-                for (int newPosition = 0; newPosition < boxes.length();
-                     newPosition++) {
-                    answer[newPosition] += abs(newPosition - currentBox);
-                }
-            }
+        int n = boxes.size();
+        vector<int> answer(n, 0), answer2(n,0), answer3(n,0);
+        int ballsToLeft = 0, movesToLeft = 0;
+        int ballsToRight = 0, movesToRight = 0;
+
+        // Single pass: calculate moves from both left and right
+        for (int i = 0; i < n; i++) {
+            // Left pass
+            answer[i] += movesToLeft;
+            ballsToLeft += boxes[i] - '0';
+            movesToLeft += ballsToLeft;
+
+            // Right pass
+            int j = n - 1 - i;
+            answer[j] += movesToRight;
+            ballsToRight += boxes[j] - '0';
+            movesToRight += ballsToRight;
         }
+
         return answer;
     }
 };
