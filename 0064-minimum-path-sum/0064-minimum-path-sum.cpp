7d93158fa6b14ee73933f8249c2afd00af06236b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-05-18 20:42:52 -0400

    Time: 17 ms (5.89%), Space: 16.4 MB (38.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,25 +1,53 @@
+/*
+    go: top left to right
+    moves: down / right
+
+    Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+    Output: 7   
+
+    [1,3,1]
+    [1,5,1]
+    [4,2,1]
+
+
+*/
 class Solution {
 public:
-int func(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& dp){
-	if(r == 0 && c == 0) return grid[r][c];
-	if(r < 0 || c < 0) return INT_MAX;
-	
-	if(dp[r][c] != -1) return dp[r][c];
-	
-//r, c-1
-	//r-1, c
-	int left = func(r, c-1, grid, dp);
-	int up = func(r-1, c, grid, dp);
-	int curPathSum = grid[r][c] + min(left, up);
-	return dp[r][c] = curPathSum;
-}
-
-
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-	int n = grid[0].size();
-	vector<vector<int>> dp(m, vector<int>(n, -1));
-	return func(m-1, n-1, grid, dp);
+        if(grid.size() == 1 && grid[0].size() == 1) return grid[0][0];
 
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>>dist(grid.size(), vector<int>(grid[0].size(), INT_MAX));
+
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0],{0,0}}); // path sum, row, col
+
+        while(!pq.empty())
+        {
+            int d = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            if(row == n-1 && col == m-1) return dist[row][col];
+            pq.pop();
+            int rrow = row; 
+            int rcol = col + 1; // for right column index
+            if(rcol >= 0 && rcol < m && d + grid[rrow][rcol] < dist[rrow][rcol])
+            {
+                dist[rrow][rcol] = d + grid[rrow][rcol];
+                pq.push({dist[rrow][rcol], {rrow, rcol}});
+            }
+            int drow = row + 1; // for down row index
+            int dcol = col; 
+            if(drow >= 0 && drow < n && d + grid[drow][dcol] < dist[drow][dcol])
+            {
+                dist[drow][dcol] = d + grid[drow][dcol];
+                pq.push({dist[drow][dcol], {drow, dcol}});
+            }
+        }
+        return 0;
     }
-};
+}; 
