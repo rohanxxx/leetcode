712665b8979fc4bbd29c3b88eb386ce61fea12ad
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-02 11:00:17 -0400

    Time: 936 ms (10.39%), Space: 495.1 MB (7.61%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    //TC: O(N+T)
+    //SC: O(52)
     int lengthAfterTransformations(string s, int t) {
         int mod = 1e9+7;
         vector<int> curFreq(26, 0);
+        //O(n)
         for(auto c: s) curFreq[c-'a']++;
+        //O(26*t)
         for(int i = 0; i < t; i++){
             vector<int> nextFreq(26, 0);
+            //O(26)
             for(int j = 0; j < 26; j++){
                 int nextIndex = j+1;
                 if(nextIndex == 26 && curFreq[j] > 0){
