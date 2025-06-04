cd77a4173d3195421b099721b7132117581b05c1
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-04 11:01:23 -0400

    Time: 0 ms (100%), Space: 17.8 MB (18.82%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
@@ -0,0 +1,46 @@
+/*
+    [-1,-1,-1,-1,-1,-1]6
+    [-1,-1,-1,-1,-1,-1]5
+    [-1,-1,-1,-1,-1,-1]4
+    [-1,35,-1,-1,13,-1]3
+    [-1,-1,-1,-1,-1,-1]2
+    [-1,15,-1,-1,-1,-1]1
+
+    if i is odd
+    then we do (i*n+1) + n - j = 12
+    if i is even 
+    then we do i*n+1 + colSize
+*/
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>> &board) {
+        int n = board.size();
+        vector<pair<int, int>> cells(n * n + 1);
+        int label = 1;
+        vector<int> columns(n);
+        iota(columns.begin(), columns.end(), 0);
+        for (int row = n - 1; row >= 0; row--) {
+            for (int column : columns) {
+                cells[label++] = {row, column};
+            }
+            reverse(columns.begin(), columns.end());
+        }
+        vector<int> dist(n * n + 1, -1);
+        queue<int> q;
+        dist[1] = 0;
+        q.push(1);
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int next = curr + 1; next <= min(curr + 6, n * n); next++) {
+                auto [row, column] = cells[next];
+                int destination = board[row][column] != -1 ? board[row][column] : next;
+                if (dist[destination] == -1) {
+                    dist[destination] = dist[curr] + 1;
+                    q.push(destination);
+                }
+            }
+        }
+        return dist[n * n];
+    }
+};
