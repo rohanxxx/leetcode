46d5bb64ad00a26c35bc44570b540d904d60b9a4
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-05 19:39:22 -0400

    Time: 0 ms (100%), Space: 8.4 MB (35.86%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -4,12 +4,8 @@ public:
         int nums1 = 0;
         int nums2 = 0;
         for(int i = 1; i <= n; i++){
-            if(i % m) {
-                nums1 += i;
-            }
-            else {
-                nums2 += i;
-            }
+            if(i % m) nums1 += i;
+            else nums2 += i;
         }
         return nums1 - nums2;
     }
