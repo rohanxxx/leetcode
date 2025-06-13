ecd73856bd32f43274e30e30a3ef5ddeef32d7f9
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-13 12:25:38 -0400

    Time: 0 ms (100%), Space: 9.6 MB (33.45%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -5,6 +5,19 @@
     [1,1,1]
     [0,0,0]
 
+    [ 0,-1,0]
+    [ 2, 0,1]
+    [-1, 1,1]
+    [ 0, 2,0]
+
+    [ 0, 0,0]
+    [ 1, 0,1]
+    [ 0, 1,1]
+    [ 0, 1,0]
+
+    1 becomes 0 -> -1
+    0 becomes 1 -> 2
+
     [0,0,0]
     [1,0,1]
     [0,1,1]
@@ -14,14 +27,12 @@
 class Solution {
 
 public:
+    //TC: O(3*N*M)
     //SC: O(2*N*M)
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size();
         int m = board[0].size();
-        
-        //TC: O(N*M)
-        //SC: O(N*M)
-        vector<vector<int>> state(n, vector<int>(m, 0));
+    
         //8 directional search
         //SC: O(2*8)
         vector<vector<int>> dir = {{0,1}, {0,-1}, {1, 0}, {-1, 0}, 
@@ -36,22 +47,27 @@ public:
                     int adjr = it[0]+i;
                     int adjc = it[1]+j;
                     if(adjr >= 0 && adjc >= 0 && adjr < n && adjc < m){
-                        count += board[adjr][adjc];
+                        if(board[adjr][adjc] == -1 || board[adjr][adjc] == 1) count++;
                     }
                 }
                 //if current cell dead or alive
                 //TC: O(1)
                 if(board[i][j] == 1){
-                    if(count == 2 || count == 3) state[i][j] = 1;
-                    if(count < 2) state[i][j] = 0;
-                    if(count > 3) state[i][j] = 0;
+                    //if(count == 2 || count == 3) state[i][j] = 1;
+                    if(count < 2 || count > 3) board[i][j] = -1;
                 }
                 else{
-                    if(count == 3) state[i][j] = 1;
+                    if(count == 3) board[i][j] = 2;
                 }
             }
         }
 
-        board = state;
+        //TC: (N*M)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == -1) board[i][j] = 0;
+                else if(board[i][j] == 2) board[i][j] = 1;
+            }
+        }
     }
 };
