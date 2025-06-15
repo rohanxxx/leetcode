2a03b7b338e70e7c8218b55641ea5707c5925de4
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-15 13:10:46 -0400

    Time: 1684 ms (5.21%), Space: 13.5 MB (5.21%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.cpp
+++ 0360-sort-transformed-array/0360-sort-transformed-array.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
         vector<int> ans;
+        unordered_map<int, int> map;
         for(auto& num: nums){
-            ans.push_back(a*num*num+b*num+c);
+            map[a*num*num+b*num+c]++;
         }
-        sort(ans.begin(), ans.end());
+        for(int i = -1e6; i <= 1e6; i++){
+            if(map.find(i) != map.end()){
+                for(int j = 0; j < map[i]; j++){
+                    ans.push_back(i);
+                }
+            }
+        }
         return ans;
     }
 };
