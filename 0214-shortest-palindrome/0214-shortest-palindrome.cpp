72ea1c6a100beda7e1e17f783f036e350b0691e5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-18 12:58:26 -0400

    Time: 3 ms (71.01%), Space: 12.1 MB (94.59%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -8,13 +8,23 @@
     a a a c e c a a
 
     617 580 811
+
+    You’re computing:
+	•	prefixMap: Rolling hash of s[0..i] (left-to-right)
+	•	suffixMap: Rolling hash of the reversed s[0..i] (built in reverse order using powers of base)
+
+So at each i, you’re effectively asking:
+
+Is s[0..i] == reverse(s[0..i])?
+
+If yes, s[0..i] is a palindrome — update index.
 */
 class Solution {
 public:
     string shortestPalindrome(string s) {
         long long prefixMap = 0, suffixMap = 0;
-        long long base = 26, power = 1;
-
+        long long base = 29, power = 1;
+        
         int index = -1;
 
         int mod = 1e9 + 7;
