d87df8157a7367133041db2d468e7d14d2a649a8
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-24 12:24:56 -0400

    Time: 110 ms (16.51%), Space: 82.2 MB (26.91%) - LeetHub

---
+++ 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        int n = messages.size();
+        //SC: O(N)
+        unordered_map<string, int> wordCount;
+        //builds the wordCount map
+
+        //TC: O(N*M)
+        for(int i = 0; i < n; i++){
+            istringstream ss(messages[i]);
+            string word = "";
+            while(ss >> word){
+                wordCount[senders[i]]++;
+            }
+        }
+
+        int maxCount = INT_MIN;
+        string ans = "";
+        //TC: O(N)
+        for(auto& [sender, count]: wordCount){
+            //cout << "sender: " << sender << " count: " << count << endl;
+            if(maxCount <= count){
+                //cout << "sender: " << sender << " count: " << count << endl;
+                if(maxCount == count){
+                    if(ans < sender) ans = sender;
+                }
+                else{
+                    ans = sender;
+                }
+                maxCount = count;
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+    ["nT","y Mx p iD Lo",
+    "xVg Ak IyQ u MG lmS Hq q e mb","N Qf i y",
+    "Zrs sbc tKE GGx xM D","wo UEZ uPf Xi XjE",
+    "tL y n","ac Up cy znC uw I","Dx vVW qa vxm",
+    "spz D MEB pq","ktu buy","Mau Y ob J gqD jVZ B pXS",
+    "q dB bdC Zd p dpr Np N","Qqx O umB j iO hM eMW Nt",
+    "T Vo C pnD r Dso PP qA q","u s vPx c LeH ju z",
+    "W Dee VQG a e"]
+
+    ["dhOKiI",
+    "OfOpYKCNku",
+    "zyh",
+    "hr",
+    "o",
+    "jDklz",
+    "lre",
+    "Zpi",
+    "doorY","OfOpYKCNku","doorY","b","wjAC","fBsUhoLpG","Zpi","wjAC","Owwx"]
+*/
