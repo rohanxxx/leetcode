340526283b2e55c456b8f908c930280ee3c82e09
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-06-30 02:28:20 -0400

    Time: 3 ms (71.03%), Space: 14.7 MB (51.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    //TC: O(N)
+    //SC: O(N)
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            int dif = target-nums[i];
-            if(mp.find(dif) != mp.end()){
-                return {mp[dif], i};
+        //{key->nums[j], val-> index of nums[j]}
+        //SC: O(N) at max in worstcase
+        unordered_map<int, int> hashmap;
+
+        int n = nums.size();
+        //TC: O(N*1*1) = O(N)
+        for(int i = 0; i < n; i++){
+            int numsJ = target-nums[i];
+            //checks whether numsJ exists in my hashmap or not
+            //if exists then go inside the if loop
+            if(hashmap.find(numsJ) != hashmap.end()){
+                return {hashmap[numsJ], i};
             }
-            mp[nums[i]] = i; 
+            hashmap.insert({nums[i], i});
         }
-        return {};
+        return vector<int>();
     }
 };
