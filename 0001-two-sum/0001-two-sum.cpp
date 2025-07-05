f19e5a7d2a488f3d31d0cf7e30983bf66d33e52a
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-05 19:08:33 -0400

    Time: 0 ms (100%), Space: 14.9 MB (36.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    //Tc: O(N)
+    //TC: O(N)
     //SC: O(N)
     vector<int> twoSum(vector<int>& nums, int target) {
-        //first declare all the necessary variables
+        //SC: O(N)
         unordered_map<int, int> hashmap;
-
         int n = nums.size();
-        //0->n-1
         //TC: O(N)
         for(int i = 0; i < n; i++){
-            int numsJ = target-nums[i];
-            //if numsJ key exists in the hashmap
+            int numsJ = target - nums[i];
+            //check numsJ key exists in the hashmap or not
             //TC: O(1)
             if(hashmap.find(numsJ) != hashmap.end()){
                 return {hashmap[numsJ], i};
             }
+            //TC: O(1)
             hashmap.insert({nums[i], i});
         }
-
         return {};
     }
 };
