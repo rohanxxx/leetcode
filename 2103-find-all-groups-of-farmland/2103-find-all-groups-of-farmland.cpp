73d8589160237969b2527627f007aac0a219a48e
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-07 12:58:10 -0400

    Time: 136 ms (22.33%), Space: 154.7 MB (9.95%) - LeetHub

--- 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.cpp
@@ -3,51 +3,58 @@
 */
 class Solution {
 public:
+    void bfs(int &i, int &j, vector<vector<int>>& dir, vector<vector<int>>& land, 
+                        vector<vector<int>>& ans, vector<vector<int>>& visited)
+    {
+        int n = land.size();
+        int m = land[0].size();
+        if(land[i][j] == 1 && visited[i][j] == 0){
+            queue<pair<int, int>> q; q.push({i,j});
+                    
+            visited[i][j] = 1;
+
+            vector<int> curLand;
+            curLand.push_back(i);
+            curLand.push_back(j);
+
+            pair<int, int> prev;
+            while(!q.empty()){
+                int r = q.front().first;
+                int c = q.front().second;
+                        
+                prev = {r,c};
+                        
+                q.pop();
+
+                for(auto& it: dir){
+                    int adjr = it[0] + r;
+                    int adjc = it[1] + c;
+                    if(adjr < n && adjc < m){
+                        if(land[adjr][adjc] == 1 && visited[adjr][adjc] == 0){
+                            q.push({adjr, adjc});
+                            visited[adjr][adjc] = 1;
+                        }
+                    } 
+                }
+            }
+            curLand.push_back(prev.first);
+            curLand.push_back(prev.second);
+                    
+            ans.push_back(curLand);
+        }
+    }
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         int n = land.size();
         int m = land[0].size();
-        //cout << "n: " << n << endl;
-        //cout << "m: " << m << endl;
+
         vector<vector<int>> ans;
         vector<vector<int>> dir = {{1,0}, {0,1}};
         vector<vector<int>> visited(n, vector<int>(m, 0));
         
+        //TC: O(N*M)
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(land[i][j] == 1 && visited[i][j] == 0){
-                    queue<pair<int, int>> q; q.push({i,j});
-                    
-                    visited[i][j] = 1;
-
-                    vector<int> curLand;
-                    curLand.push_back(i);
-                    curLand.push_back(j);
-
-                    pair<int, int> prev;
-                    while(!q.empty()){
-                        int r = q.front().first;
-                        int c = q.front().second;
-                        
-                        prev = {r,c};
-                        
-                        q.pop();
-
-                        for(auto& it: dir){
-                            int adjr = it[0] + r;
-                            int adjc = it[1] + c;
-                            if(adjr < n && adjc < m){
-                                if(land[adjr][adjc] == 1 && visited[adjr][adjc] == 0){
-                                    q.push({adjr, adjc});
-                                    visited[adjr][adjc] = 1;
-                                }
-                            } 
-                        }
-                    }
-                    curLand.push_back(prev.first);
-                    curLand.push_back(prev.second);
-                    
-                    ans.push_back(curLand);
-                }
+                bfs(i, j, dir, land, ans, visited);
             }
         }
 
