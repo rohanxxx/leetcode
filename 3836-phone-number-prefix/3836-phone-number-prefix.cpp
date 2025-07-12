ec53826dd0992056419ff59e42749bd584343d2f
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-12 01:16:43 -0400

    Time: 2 ms (70.83%), Space: 30.5 MB (60.42%) - LeetHub

--- 3836-phone-number-prefix/3836-phone-number-prefix.cpp
+++ 3836-phone-number-prefix/3836-phone-number-prefix.cpp
@@ -1,9 +1,20 @@
+/*
+    ["034","329","318","31"]
+    
+    034, 31, 318, 329
+*/
 class Solution {
 public:
     bool phonePrefix(vector<string>& numbers) {
-        std::sort(numbers.begin(), numbers.end());
-        for (size_t i{1}; i < numbers.size(); ++i) {
-            if (numbers[i].starts_with(numbers[i - 1])) return false;
+        sort(numbers.begin(), numbers.end());
+        /*for(auto& it: numbers){
+            cout << it << " ";
+        }
+        cout << endl;*/
+        int n = numbers.size();
+        for(int i = 1; i < n; i++){
+            string prefix = numbers[i].substr(0, numbers[i-1].size());
+            if(numbers[i-1] == prefix) return false;
         }
         return true;
     }
