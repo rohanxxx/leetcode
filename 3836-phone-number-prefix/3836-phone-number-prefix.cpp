ba72599aecc51d942fc9c8935594f67f849014aa
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-12 01:06:02 -0400

    Time: 0 ms (100%), Space: 30.5 MB (60.42%) - LeetHub

--- 3836-phone-number-prefix/3836-phone-number-prefix.cpp
+++ 3836-phone-number-prefix/3836-phone-number-prefix.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     bool phonePrefix(vector<string>& numbers) {
-        int n = numbers.size();
-
-        //TC: O(N*N*m)
-        //SC: O(1)
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                string prefix = "";
-                if(numbers[i].size() <= numbers[j].size()){
-                    prefix = numbers[j].substr(0, numbers[i].size());
-                    if(numbers[i] == prefix){
-                        return false;
-                    }
-                }
-                else{
-                    prefix = numbers[i].substr(0, numbers[j].size());
-                    if(numbers[j] == prefix){
-                        return false;
-                    }
-                }
-            }
+        std::sort(numbers.begin(), numbers.end());
+        for (size_t i{1}; i < numbers.size(); ++i) {
+            if (numbers[i].starts_with(numbers[i - 1])) return false;
         }
-
         return true;
     }
 };
