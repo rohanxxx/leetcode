4558dd932c2fd52f85a13dbb7886486d56b44344
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-15 12:00:16 -0400

    Time: 147 ms (46.31%), Space: 177.5 MB (15.48%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.cpp
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.cpp
@@ -3,14 +3,17 @@ private:
     vector<int> v;
 public:
     SparseVector(vector<int> &nums) {
+        //TC: O(N)
         v = nums;
     }
     
     // Return the dotProduct of two sparse vectors
+    //Overall -> TC: O(N)
+    //Overall -> SC: O(N+N) = O(2N)
     int dotProduct(SparseVector& vec) {
         int ans = 0;
         int n = vec.v.size();
-        
+        //TC: O(N)
         for(int i = 0; i < n; i++){
             ans += (v[i]*vec.v[i]);
         }
