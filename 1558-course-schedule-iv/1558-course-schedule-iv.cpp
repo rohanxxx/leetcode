39067cf0b59e6563f1ef904a354517bb1b72f64d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-21 14:09:42 -0400

    Time: 55 ms (53.64%), Space: 72.6 MB (45.31%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -4,6 +4,8 @@ public:
         int n = numCourses;
         vector<int> indegree(n,0);
         vector<vector<int>> adjList(n);
+
+        //TC: O(N+E)
         for(auto& prereq: prerequisites){
             int u = prereq[0];
             int v = prereq[1];
@@ -12,6 +14,7 @@ public:
         }
 
         queue<int> q;
+        //TC: O(N)
         for(int i = 0; i < n; i++){
             if(indegree[i] == 0){
                 q.push(i);
@@ -20,11 +23,15 @@ public:
 
         //build the prerequisite
         vector<unordered_set<int>> prereqs(n);
+
+        //TC: O(N*N*N)
         while(!q.empty()){
             int node = q.front(); q.pop();
+            //TC: O(N*N)
             for(auto& adj: adjList[node]){
                 prereqs[adj].insert(node);
                 //getting all the prereqs
+                //TC: O(N+E)
                 for(auto& prereq: prereqs[node]){
                     prereqs[adj].insert(prereq);
                 }
@@ -36,6 +43,8 @@ public:
         }
 
         vector<bool> ans;
+
+        //TC: O(N)
         for(auto& query: queries){
             int u = query[0];
             int v = query[1];
