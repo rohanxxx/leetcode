0b38a463e47e26a360ca2f49fca64db39a6b335d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-07-30 17:44:39 -0400

    Time: 39 ms (87.85%), Space: 65.4 MB (78.38%) - LeetHub

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dfs(int node, int parent, vector<bool>& hasApple, vector<vector<int>>& graph){
+        int childTime = 0, totalTime = 0;
+        for(auto& child: graph[node]){
+            if(child == parent) continue;
+            childTime = dfs(child, node, hasApple, graph);
+            if(childTime || hasApple[child]) {
+                totalTime += childTime + 2;
+            }
+        }
+        return totalTime;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>> graph(n);
+        for(auto& edge: edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        return dfs(0, -1, hasApple, graph);
+    }
+};
