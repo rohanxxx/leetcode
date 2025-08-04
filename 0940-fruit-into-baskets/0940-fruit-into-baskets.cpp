fb561477dd01305348e5454153fc3da59c29b5ed
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-08-03 21:07:16 -0400

    Time: 35 ms (75.57%), Space: 75.1 MB (57.1%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
+    /*
+         0 1 2 3 4 5 6 7 8
+        [1,0,1,4,1,4,1,2,3]
+    */
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> mp;
-        int left, right;
-        for(left = 0, right = 0; right < fruits.size(); right++){
-            mp[fruits[right]]++;
-            if(mp.size() > 2){
-                mp[fruits[left]]--;
-                if(mp[fruits[left]] == 0){
-                    mp.erase(fruits[left]);
+        int n = fruits.size(), count = 0, maxPick = 0;
+        //SC: O(3)
+        //{key, index}
+        unordered_map<int, int> fruitMap;
+
+        //TC: O(3*N)
+        for(int i = 0; i < n; i++){
+            count++;
+            fruitMap[fruits[i]] = i;
+            if(fruitMap.size() == 3){
+                //reset it here
+                int prevPick = fruits[i-1];
+                int curPick = fruits[i];
+                //TC: O(3)
+                for(auto it: fruitMap){
+                    if(it.first == prevPick || it.first == curPick) continue;
+                    //before you erase update the count
+                    count = i-(fruitMap[it.first]);
+                    //erase the fruitMap that is not consecutive
+                    //O(1)
+                    fruitMap.erase(it.first);
+                    break;
                 }
-                left++;
             }
+            //O(1)
+            maxPick = max(maxPick, count);
         }
-        return right-left;
+
+        return maxPick;
     }
 };
