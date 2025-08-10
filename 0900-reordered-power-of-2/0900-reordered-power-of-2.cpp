f09de6769a4c7d0b19294519b1a497980ac2176d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-08-10 12:48:44 -0400

    Time: 0 ms (100%), Space: 9 MB (62.83%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+
+        for(int i = 0; i <= 29; i++){
+            string str = to_string(1 << i);
+            sort(str.begin(), str.end());
+            if(str == s) return true;
+        }
+
+        return false;
+    }
+};
