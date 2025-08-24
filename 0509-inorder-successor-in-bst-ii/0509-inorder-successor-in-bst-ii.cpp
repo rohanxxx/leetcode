12e3cceff96d0ec5f11720517df5ec80870c8f94
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-08-24 11:23:05 -0400

    Time: 17 ms (58.45%), Space: 19.3 MB (2.11%) - LeetHub

--- 0509-inorder-successor-in-bst-ii/0509-inorder-successor-in-bst-ii.cpp
+++ 0509-inorder-successor-in-bst-ii/0509-inorder-successor-in-bst-ii.cpp
@@ -15,26 +15,16 @@ public:
     have direct access to the node, but not the root,
     each node will have the reference to it's parent
 */
-/*class Solution {
+class Solution {
 public:
-    void dfs(Node* node, Node* ans, int& target, unordered_map<int, int>& visited){
+    Node* ans;
+    void dfs(Node* node, int& target, unordered_map<int, int>& visited){
         if(!node) return;
         //if visited then return immediately
         if(visited.find(node->val) != visited.end()){
             return;
         }
-        visited[node->val] = 1;*/
-        /*if(node->val < target) return;
-
-        if(ans == NULL){
-            ans = node;
-        }
-        else{
-            if(ans->val > node->val){
-                ans = node;
-            }
-        }*/
-/*
+        visited[node->val] = 1;
         // successor must be strictly greater
         if (node->val > target) {
             if (ans == NULL) {
@@ -45,26 +35,27 @@ public:
                 }
             }
         }
-        dfs(node->parent, ans, target, visited);
-        dfs(node->left, ans, target, visited);
-        dfs(node->right, ans, target, visited);
+        dfs(node->parent, target, visited);
+        dfs(node->left, target, visited);
+        dfs(node->right, target, visited);
     }
     Node* inorderSuccessor(Node* node) {
         if(!node) return NULL;
 
-        Node* ans = NULL;
+        ans = NULL;
         unordered_map<int, int> visited;
 
         visited[node->val] = 1;
-        dfs(node->parent, ans, node->val, visited);
-        dfs(node->right,  ans, node->val, visited);
+        dfs(node->parent, node->val, visited);
+        dfs(node->right, node->val, visited);
         return ans;
     }
-};*/
+};
 
-class Solution {
+/*class Solution {
 public:
-    void dfs(Node* node, Node*& ans, int target, unordered_map<int, int>& visited){
+    Node* ans;
+    void dfs(Node* node, int& target, unordered_map<int, int>& visited){
         if(!node) return;
         if(visited.find(node->val) != visited.end()) return;
         visited[node->val] = 1;
@@ -80,20 +71,21 @@ public:
             }
         }
 
-        dfs(node->parent, ans, target, visited);
-        dfs(node->left,   ans, target, visited);
-        dfs(node->right,  ans, target, visited);
+        dfs(node->parent, target, visited);
+        dfs(node->left, target, visited);
+        dfs(node->right, target, visited);
     }
 
     Node* inorderSuccessor(Node* node) {
         if(!node) return NULL;
 
-        Node* ans = NULL;
+        ans = NULL;
         unordered_map<int, int> visited;
 
         visited[node->val] = 1;
-        dfs(node->parent, ans, node->val, visited);
-        dfs(node->right,  ans, node->val, visited);
+        dfs(node->parent, node->val, visited);
+        dfs(node->right, node->val, visited);
         return ans;
     }
 };
+*/
