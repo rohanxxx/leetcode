c143cfe308bd5463db32a95b7aadbeb4292f7b03
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-08-23 23:21:42 -0400

    Time: 0 ms (100%), Space: 60.2 MB (23.29%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    /*
-        nums arr 
-        delete one element from it
-
-        return the size of the longest non-empty subarray containing only 1's
-
-        return 0 if there is no such subarray
-    */
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> leftSum(n, 0), rightSum(n, 0);
 
-        int left = 0, right = 0;
-        
-        leftSum[0] = nums[0];
-        rightSum[n-1] = nums[n-1];
-        for(int i = 1; i < n; i++){
-            if(nums[i] == 1){
-                leftSum[i] = leftSum[i-1]+nums[i];
-            }
-        }
-        for(int i = n-2; i >= 0; i--){
-            if(nums[i] == 1){
-                rightSum[i] = rightSum[i+1]+nums[i];
-            }
-        }
+        int maxCount = 0;
+        int prevCount = 0, curCount = 0;
 
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0 && i-1 >= 0 && i+1 < n){
-                count = max(count, leftSum[i-1]+rightSum[i+1]);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                curCount++;
+            } 
+            else { // nums[i] == 0
+                if (i > 0 && nums[i - 1] == 0) {
+                    prevCount = 0;
+                } 
+                else {
+                    prevCount = curCount;
+                }
+                curCount = 0;
             }
-            else{
-                count = max(count, leftSum[i]);
+
+            if (prevCount + curCount > maxCount) {
+                maxCount = prevCount + curCount;
             }
         }
 
-        if(count == n) return count-1;
-        return count;
+        if (maxCount == n) {
+            return n - 1;
+        } 
+        else {
+            return maxCount;
+        }
     }
 };
