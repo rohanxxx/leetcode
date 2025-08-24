6f935e03c265bc00e888599d73c35839aeca1931
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-08-23 23:24:40 -0400

    Time: 0 ms (100%), Space: 59.9 MB (85.53%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -7,29 +7,27 @@ public:
         int prevCount = 0, curCount = 0;
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
-                curCount++;
-            } 
-            else { // nums[i] == 0
-                if (i > 0 && nums[i - 1] == 0) {
-                    prevCount = 0;
-                } 
-                else {
-                    prevCount = curCount;
-                }
+            if (nums[i] == 0 && i + 1 < n && nums[i + 1] == 0) {
+                // two zeros in a row: break the bridge
                 curCount = 0;
+                prevCount = 0;
             }
-
-            if (prevCount + curCount > maxCount) {
-                maxCount = prevCount + curCount;
+            else if (nums[i] == 0 && i + 1 < n && nums[i + 1] == 1) {
+                // single zero before a ones run: save left run, reset current
+                prevCount = curCount;
+                curCount = 0;                 // <-- minimal fix #1
             }
+            else if (nums[i] == 1) {
+                curCount++;
+                // combine left run (prevCount) + current run (curCount)
+                if (prevCount + curCount > maxCount) {   // <-- minimal fix #2
+                    maxCount = prevCount + curCount;
+                }
+            }
+            // (if nums[i] == 0 and i+1 == n, do nothing; result is already tracked)
         }
 
-        if (maxCount == n) {
-            return n - 1;
-        } 
-        else {
-            return maxCount;
-        }
+        if (maxCount == n) return maxCount - 1; // all ones: must delete one
+        return maxCount;
     }
 };
