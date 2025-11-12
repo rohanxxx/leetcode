9799d46621bf12284a8b3e06ef79840e6fe080c3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2025-11-11 22:11:49 -0500

    Time: 0 ms (100%), Space: 17.6 MB (43%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int val = 0;
+        //TC: O(N), SC: O(1)
         for(string& ops: operations){
             if(ops == "--X" || ops == "X--") val--;
             if(ops == "++X" || ops == "X++") val++;
