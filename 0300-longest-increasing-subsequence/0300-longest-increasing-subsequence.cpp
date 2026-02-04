7a89fc7e501667b4b309a3526259a9e3975cdc34
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-04 13:31:23 -0500

    Time: 299 ms (28.96%), Space: 296.5 MB (20.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        vector<int> subArr;
-        subArr.push_back(nums[0]);
+/*
+        [10,9,2,5,3,7,101,18]
+        [2,3,7,101]
 
-        for(int i = 1; i < nums.size(); i++){
-            int num = nums[i];
-            if(num > subArr.back()){
-                subArr.push_back(num);
-            }
-            else{
-                int index = lower_bound(subArr.begin(), subArr.end(), num) - subArr.begin();
-                subArr[index] = nums[i];
+        func(i, prev_i)
+        take = func(i+1, i);
+        notTake = func(i+1, prev_i);
+*/
+    int func(int i, int prev_i, vector<int>& nums, vector<vector<int>>& dp){
+        if(i >= nums.size()) return 0;
+        if(dp[i][prev_i+1] != -1) return dp[i][prev_i+1];
+
+        int take = 0, notTake = 0;
+        if(prev_i == -1 || nums[i] > nums[prev_i]){
+            take = 1+func(i+1, i, nums, dp);
+        }
+        notTake = func(i+1, prev_i, nums, dp);
+        return dp[i][prev_i+1] = max(take, notTake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i = n; i >= 0; i--){
+            for(int prev_i = i-1; prev_i >= -1; prev_i--){
+                if(i == n){
+                    dp[i][prev_i+1] = 0;
+                    continue;
+                }
+                int take = 0, notTake = 0;
+                if(prev_i == -1 || nums[i] > nums[prev_i]){
+                    take = 1+dp[i+1][i+1];
+                }
+                notTake = dp[i+1][prev_i+1];
+                dp[i][prev_i+1] = max(take, notTake);
             }
         }
-        return subArr.size();
+        //func(0, -1, nums, dp);
+        return dp[0][-1+1];
     }
 };
