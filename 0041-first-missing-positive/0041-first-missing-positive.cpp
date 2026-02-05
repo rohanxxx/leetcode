7fdefef1f5fa473e1cdf3c27475b1903e5a4a6e2
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-05 14:39:35 -0500

    Time: 3 ms (53.41%), Space: 55.1 MB (62.26%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,15 +1,52 @@
+/*
+    Input: nums = [1,2,0]
+    Output: 3
+    
+    [1,2,0]
+    [-1, -2, 0]
+    
+    
+    Input: nums = [3,4,-1,1]
+    Output: 2
+    
+    [3,4,-1,1]
+    [3,4,0,1]
+    [-3,4,0,-1]
+    
+    Input: nums = [7,8,9,11,12]
+    Output: 1
+
+*/
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        vector<int> seen(nums.size()+1, 0);
-        for(auto num: nums){
-            if(num > 0 && num <= nums.size()){
-                seen[num]++;
+        int n = nums.size();
+
+        // marks negative / zero to 0's
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 1) nums[i] = 0;
+        }
+
+        // marks indexes to negative (presence marking)
+        for (int i = 0; i < n; i++) {
+            int val = abs(nums[i]);
+            if (val >= 1 && val <= n) {
+                int index = val - 1;
+
+                if (nums[index] > 0) {
+                    nums[index] *= -1;
+                } else if (nums[index] == 0) {
+                    nums[index] = -(n + 1);  // special negative marker
+                }
             }
         }
-        for(int i = 1; i <= nums.size(); i++){
-            if(!seen[i]) return i;
+
+        // find first missing
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) return i + 1;
         }
-        return nums.size()+1;
+
+        return n + 1;
     }
 };
