c34b6473309299abdf3d23757d80d36542a4fa82
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-08 01:44:05 -0500

    Time: 3 ms (24.8%), Space: 15.5 MB (16.78%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,44 +1,59 @@
+/*
+Input: board = 
+[
+    ["X","X","X","X"],
+    ["X","O","O","X"],
+    ["X","X","O","X"],
+    ["X","O","X","X"]
+]
+
+Output: 
+[
+    ["X","X","X","X"],
+    ["X","X","X","X"],
+    ["X","X","X","X"],
+    ["X","O","X","X"]
+]
+*/
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
 
-        queue<pair<int, int>> q;
-
-        //TC -> O(n*m)
-        //mark the edge islands with 'E'
+        queue<vector<int>> q;
+        //O(N*M)
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(i == 0 || j == 0 || i == n-1 || j == m-1){
-                    if(board[i][j] == 'O'){
-                        q.push({i, j});
-                    }
+                if(board[i][j] == 'O' && (i == 0 || j == 0 || i == n-1 || j == m-1)){
+                    q.push({i,j});
+                    board[i][j] = 'E';
                 }
             }
         }
 
         vector<vector<int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        //now mark region that connects to the edges "E"
+
+        //TC: O(4*N*M)
         while(!q.empty()){
-            pair<int, int> p = q.front(); q.pop();
-            int curRow = p.first;
-            int curCol = p.second;
-            
-            board[curRow][curCol] = 'E';
-            
-            for(auto it: dir){
-                int adjr = curRow + it[0];
-                int adjc = curCol + it[1];
+            auto it = q.front(); q.pop();
+            int r = it[0];
+            int c = it[1];
+
+            //TC: O(4)
+            for(auto d: dir){
+                int adjr = d[0] + r;
+                int adjc = d[1] + c;
                 if(adjr >= 0 && adjr < n && adjc >= 0 && adjc < m){
                     if(board[adjr][adjc] == 'O'){
                         q.push({adjr, adjc});
+                        board[adjr][adjc] = 'E';
                     }
                 }
             }
         }
 
-        //TC -> O(n*m)
-        //convert the regions to the water land
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'O'){
@@ -47,8 +62,6 @@ public:
             }
         }
 
-        //TC -> O(n*m)
-        //mark the edge islands back to '0'
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'E'){
@@ -56,6 +69,5 @@ public:
                 }
             }
         }
-        return;
     }
 };
