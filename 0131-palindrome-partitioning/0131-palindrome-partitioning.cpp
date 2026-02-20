17b9634d971920ab26b0ca6b536eac0c09c40e08
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-20 07:49:23 -0500

    Time: 24 ms (96.79%), Space: 53.3 MB (92.9%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     bool isPalindrome(string& s, int left, int right){
-        while(left < right){
-            if(s[left++] != s[right--]) return false;
+        while(left <= right){
+            if(s[left] != s[right]) return false;
+            left++; right--;
         }
         return true;
     }
-    void backtrack(vector<vector<string>>& ans, string& s, int left, vector<string>& cur){
-        if(left == s.length()){
-            ans.push_back(cur);
+    void backtrack(int left, string& s, vector<string>& str, vector<vector<string>>& ans){
+        if(left >= s.length()){
+            ans.push_back(str);
             return;
         }
+        
         for(int right = left; right < s.length(); right++){
             if(isPalindrome(s, left, right)){
-                cur.push_back(s.substr(left, right-left+1));
-                backtrack(ans, s, right+1, cur);
-                cur.pop_back();
+                str.push_back(s.substr(left, right-left+1));
+                backtrack(right+1, s, str, ans);
+                str.pop_back();
             }
         }
+        
     }
     vector<vector<string>> partition(string s) {
-        vector<string> cur;
+        vector<string> str;
         vector<vector<string>> ans;
-        backtrack(ans, s, 0, cur);
+        backtrack(0, s, str, ans);
         return ans;
     }
 };
