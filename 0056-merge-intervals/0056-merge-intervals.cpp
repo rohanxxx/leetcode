c071980290dd9ac1208948358384dec0dec4d9ba
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-28 10:22:06 -0500

    Time: 7 ms (39.89%), Space: 25 MB (8.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    //TC: O(NlogN + N)
+    //SC: O(N)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //TC: O(NLogN)
+        //SC: O(N)
         sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        
-        for(int i = 1; i < intervals.size(); i++){
-            if(ans.back()[1] >= intervals[i][0]){
-                if(ans.back()[1] <= intervals[i][1]){
-                    ans.back()[1] = intervals[i][1];
-                }
+
+        vector<vector<int>> mergedPairs;
+
+        //TC: O(N)
+        for(auto it: intervals){
+            if(mergedPairs.empty()){
+                mergedPairs.push_back({it[0], it[1]});
                 continue;
             }
-            ans.push_back(intervals[i]);
+            if(mergedPairs.back()[1] >= it[0]){
+                mergedPairs.back()[1] = max(it[1], mergedPairs.back()[1]);
+            }
+            else{
+                mergedPairs.push_back({it[0], it[1]});
+            }
         }
 
-        return ans;
+        return mergedPairs;
     }
 };
