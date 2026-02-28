2b585e2909054b1347f70393d75f5cb392edf749
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-02-28 09:58:52 -0500

    Time: 101 ms (5.06%), Space: 42.1 MB (5.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int m, n;
-    void vanishIslands(vector<vector<char>>& grid, int r, int c){
-        if(grid[r][c] == '0') return;
-        grid[r][c] = '0';
-        vector<vector<int>> cood = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
-        for(int i = 0; i < cood.size(); i++){
-            int R = r+cood[i][0];
-            int C = c+cood[i][1];
-            if(R >= 0 && R < grid.size() && C >= 0 && C < grid[0].size()){
-                vanishIslands(grid, R, C);
+    vector<vector<int>> dir = {{0,1}, {1, 0}, {0, -1}, {-1, 0}};
+    void bfs(int r, int c, vector<vector<char>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<vector<int>> q; q.push({r,c});
+        while(!q.empty()){
+            int curRow = q.front()[0];
+            int curCol = q.front()[1];
+            q.pop();
+            for(auto d: dir){
+                int adjr = d[0]+curRow;
+                int adjc = d[1]+curCol;
+                if(adjr >= 0 && adjr < n && adjc >= 0 && adjc < m){
+                    if(grid[adjr][adjc] == '1'){
+                        //marks visited
+                        grid[adjr][adjc] = '0';
+                        q.push({adjr, adjc});
+                    }
+                }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-
         int count = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    vanishIslands(grid, i, j);
+                    //marks as visited
+                    grid[i][j] = '0';
+                    bfs(i, j, grid);
                 }
             }
         }
