770b5b8969ea1c540947b54d75569ba584eab4a0
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-03 12:21:38 -0500

    Time: 0 ms (100%), Space: 10.5 MB (27.47%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,81 +1,30 @@
-/*
-    nums = [7,2,5,10,8], k = 2 
-    25, 23, 18, 24
-*/
-
 class Solution {
 public:
-    unordered_map<string, long long> dp;
-
-    long long dfs(int i, int m, vector<int>& nums) {
-        if (m == 1) {
-            long long sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
+    int countSplits(vector<int>& nums, int hi){
+        int splits = 1;
+        long long sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            if(sum > hi){
+                splits++;
+                sum = nums[i];
             }
-            return sum;
         }
-
-        string key = to_string(i) + "," + to_string(m);
-        if (dp.count(key)) return dp[key];
-
-        long long res = LLONG_MAX;
-        long long curSum = 0;
-
-        for (int j = i; j <= nums.size() - m; j++) {
-            curSum += nums[j];
-            long long maxSum = max(curSum, dfs(j + 1, m - 1, nums));
-            res = min(res, maxSum);
-
-            if (curSum > res) break;
-        }
-
-        dp[key] = res;
-        return res;
+        return splits;
     }
+    int splitArray(vector<int>& nums, int k) {
+        //TC: O(N+N) = O(2N) = O(N)
+        int lo = *max_element(nums.begin(), nums.end()); //gives max element
+        int hi = accumulate(nums.begin(), nums.end(), 0); //gives sum of arr
 
-    int splitArray(vector<int>& nums, int m) {
-        dp.clear();
-        return dfs(0, m, nums);
+        //TC: O(log(sum))
+        while(lo <= hi){
+            int mid = lo+(hi-lo)/2;
+            int count = countSplits(nums, mid);
+            if(count > k) lo = mid+1;
+            else hi = mid-1;
+        }
+
+        return lo;
     }
 };
-/*
-class Solution {
-public:
-    
-    int memo[1001][51];
-    
-    int largestMinSumSplit(vector<int> &prefixSum, int curIndex, int subArrayCount){
-        int n = prefixSum.size()-1;
-
-        if(memo[curIndex][subArrayCount] != -1){
-            return memo[curIndex][subArrayCount];
-        }
-        if(subArrayCount == 1){
-            return memo[curIndex][subArrayCount] = prefixSum[n]-prefixSum[curIndex];
-        }
-
-        int minimumLargestSumSplit = INT_MAX;
-        for(int i = curIndex; i <= n - subArrayCount; i++){
-            int firstSplitSum = prefixSum[i+1] - prefixSum[curIndex];
-            int largestSplitSum = max(firstSplitSum, largestMinSumSplit(prefixSum, i+1, subArrayCount-1));
-            minimumLargestSumSplit = min(minimumLargestSumSplit, largestSplitSum);  
-
-            if(firstSplitSum >= minimumLargestSumSplit){
-                break;
-            }
-        }
-
-        return memo[curIndex][subArrayCount] = minimumLargestSumSplit;
-    }
-
-    int splitArray(vector<int>& nums, int k) {
-        memset(memo, -1, sizeof(memo));
-        int n = nums.size();
-        vector<int> prefixSum (n+1, 0);
-        for(int i = 1; i < n+1; i++){
-            prefixSum[i] += (prefixSum[i-1]+nums[i-1]);
-        }
-        return largestMinSumSplit(prefixSum, 0, k);
-    }
-};*/
