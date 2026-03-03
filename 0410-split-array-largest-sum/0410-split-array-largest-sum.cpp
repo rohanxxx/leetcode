90930fa9725f30a56012b3e8c5a7255924d9f371
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-03 11:02:19 -0500

    Time: 2029 ms (4.04%), Space: 20.7 MB (6.57%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,3 +1,45 @@
+/*
+    nums = [7,2,5,10,8], k = 2 
+    25, 23, 18, 24
+*/
+
+class Solution {
+public:
+    unordered_map<string, long long> dp;
+
+    long long dfs(int i, int m, vector<int>& nums) {
+        if (m == 1) {
+            long long sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+            }
+            return sum;
+        }
+
+        string key = to_string(i) + "," + to_string(m);
+        if (dp.count(key)) return dp[key];
+
+        long long res = LLONG_MAX;
+        long long curSum = 0;
+
+        for (int j = i; j <= nums.size() - m; j++) {
+            curSum += nums[j];
+            long long maxSum = max(curSum, dfs(j + 1, m - 1, nums));
+            res = min(res, maxSum);
+
+            if (curSum > res) break;
+        }
+
+        dp[key] = res;
+        return res;
+    }
+
+    int splitArray(vector<int>& nums, int m) {
+        dp.clear();
+        return dfs(0, m, nums);
+    }
+};
+/*
 class Solution {
 public:
     
@@ -36,4 +78,4 @@ public:
         }
         return largestMinSumSplit(prefixSum, 0, k);
     }
-};
+};*/
