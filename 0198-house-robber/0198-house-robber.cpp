633f7563e694b15cf21091b81de0cc7b63dc1142
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-04 11:38:25 -0500

    Time: 0 ms (100%), Space: 10.8 MB (21.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,28 @@
+/*
+            0 1 2 3
+    nums = [1,2,3,1]
+*/
 class Solution {
 public:
-    int func(int i, vector<int>& nums, vector<int>& dp){
+    int dfs(int i, vector<int>& nums, vector<int>& dp){
         if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
-        int take = nums[i] + func(i-2, nums, dp);
-        int notTake = func(i-1, nums, dp);
+        if(dp[i] != INT_MIN) return dp[i];
+
+        int take = nums[i] + dfs(i-2, nums, dp);
+        int notTake = dfs(i-1, nums, dp);
+
         return dp[i] = max(take, notTake);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return func(n-1, nums, dp);
+        vector<int> dp(n, 0);
+        for(int i = 0; i < n; i++) {
+            int take = nums[i];
+            int notTake = 0; 
+            if(i-2 >= 0) take += dp[i-2];
+            if(i-1 >= 0) notTake += dp[i-1];
+            dp[i] = max(take, notTake);
+        }
+        return dfs(n-1, nums, dp);
     }
 };
