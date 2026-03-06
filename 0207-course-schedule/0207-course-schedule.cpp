8dff86c8ff95d32933663a732629275ddd7a2444
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-06 13:41:47 -0500

    Time: 12 ms (16.66%), Space: 20 MB (41.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-/*
-    a is dependent on b. in order to take course a we should always take course b first
-    indegree vector:    0 1
-                        0 1
-    BFS 
-    first we would push the indegree's that has 0 dependencies
-    queue: 0
-    once we pop the queue and get 0 we mark it visited then we go to the outdegree node which we 
-    traverse 
-    like this 
-    for(i->adjList){
-        before we push it to queue 
-        we are going to decrease 1 by 1 and then if it is 0 the indegree then we push it to the queue
-    }
-*/
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        vector<int> indegrees(n, 0);
-        //build the graph
-        vector<vector<int>> graph(n);
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> graph(numCourses);
         for(auto it: prerequisites){
-            graph[it[1]].push_back(it[0]);
-            indegrees[it[0]]++;
+            int v = it[0];
+            int u = it[1];
+            indegree[v]++;
+            graph[u].push_back(v);
         }
 
         queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegrees[i] == 0) q.push(i);
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.push(i); 
         }
 
+        int count = 0;
         while(!q.empty()){
-            n--;
-            int node = q.front(); q.pop();
-            for(auto adjNode: graph[node]){
-                if(--indegrees[adjNode] == 0){
-                    q.push(adjNode);
+            count++;
+            int v = q.front(); q.pop();
+            for(auto adjn: graph[v]){
+                indegree[adjn]--;
+                if(indegree[adjn] == 0){
+                    q.push(adjn);
                 }
             }
         }
 
-        return n==0;
+        if(count == numCourses) return true;
+        else return false;
     }
 };
