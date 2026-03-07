5ae45eb560da16859be9190ac230b281b1b84e87
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-07 14:30:01 -0500

    Time: 1 ms (81.24%), Space: 13.9 MB (94.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,42 +1,30 @@
+/*
+      0 1 2 3 4 5   6  7
+    [10,9,2,5,3,7,101,18]
+
+    10x, 9x, 2, 3, 7, 18,    
+*/
+
 class Solution {
 public:
-/*
-        [10,9,2,5,3,7,101,18]
-        [2,3,7,101]
-
-        func(i, prev_i)
-        take = func(i+1, i);
-        notTake = func(i+1, prev_i);
-*/
-    int func(int i, int prev_i, vector<int>& nums, vector<vector<int>>& dp){
-        if(i >= nums.size()) return 0;
-        if(dp[i][prev_i+1] != -1) return dp[i][prev_i+1];
-
-        int take = 0, notTake = 0;
-        if(prev_i == -1 || nums[i] > nums[prev_i]){
-            take = 1+func(i+1, i, nums, dp);
-        }
-        notTake = func(i+1, prev_i, nums, dp);
-        return dp[i][prev_i+1] = max(take, notTake);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int i = n; i >= 0; i--){
-            for(int prev_i = i-1; prev_i >= -1; prev_i--){
-                if(i == n){
-                    dp[i][prev_i+1] = 0;
-                    continue;
-                }
-                int take = 0, notTake = 0;
-                if(prev_i == -1 || nums[i] > nums[prev_i]){
-                    take = 1+dp[i+1][i+1];
-                }
-                notTake = dp[i+1][prev_i+1];
-                dp[i][prev_i+1] = max(take, notTake);
+        int maxSize = 0;
+        vector<int> ans; 
+        ans.push_back(nums[0]);
+        maxSize = max(maxSize, (int)ans.size());
+        for(int i = 1; i < n; i++){
+            if(ans.back() >= nums[i]){
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i];
             }
+            else{
+                ans.push_back(nums[i]);
+            }
+
+            maxSize = max(maxSize, (int)ans.size());
         }
-        //func(0, -1, nums, dp);
-        return dp[0][-1+1];
+
+        return maxSize;
     }
 };
