284971913870f78d9dbb1e725bb8fc6fde14417c
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-12 01:10:48 -0400

    Time: 5 ms (34.1%), Space: 11.6 MB (48.76%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -5,8 +5,11 @@
 */
 class Solution {
 public:
+    //TC: O(N+N+N+N+N) = O(5N)
     string removeKdigits(string num, int k) {
         stack<char> stack;
+
+        //TC: O(N)
         for(char c: num){
             if(stack.empty()){
                 stack.push(c);
@@ -19,18 +22,23 @@ public:
             stack.push(c);
         }
 
+        //TC:O(N)
         while(!stack.empty() && k > 0){
             stack.pop(); k--;
         }
 
         string newNum = "";
+
+        //TC: O(N)
         while(!stack.empty()){
             newNum.push_back(stack.top());
             stack.pop();
         }
 
+        //TC: O(N)
         while(!newNum.empty() && newNum.back() == '0') newNum.pop_back();
 
+        //TC: O(N)
         reverse(newNum.begin(), newNum.end());
 
         if(newNum.empty()) return "0";
