6bf95f79cb39d30a5538e65eb4699a62b1e10f70
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-12 02:02:45 -0400

    Time: 38 ms (74.19%), Space: 85 MB (34.81%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,7 +5,7 @@ public:
         int left = 0, maxLen = 0;
         for(int right = 0; right < fruits.size(); right++){
             map[fruits[right]]++;
-            while(left <= right && map.size() > 2){
+            if(left <= right && map.size() > 2){
                 map[fruits[left]]--;
                 if(map[fruits[left]] == 0){
                     map.erase(fruits[left]);
