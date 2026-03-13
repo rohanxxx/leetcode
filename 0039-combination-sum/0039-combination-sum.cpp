5b0d6774b5c59e6c9dda4f61d8547af254c85aaf
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-13 02:13:31 -0400

    Time: 19 ms (17.6%), Space: 19.7 MB (18.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,44 @@
 class Solution {
-public:
-    void backtrack(int index, set<vector<int>>& ans, vector<int> temp, int target, vector<int>& candidates){
-        int n = candidates.size();
-        if(target == 0){
-            ans.insert(temp);
+    /*
+        array with distinct input of integers 
+        and we have a target integer
+
+        from the arr return a list of unique combinations of candidates where the chosen number
+        sum to the target.
+
+        so the same number can be picked multiple times
+        if(sum > target){
             return;
         }
-        if(target < 0) return;
-        for(int i = index; i < n; i++){
-            temp.push_back(candidates[i]);
-            backtrack(i, ans, temp, target-candidates[i], candidates);
+        if(sum == target){
+            ans.push_back(temp);
+        }
+        for(int i = 0; i < n; i++){
+            temp.push_back(nums[i]);
+            backtrack(sum+nums[i], target, candidates);
             temp.pop_back();
+        }
+    */
+public:
+    void backtrack(int index, int target, vector<int> cur, vector<vector<int>>& ans, vector<int>& cand){
+        if(target == 0){
+            ans.push_back(cur);
+            return;
+        }
+
+        if(target < 0){
+            return;
+        }
+
+        for(int i = index; i < cand.size(); i++){
+            cur.push_back(cand[i]);
+            backtrack(i, target-cand[i], cur, ans, cand);
+            cur.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
-        set<vector<int>> ans;
-        backtrack(0, ans, temp, target, candidates);
-
-        return vector<vector<int>>(ans.begin(), ans.end());
+        vector<vector<int>> ans;
+        backtrack(0, target, {}, ans, candidates);
+        return ans;
     }
 };
