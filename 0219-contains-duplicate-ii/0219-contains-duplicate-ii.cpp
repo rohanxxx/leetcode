486b5cc2d93e14c91dfe4481970fdb7007f59d8a
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-13 02:02:47 -0400

    Time: 71 ms (76.13%), Space: 98.7 MB (50.17%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -16,8 +16,9 @@ public:
         //key = nums[i], val = i
         unordered_map<int, int> mp;
         for(int i = 0; i < n; i++){
-            if(mp.find(nums[i]) != mp.end() && abs(mp[nums[i]]-i) <= k){
-                return true;
+            if(mp.find(nums[i]) != mp.end()){
+                if(abs(mp[nums[i]]-i) <= k) return true;
+                else mp.erase(nums[i]);
             }
             mp[nums[i]] = i;
         }
