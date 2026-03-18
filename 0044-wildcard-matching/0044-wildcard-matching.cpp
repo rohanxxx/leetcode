2016dd5007805cb5b99d11e9a88be0a3c7198188
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-18 01:46:45 -0400

    Time: 60 ms (34.75%), Space: 54.7 MB (15.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
-    bool dfs(int i, int j, string& s, string& p, vector<vector<int>>& dp){
-        if(i == 0 && j == 0) return true;
-        if(i > 0  && j == 0) return false;
-        if(i == 0 && j > 0){
-            while(j){
-                if(p[j-1] != '*') return false;
-                j--;
-            }
-            return true;
-        }
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(p[j-1] == '*'){
-            return dp[i][j] = dfs(i, j-1, s, p, dp) || dfs(i-1, j, s, p, dp);
-        }
-        if(s[i-1] == p[j-1] || p[j-1] == '?'){
-            return dp[i][j] = dfs(i-1, j-1, s, p, dp);
-        } 
-
-        return dp[i][j] = false;
-    }
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
