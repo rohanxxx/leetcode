91fcfdc0419f8c98c8f8e5ba060945d24df8cb46
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-19 00:18:49 -0400

    Time: 0 ms (100%), Space: 14.4 MB (58.32%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,54 +1,34 @@
-/*
-Input: board = 
-[
-    ["X","X","X","X"],
-    ["X","O","O","X"],
-    ["X","X","O","X"],
-    ["X","O","X","X"]
-]
-
-Output: 
-[
-    ["X","X","X","X"],
-    ["X","X","X","X"],
-    ["X","X","X","X"],
-    ["X","O","X","X"]
-]
-*/
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-
-        queue<vector<int>> q;
-        //O(N*M)
+        queue<pair<int, int>> q;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(board[i][j] == 'O' && (i == 0 || j == 0 || i == n-1 || j == m-1)){
-                    q.push({i,j});
-                    board[i][j] = 'E';
+                if(board[i][j] == 'O'){
+                    if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                        board[i][j] = 'E';
+                        q.push({i,j});
+                    }
                 }
             }
         }
 
-        vector<vector<int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        //now mark region that connects to the edges "E"
-
-        //TC: O(4*N*M)
+        vector<pair<int, int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.empty()){
-            auto it = q.front(); q.pop();
-            int r = it[0];
-            int c = it[1];
+            int r = q.front().first;
+            int c = q.front().second;
 
-            //TC: O(4)
-            for(auto d: dir){
-                int adjr = d[0] + r;
-                int adjc = d[1] + c;
+            q.pop();
+
+            for(auto offset: dir){
+                int adjr = offset.first + r;
+                int adjc = offset.second + c;
                 if(adjr >= 0 && adjr < n && adjc >= 0 && adjc < m){
                     if(board[adjr][adjc] == 'O'){
-                        q.push({adjr, adjc});
                         board[adjr][adjc] = 'E';
+                        q.push({adjr, adjc});
                     }
                 }
             }
@@ -61,7 +41,6 @@ public:
                 }
             }
         }
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'E'){
