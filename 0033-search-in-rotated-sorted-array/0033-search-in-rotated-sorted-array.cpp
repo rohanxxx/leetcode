6fc74ba26dc4d243b8e940d173f9e929f110d1fd
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-27 12:44:41 -0400

    Time: 0 ms (100%), Space: 15.1 MB (67.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,15 +3,29 @@
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
 
-if(target <= nums[right]){
-    mid = left + (right-left/2)
-    if(nums[mid] == target){
-        return index;
+while(l < r){
+    int mid = left + (right-left)/2;
+    if(nums[mid] == target) return mid;
+    
+    //left sorted porion
+    if(nums[l] <= nums[mid]){
+        if(target > nums[mid] || target < nums[l]){
+            l = mid+1;
+        }
+        else{
+            r = mid-1;
+        }
     }
-    else if(nums[mid] > target)
-}
-else{
+    else{ //right sorted array
+        if(target < nums[mid] || target > nums[r]){
+            r = mid-1;
+        }
+        else{
+            l = mid+1;
+        }
+    }
 
+    return -1;
 }
 */
 class Solution {
@@ -19,29 +33,29 @@ public:
     int search(vector<int>& nums, int target) {
         int l = 0, r = (int)nums.size() - 1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // left sorted portion
-            if (nums[l] <= nums[mid]) {
-                if (target > nums[mid] || target < nums[l]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+        while(l <= r){
+        int mid = l + (r-l)/2;
+        if(nums[mid] == target) return mid;
+        
+        //left sorted porion
+        if(nums[l] <= nums[mid]){
+            if(target > nums[mid] || target < nums[l]){
+                l = mid+1;
             }
-            // right sorted portion
-            else {
-                if (target < nums[mid] || target > nums[r]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+            else{
+                r = mid-1;
             }
+        }
+        else{ //right sorted array
+            if(target < nums[mid] || target > nums[r]){
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
         }
 
         return -1;
-    }
+        }
 };
