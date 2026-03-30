48eb32983754c677689bd70bf74cd780b2fd586b
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-30 07:16:53 -0400

    Time: 34 ms (48.96%), Space: 69 MB (5.59%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -5,17 +5,21 @@
 */
 class Solution {
 public:
+    //TC: O(N+(Max-Min)) = O(N+M)
+    //SC: O(N)
     int findKthLargest(vector<int>& nums, int k) {
         int maxElement = INT_MIN;
         int minElement = INT_MAX;
+        //SC: O(N)
         unordered_map<int, int> freq;
+        //TC: O(N)
         for(auto num: nums){
             freq[num]++;
             maxElement = max(maxElement, num);
             minElement = min(minElement, num);
         }
 
-        
+        //TC: O(MAX-MIN)
         for(int i = maxElement; i >= minElement; i--){
             if(freq.find(i) != freq.end()){
                 k -= freq[i];
