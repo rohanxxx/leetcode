c11b3f3cb563e744ea8ad4ad64dcd3881e9469ad
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-03-31 13:04:35 -0400

    Time: 14 ms (25.25%), Space: 19.1 MB (64.91%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    const int EMPTY = INT_MAX;
-    const int GATE = 0;
-    const vector<vector<int>> DIRECTIONS = {
-        {1, 0},  // Down
-        {-1, 0}, // Up
-        {0, 1},  // Right
-        {0, -1}  // Left
-    };
-
     void wallsAndGates(vector<vector<int>>& rooms) {
-        int m = rooms.size();
-        if (m == 0) return;
-        int n = rooms[0].size();
-    
+        int n = rooms.size();
+        int m = rooms[0].size();
         queue<pair<int, int>> q;
-    
-        // Enqueue all gates
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (rooms[row][col] == GATE) {
-                    q.push({row, col});
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(rooms[i][j] == 0){
+                    q.push({i,j});
                 }
             }
         }
-    
-        // Perform BFS from each gate
-        while (!q.empty()) {
-            pair<int, int> point = q.front(); q.pop();
-            int row = point.first;
-            int col = point.second;
-        
-            for (const vector<int>& direction : DIRECTIONS) {
-                int r = row + direction[0];
-                int c = col + direction[1];
-            
-                // Check if the neighboring cell is within bounds and is empty
-                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
-                    continue;
+
+        vector<pair<int, int>> dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(auto it: dir){
+                int adjr = it.first+r;
+                int adjc = it.second+c;
+                if(adjr >= 0 && adjr < n && adjc >= 0 && adjc < m){
+                    if(rooms[adjr][adjc] == -1){
+                        continue;
+                    }
+                    if(rooms[r][c]+1 < rooms[adjr][adjc]){
+                        q.push({adjr, adjc});
+                        rooms[adjr][adjc] = rooms[r][c]+1;
+                    }
                 }
-            
-                // Update the distance to the nearest gate
-                rooms[r][c] = rooms[row][col] + 1;
-            
-                // Add the updated cell to the queue
-                q.push({r, c});
             }
         }
     }
