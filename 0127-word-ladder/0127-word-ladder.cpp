36c9bdf27996c9add546676fe1c337df12fd9528
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-01 06:42:38 -0400

    Time: 51 ms (76.96%), Space: 19.6 MB (92.53%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-/*
-    beginWord<->EndWord
-    beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
-*/
+    //TC: O(N+N*K*log(N))
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> set(wordList.begin(), wordList.end());
+        //TC: O(N)
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> q; q.push(beginWord);
         
         int count = 0;
-        queue<string> q; q.push(beginWord);
-        //O(logN)
-        set.erase(beginWord);
         while(!q.empty()){
             count++;
             int size = q.size();
+            //TC: O(26*N*K*Log(N))
             for(int i = 0; i < size; i++){
-                string node = q.front(); q.pop();
-                if(node == endWord) return count;
-                //O(N*k*26*logN)
-                for(int i = 0; i < node.size(); i++){
-                    char original = node[i];
-                    //O(26)
+                string word = q.front(); q.pop();
+                if(word == endWord){
+                    return count;
+                }
+                //TC: O(K)
+                for(int j = 0; j < word.size(); j++){
+                    char curChar = word[j];
                     for(char c = 'a'; c <= 'z'; c++){
-                        node[i] = c;
-                        if(set.find(node) != set.end()){
-                            q.push(node);
-                            //O(logN)
-                            set.erase(node);
+                        word[j] = c;
+                        //if found
+                        //TC: Log(N)
+                        if(dict.find(word) != dict.end()){
+                            dict.erase(word);
+                            q.push(word);
                         }
                     }
-                    node[i] = original;
+                    word[j] = curChar;
                 }
             }
         }
+        
         return 0;
     }
 };
