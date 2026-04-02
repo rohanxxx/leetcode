b7697c61f843c01fd11ca94813807c19091f5e64
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 16:39:39 -0400

    Time: 0 ms (100%), Space: 8.7 MB (17.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dfs(int n, vector<int>& dp){
+    /*int dfs(int n, vector<int>& dp){
         if(n < 0){
             return 0;
         }
@@ -11,9 +11,18 @@ public:
             return dp[n];
         }
         return dp[n] = dfs(n-1, dp) + dfs(n-2, dp);
-    }
+    }*/
     int climbStairs(int n) {
-        vector<int> dp(n+1, INT_MIN);
-        return dfs(n, dp);
+        vector<int> dp(n+1, 0);
+        //return dfs(n, dp);
+        dp[0] = 1;
+        for(int i = 0; i <= n; i++){
+            if(i == 0){
+                continue;
+            }
+            if(i-1 >= 0) dp[i] += dp[i-1];
+            if(i-2 >= 0) dp[i] += dp[i-2];
+        }
+        return dp[n];
     }
 };
