9850e5a45b3419b2b0b1f7c3f0cf1f1056808c92
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 16:34:26 -0400

    Time: 0 ms (100%), Space: 8.7 MB (17.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-/*
-    we can only do 1 or 2 steps right?
-    if n == 3
-                    1?2
-                1       2
-            1   2       1   2
-*/
-    int func(int n, vector<int>& dp){
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        if(n <= 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int take1 = func(n-1, dp);
-        int take2 = func(n-2, dp);
-        return dp[n] = take1+take2;
+    int dfs(int n, vector<int>& dp){
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        if(dp[n] != INT_MIN){
+            return dp[n];
+        }
+        return dp[n] = dfs(n-1, dp) + dfs(n-2, dp);
     }
     int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        vector<int> dp(n+1, 0);
-        int prev1 = 1;
-        int prev2 = 2;
-        for(int i = 3; i <= n; i++){
-            int take1 = prev1;
-            int take2 = prev2;
-            prev1 = prev2;
-            prev2 = take1+take2;
-        }
-        return prev2;
+        vector<int> dp(n+1, INT_MIN);
+        return dfs(n, dp);
     }
 };
