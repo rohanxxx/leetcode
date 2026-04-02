101667b006d9127ad9ed0d7b1d27db8698255072
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 16:42:26 -0400

    Time: 0 ms (100%), Space: 8.7 MB (17.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,14 +15,15 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1, 0);
         //return dfs(n, dp);
-        dp[0] = 1;
+        int prev1 = 1;
+        int prev2 = 0;
         for(int i = 0; i <= n; i++){
-            if(i == 0){
-                continue;
-            }
-            if(i-1 >= 0) dp[i] += dp[i-1];
-            if(i-2 >= 0) dp[i] += dp[i-2];
+            int cur = 0;
+            if(i == 0) continue;
+            cur = prev1+prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[n];
+        return prev1;
     }
 };
