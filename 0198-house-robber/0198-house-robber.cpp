9025f983ca8322551df9934c648a2dba6d2f7867
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 23:40:41 -0400

    Time: 0 ms (100%), Space: 10.6 MB (43.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,46 +1,24 @@
-/*
-            0 1 2 3
-    nums = [1,2,3,1]
-*/
 class Solution {
 public:
-    /*
     int dfs(int i, vector<int>& nums, vector<int>& dp){
-        if(i < 0) return 0;
-        if(dp[i] != INT_MIN) return dp[i];
+        if(i < 0){
+            return 0;
+        }
+        if(dp[i] != INT_MIN){
+            return dp[i];
+        }
+        
+        int take = 0;
+        int notTake = 0;
 
-        int take = nums[i] + dfs(i-2, nums, dp);
-        int notTake = dfs(i-1, nums, dp);
+        take = nums[i] + dfs(i-2, nums, dp);
+        notTake = dfs(i-1, nums, dp);
 
         return dp[i] = max(take, notTake);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0);
-        for(int i = 0; i < n; i++) {
-            int take = nums[i];
-            int notTake = 0; 
-            if(i-2 >= 0) take += dp[i-2];
-            if(i-1 >= 0) notTake += dp[i-1];
-            dp[i] = max(take, notTake);
-        }
+        vector<int> dp(n, INT_MIN);
         return dfs(n-1, nums, dp);
-    }*/
-
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        //vector<int> dp(n, 0);
-        int p1 = 0, p2 = 0;
-        for(int i = 0; i < n; i++) {
-            int take = nums[i];
-            int notTake = 0; 
-            if(i-2 >= 0) take += p2;
-            if(i-1 >= 0) notTake += p1;
-            int res = max(take, notTake);
-
-            p2 = p1;
-            p1 = res;
-        }
-        return p1;
     }
 };
