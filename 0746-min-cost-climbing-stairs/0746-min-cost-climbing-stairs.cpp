f2d4acc03fbf7d03b7da46cfc0ae786281a3987f
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 23:18:39 -0400

    Time: 0 ms (100%), Space: 17.8 MB (28.51%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dfs(int i, const int& n, const vector<int>& cost, vector<int>& dp){
+        if(i < 0) return 0; //just in case to avoid any sort of bound errors
+        if(i == 0) return cost[0];
+        if(i == 1) return cost[1];
+        if(dp[i] != INT_MAX){
+            return dp[i];
+        }
+        int curCost = 0;
+        if(i < n){
+            curCost = cost[i];
+        }
+        return dp[i] = curCost + min(dfs(i-1, n, cost, dp), dfs(i-2, n, cost, dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, INT_MAX);
+        return dfs(n, n, cost, dp);
+    }
+};
