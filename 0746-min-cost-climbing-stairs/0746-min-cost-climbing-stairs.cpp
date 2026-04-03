7ae0e92ee4f19d86bab86e879192ba1b9fcad34f
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 23:24:18 -0400

    Time: 0 ms (100%), Space: 17.7 MB (28.51%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -14,8 +14,21 @@ public:
         return dp[i] = curCost + min(dfs(i-1, n, cost, dp), dfs(i-2, n, cost, dp));
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n+1, INT_MAX);
-        return dfs(n, n, cost, dp);
+        const int n = cost.size();
+        vector<int> dp(n+1, 0);
+        
+        //if(i < 0) return 0; //just in case to avoid any sort of bound errors
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i <= n; i++){
+            int curCost = 0;
+            if(i < n){
+                curCost = cost[i];
+            }
+
+            dp[i] = curCost + min(dp[i-1], dp[i-2]);
+        }
+        
+        return dp[n];
     }
 };
