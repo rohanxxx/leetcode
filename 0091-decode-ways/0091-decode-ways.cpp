53f2289525e5eb07d11f42756e953e82dc29d8a1
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-03 12:40:21 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int solve(int i, string& s, vector<int>& dp){
-        if(s[i] == '0') return 0;
-        if(i >= s.length()-1) return 1;
-        
-        if(dp[i] != -1) return dp[i];
-
-        int ans = solve(i+1, s, dp);
-        
-        if(stoi(s.substr(i, 2)) <= 26) {
-            ans += solve(i+2, s, dp);
+    int dfs(int i, const int& n, string& s){
+        if(i >= n){
+            return 0;
         }
-    
-        return dp[i] = ans;
+        if(i == n-1){
+            return 1;
+        }
+        int singleDigit = s[i]-'0';
+        int doubleDigit = singleDigit*10+(s[i+1]-'0');
+        
+        int count = 0;
+        if(singleDigit != 0){
+            //for single digit case
+            count += dfs(i+1, n, s);
+        }
+        if(singleDigit != 0 && doubleDigit <= 26){
+            //for double digit case
+            count += dfs(i+2, n, s);
+        }
+        return count;
     }
     int numDecodings(string s) {
-        int n = s.length();
-        vector<int> dp(n, -1);
-        return solve(0, s, dp);
+        int n = (int)s.length();
+        if(s[0] == '0'){
+            return 0;
+        }
+        return 1+dfs(0, n, s);
     }
 };
