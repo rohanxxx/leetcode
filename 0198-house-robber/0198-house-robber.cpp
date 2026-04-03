8cd677ebd3d91c315745e26134cfad5500423ddd
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-02 23:46:31 -0400

    Time: 0 ms (100%), Space: 10.6 MB (60.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dfs(int i, vector<int>& nums, vector<int>& dp){
+    /*int dfs(int i, vector<int>& nums, vector<int>& dp){
         if(i < 0){
             return 0;
         }
@@ -15,10 +15,23 @@ public:
         notTake = dfs(i-1, nums, dp);
 
         return dp[i] = max(take, notTake);
-    }
+    }*/
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, INT_MIN);
-        return dfs(n-1, nums, dp);
+        vector<int> dp(n, 0);
+        for(int i = 0; i < n; i++){
+            int take = nums[i];
+            int notTake = 0;
+
+            if(i-2 >= 0){
+                take = take + dp[i-2];
+            }
+            if(i-1 >= 0){
+                notTake = dp[i-1];
+            }
+
+            dp[i] = max(take, notTake);
+        }
+        return dp[n-1];
     }
 };
