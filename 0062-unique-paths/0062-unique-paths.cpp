96778a28baaa793c245d6b734f30c2f59f2a8539
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-04 06:33:11 -0400

    Time: 0 ms (100%), Space: 8.2 MB (79.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,11 +14,13 @@ public:
         int r2 = dfs(i, j-1, dp);
         return dp[i][j] = r1+r2;
     }
+    //TC: O(M*N)
+    //SC: O(N)
     int uniquePaths(int m, int n) {
-        //vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         vector<int> prev(n+1, 0), curr(n+1, 0);
-        
+
         curr[1] = 1;
+        //TC: O(M*N)
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(i == 1 && j == 1){
