fb134b270c061e0b50e34ec05b4ce055dea77c79
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-04 06:14:28 -0400

    Time: 65 ms (85.43%), Space: 15.6 MB (73.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -7,25 +7,25 @@ public:
         if(sum % 2 == 1) return false;
         
         int target = sum / 2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
-        for(int i = 0; i < n; i++){
-            dp[i][0] = true;
-        }
-        /*if(nums[0] <= target){
-            dp[0][nums[0]] = true;
+        //vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
+        vector<int> prev(target+1, 0), curr(target+1, 0);
+        /*for(int i = 0; i < n; i++){
+            prev[0] = true;
         }*/
-        
+
         for(int i = 1; i < n; i++){
+            prev[0] = true;
             for(int j = 1; j <= target; j++){
-                bool notTake = dp[i-1][j];
+                bool notTake = prev[j];
                 bool take = false;
                 if(nums[i] <= j){
-                    take = dp[i-1][j-nums[i]];
+                    take = prev[j-nums[i]];
                 }
-                dp[i][j] = take || notTake;
+                curr[j] = take || notTake;
             }
+            prev = curr;
         }
 
-        return dp[n-1][target];
+        return prev[target];
     }
 };
