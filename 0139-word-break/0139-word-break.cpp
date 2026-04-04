0c02d288b02ccdef3a5248a9e2a70ed08049ebb5
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-04 00:20:45 -0400

    Time: 0 ms (100%), Space: 10.7 MB (81.75%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -18,7 +18,18 @@ public:
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
-        vector<int> dp(n,-1);
-        return dfs(0, n, s, wordDict, dp);
+        vector<int> dp(n+1,0);
+        dp[n] = 1;//base case
+        for(int i = n-1; i >= 0; i--){
+            bool result = false;
+            for(auto word: wordDict){
+                int k = word.size();
+                if(i+k-1 < n && s.substr(i, k) == word){
+                    result = (result || dp[i+k]);
+                }
+            }
+            dp[i] = result;
+        }
+        return dp[0];
     }
 };
