ea4325c82f8fc55cb24988ccaf35869de3ed28f3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-04 00:20:56 -0400

    Time: 3 ms (72.61%), Space: 10.5 MB (89.9%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    bool dfs(int i, const int& n, string& s, vector<string>& wordDict, vector<int>& dp){
-        if(i == n){
-            return true;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        bool result = false;
-        for(auto word: wordDict){
-            int k = word.size();
-            if(i+k-1 < n && s.substr(i, k) == word){
-                result = (result || dfs(i+k, n, s, wordDict, dp));
-            }
-        }
-        return dp[i] = result;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
         vector<int> dp(n+1,0);
