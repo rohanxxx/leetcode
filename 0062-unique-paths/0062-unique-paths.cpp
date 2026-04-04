371c2916fbc6acd6695797310944565e4908565d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-04 06:24:34 -0400

    Time: 1 ms (25.06%), Space: 9.3 MB (64.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,50 +1,21 @@
-/*
-    robot starting location -> top-left
-    goal location -> bottom right
-
-    moves -> down or right 2 moves
-    2 integers given 
-
-    return the possible unique paths that the robot can take to reach the goal state
-
-    1 1 1 1   1  1  1
-    1 2 3 4   5  6  7
-    1 3 6 10 15 21 28
-
-    
-*/
 class Solution {
 public:
-    int dfs(int r, int c, vector<vector<int>>& dp){
-        if(r == 0 || c == 0) return 0;
-        if(r == 1 && c == 1) return 1;
-
-        if(dp[r][c] != 0) return dp[r][c];
-
-        int left = dfs(r-1, c, dp);
-        int up = dfs(r, c-1, dp);
-
-        return dp[r][c] = left+up;
+    int dfs(int i, int j, vector<vector<int>>& dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int r1 = dfs(i-1, j, dp);
+        int r2 = dfs(i, j-1, dp);
+        return dp[i][j] = r1+r2;
     }
     int uniquePaths(int m, int n) {
-        //vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        //return dfs(m, n, dp);
-
-        vector<int> prev(n+1, 0);
-        for(int r = 1; r <= m; r++){
-            vector<int> cur(n+1, 0);
-            for(int c = 1; c <= n; c++){
-                if(r == 1 && c == 1){
-                    cur[c] = 1;
-                    continue;
-                }
-                int left = prev[c];
-                int up = cur[c-1];
-                cur[c] = left + up;
-            }
-            prev = cur;
-        }
-
-        return prev[n];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return dfs(m-1, n-1, dp);
     }
 };
