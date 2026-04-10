24ab9bee65a4701977648ed8cfa919f2c27f3758
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-09 21:56:44 -0400

    Time: 0 ms (100%), Space: 8.3 MB (90.54%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -5,38 +5,38 @@ public:
         for (char c : s) {
             charCounts[c - 'a']++;
         }
-
-        // Max heap ordered by character counts
-        priority_queue<vector<int>> pq;
-        for (int i = 0; i < 26; i++) {
-            if (charCounts[i] > 0) {
-                pq.push(vector<int>{charCounts[i], i + 'a'});
+        int maxCount = 0, letter = 0;
+        for (int i = 0; i < charCounts.size(); i++) {
+            if (charCounts[i] > maxCount) {
+                maxCount = charCounts[i];
+                letter = i;
             }
         }
-        
-        string result;
-        while (!pq.empty()) {
-            auto first = pq.top();
-            pq.pop();
-            if (result.empty() || first[1] != result.back()) {
-                result += char(first[1]);
-                if (--first[0] > 0) {
-                    pq.push(first);
+        if (maxCount > (s.length() + 1) / 2) {
+            return "";
+        }
+        string ans = s;
+        int index = 0;
+
+        // Place the most frequent letter
+        while (charCounts[letter] != 0) {
+            ans[index] = char(letter + 'a');
+            index += 2;
+            charCounts[letter]--;
+        }
+
+        // Place rest of the letters in any order
+        for (int i = 0; i < charCounts.size(); i++) {
+            while (charCounts[i] > 0) {
+                if (index >= s.length()) {
+                    index = 1;
                 }
-            } else {
-                if (pq.empty()) {
-                    return "";
-                }
-                auto second = pq.top();
-                pq.pop();
-                result += char(second[1]);
-                if (--second[0] > 0) {
-                    pq.push(second);
-                }
-                pq.push(first);
+                ans[index] = char(i + 'a');
+                index += 2;
+                charCounts[i]--;
             }
         }
 
-        return result;
+        return ans;
     }
 };
