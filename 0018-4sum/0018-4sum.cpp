7fed7c6b3ebcf842ca27fc267a2ae289a585c918
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-09 21:37:25 -0400

    Time: 23 ms (54.94%), Space: 17.3 MB (82.91%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -10,7 +10,8 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> set;
+        //set<vector<int>> set;
+        vector<vector<int>> ans;
 
         //TC: O(NlogN)
         //SC: O(N)
@@ -19,13 +20,24 @@ public:
         //TC: O(N^2)
         //SC: O(N)
         for(int i = 0; i < n; i++){
+            if(i-1 >= 0 && nums[i-1] == nums[i]){
+                continue;
+            }
             for(int j = i+1; j < n; j++){
+                if(j-1 > i && nums[j-1] == nums[j]){
+                    continue;
+                }
                 int lo = j+1; 
                 int hi = n-1;
                 while(lo < hi){
                     long long sum = static_cast<long long> (nums[i]) + nums[j] + nums[lo] + nums[hi];
                     if(sum == target){
-                        set.insert({nums[i], nums[j], nums[lo++], nums[hi]});
+                        //set.insert({nums[i], nums[j], nums[lo++], nums[hi]});
+                        ans.push_back({nums[i], nums[j], nums[lo], nums[hi]});
+                        while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        lo++;
+                        hi--;
                     }
                     else if(sum < target){
                         lo++;
@@ -37,6 +49,7 @@ public:
             }
         }
 
-        return vector<vector<int>> (set.begin(), set.end());
+        //return vector<vector<int>> (set.begin(), set.end());
+        return ans;
     }
 };
