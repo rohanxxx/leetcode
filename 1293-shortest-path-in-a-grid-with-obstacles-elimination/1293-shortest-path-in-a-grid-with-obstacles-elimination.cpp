ee9d2b8a30fa5f35a0ca15e210d4255c4ea4eb02
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-17 08:50:46 -0400

    Time: 208 ms (20.02%), Space: 62.6 MB (21.27%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        unordered_set<string> visitedState;
         int n = grid.size(), m = grid[0].size();
-        
+
+        vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(m, vector<bool>(k+1, false)));
+
         queue<vector<int>> q;
+
         if(grid[0][0] == 0){
             q.push({0,0,0,k});
-            string key = string(to_string(0)+"#"+to_string(0)+"#"+to_string(k));
-            visitedState.insert(key);
+            visited[0][0][k] = true;
         }
-        if(grid[0][0] == 1 && k > 0){
+        else if(grid[0][0] == 1 && k > 0){
             q.push({0,0,0,k-1});
-            string key = string(to_string(0)+"#"+to_string(0)+"#"+to_string(k-1));
-            visitedState.insert(key);
+            visited[0][0][k-1] = true;
         }
 
-        vector<pair<int, int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
 
         while(!q.empty()){
             auto it = q.front(); q.pop();
+
             int r = it[0];
             int c = it[1];
             int step = it[2];
@@ -32,24 +33,24 @@ public:
             for(auto d: dir){
                 int adjr = r + d.first;
                 int adjc = c + d.second;
+
                 if(adjr < 0 || adjr >= n || adjc < 0 || adjc >= m){
                     continue;
                 }
-                //if blocked
+
+                // blocked
                 if(grid[adjr][adjc] == 1 && currK > 0){
-                    string key = string(to_string(adjr)+"#"+to_string(adjc)+"#"+to_string(currK-1));
-                    //if not found
-                    if(visitedState.find(key) == visitedState.end()){
+                    if(!visited[adjr][adjc][currK-1]){
                         q.push({adjr, adjc, step+1, currK-1});
-                        visitedState.insert(key);
+                        visited[adjr][adjc][currK-1] = true;
                     }
                 }
-                //if not blocked
+
+                // free
                 if(grid[adjr][adjc] == 0){
-                    string key = string(to_string(adjr)+"#"+to_string(adjc)+"#"+to_string(currK));
-                    if(visitedState.find(key) == visitedState.end()){
+                    if(!visited[adjr][adjc][currK]){
                         q.push({adjr, adjc, step+1, currK});
-                        visitedState.insert(key);
+                        visited[adjr][adjc][currK] = true;
                     }
                 }
             }
