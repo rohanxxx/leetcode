238c5430dd2c4a8ccaf085cc7484a958610abe0a
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-19 07:45:47 -0400

    Time: 3 ms (94.12%), Space: 10 MB (88.24%) - LeetHub

---
+++ 0294-flip-game-ii/0294-flip-game-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    unordered_map<string, bool> memo;
+
+    bool dfs(int turn, int& n, string& state){
+        // memo check
+        if(memo.count(state)) return memo[state];
+
+        for(int i = 0; i < n; i++){
+            if(i + 1 < n && state[i] == '+' && state[i+1] == '+'){
+                //hasMove = true;
+
+                // make move
+                state[i] = '-';
+                state[i+1] = '-';
+
+                // opponent turn
+                bool nextPlayer = dfs(turn + 1, n, state);
+
+                // backtrack
+                state[i] = '+';
+                state[i+1] = '+';
+
+                // if opponent loses → win
+                if(nextPlayer == false){
+                    return memo[state] = true;
+                }
+            }
+        }
+
+        // no moves → lose
+        return memo[state] = false;
+    }
+
+    bool canWin(string currentState) {
+        int n = currentState.length();
+        return dfs(0, n, currentState);
+    }
+};
