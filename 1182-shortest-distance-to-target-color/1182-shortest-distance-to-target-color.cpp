8f468f92fab3dc9bfb8437cfb0b57645f99deecd
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-18 21:05:54 -0400

    Time: 15 ms (96.77%), Space: 128.7 MB (48.39%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -15,49 +15,46 @@
 class Solution {
 public:
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        vector<vector<int>> map(4);
-
         int n = colors.size();
+
+        vector<int> rightmost = {0, 0, 0};
+        vector<int> leftmost = {n - 1, n - 1, n - 1};
+
+        vector<vector<int>> distance(3, vector<int>(n, -1));
+
+        // looking forward
         for(int i = 0; i < n; i++){
-            map[colors[i]].push_back(i);
+            int color = colors[i] - 1;
+
+            for(int j = rightmost[color]; j < i + 1; j++){
+                distance[color][j] = i - j;
+            }
+
+            rightmost[color] = i + 1;
         }
 
-        vector<int> ans;
+        // looking backward
+        for(int i = n - 1; i >= 0; i--){
+            int color = colors[i] - 1;
 
-        for(auto q: queries){
-            int i = q[0];
-            int target = q[1];
-            
-            if(map[target].empty()){
-                ans.push_back(-1);
-                continue;
+            for(int j = leftmost[color]; j > i - 1; j--){
+                if(distance[color][j] == -1 || distance[color][j] > j - i){
+                    distance[color][j] = j - i;
+                }
             }
 
-            auto it = lower_bound(map[target].begin(), map[target].end(), i);
-            int index = it - map[target].begin();
-
-            int best = INT_MAX;
-
-            // "end" case (keep continue)
-            if(index == map[target].size()){
-                int j = map[target][index-1];
-                best = min(best, abs(j - i));
-                ans.push_back(best);
-                continue;
-            }
-
-            // normal case
-            int j = map[target][index];
-            best = min(best, abs(j - i));
-
-            if(index - 1 >= 0){
-                int j = map[target][index - 1];
-                best = min(best, abs(j - i));
-            }
-
-            ans.push_back(best);
+            leftmost[color] = i - 1;
         }
 
-        return ans;
+        vector<int> queryResults;
+
+        for(int i = 0; i < queries.size(); i++){
+            int idx = queries[i][0];
+            int color = queries[i][1] - 1;
+
+            queryResults.push_back(distance[color][idx]);
+        }
+
+        return queryResults;
     }
 };
