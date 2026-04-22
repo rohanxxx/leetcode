04ac315ff6870f5fb77e8639f3833313199eb2c3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-22 10:24:10 -0400

    Time: 2 ms (57.13%), Space: 19.8 MB (20.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
+    //TC: O(4N+K) => O(N+K)
+    //SC: O(N+N) => O(N)
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        //max freq of an element would be n
+        vector<vector<int>> bucket(n+1);
+
         unordered_map<int, int> freq;
-        for(int num: nums) freq[num]++;
-        vector<vector<int>> bucket(nums.size()+1);
+        //TC: O(N)
+        for(int num: nums){
+            freq[num]++;
+        }
+
+        //fill the bucked according the elements freq level
+        //TC: O(N)
         for(auto it: freq){
             bucket[it.second].push_back(it.first);
         }
 
         vector<int> ans;
-        for(int i = nums.size(); i >= 0; i--){
-            if(bucket[i].size()){
+        //TC: O(N+K)
+        for(int i = n; i >= 0; i--){
+            if(k-bucket[i].size() >= 0){
                 for(auto it: bucket[i]){
                     ans.push_back(it);
-                    if(ans.size()>k){
-                        ans.pop_back();
-                        return ans;
-                    }
                 }
+                k-= bucket[i].size();
+            }
+            if(k <= 0){
+                break;
             }
         }
 
         return ans;
+        
     }
 };
