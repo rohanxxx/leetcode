6d24fddb5f72d4fbad7cc50faad1db8678290098
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-22 09:44:07 -0400

    Time: 9 ms (14.16%), Space: 12.1 MB (18.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,46 +1,103 @@
+/*
+      0 1 2 3
+    0 . Q . .
+    1 . . . Q
+    2 Q . . .
+    3 . . Q .
+    
+    rowVec formula => r
+    colVec formula => c
+    antiDag formula => r+c
+    dag formula => abs(n-1-r)+c
+*/
 class Solution {
 public:
-    int size;
-    vector<vector<string>> solutions;
+    bool check(int r, int c,
+               unordered_set<int>& rowVec,
+               unordered_set<int>& colVec,
+               unordered_set<int>& dagVec,
+               unordered_set<int>& antiDagVec,
+               int n) {
 
-    vector<string> createBoard(vector<vector<char>> state){
-        vector<string> board;
-        for(int row = 0; row < size; row++){
-            string current_row(state[row].begin(), state[row].end());
-            board.push_back(current_row);
+        if ((rowVec.find(r) != rowVec.end()) ||
+            (colVec.find(c) != colVec.end()) ||
+            (antiDagVec.find(r + c) != antiDagVec.end()) ||
+            (dagVec.find((n - 1 - r) + c) != dagVec.end())) {
+            return true;
         }
-        return board;
+
+        return false;
     }
 
-    void backtrack(int row, unordered_set<int> diagonals, unordered_set<int> antiDiagonals, unordered_set<int> cols, vector<vector<char>> state){
-        if(row == size){
-            solutions.push_back(createBoard(state));
+    void dfs(int r, int& n,
+             unordered_set<int>& rowVec,
+             unordered_set<int>& colVec,
+             unordered_set<int>& dagVec,
+             unordered_set<int>& antiDagVec,
+             vector<string>& currState,
+             vector<vector<string>>& ans) {
+
+        if (r == n) {
+            ans.push_back(currState);
             return;
         }
 
-        for(int col = 0; col < size; col++){
-            int currDiagonal = row - col;
-            int currAntiDiagonal = row + col;
+        for (int c = 0; c < n; c++) {
+            if (check(r, c, rowVec, colVec, dagVec, antiDagVec, n) == true) {
+                continue;
+            }
 
-            if(cols.count(col)||diagonals.count(currDiagonal)||antiDiagonals.count(currAntiDiagonal)) continue;
+            currState[r][c] = 'Q';
+            rowVec.insert(0);
+            colVec.insert(c);
+            dagVec.insert((n - 1 - r) + c);
+            antiDagVec.insert(r + c);
 
-            cols.insert(col);
-            diagonals.insert(currDiagonal);
-            antiDiagonals.insert(currAntiDiagonal);
-            state[row][col] = 'Q';
+            dfs(r + 1, n, rowVec, colVec, dagVec, antiDagVec, currState, ans);
 
-            backtrack(row+1, diagonals, antiDiagonals, cols, state);
-
-            cols.erase(col);
-            diagonals.erase(currDiagonal);
-            antiDiagonals.erase(currAntiDiagonal);
-            state[row][col] = '.';
+            currState[r][c] = '.';
+            rowVec.erase(0);
+            colVec.erase(c);
+            dagVec.erase((n - 1 - r) + c);
+            antiDagVec.erase(r + c);
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        size = n;
-        vector<vector<char>> emptyBoard(size, vector<char>(size, '.'));
-        backtrack(0, unordered_set<int>(), unordered_set<int>(), unordered_set<int>(), emptyBoard);
-        return solutions;
+        vector<vector<string>> ans;
+
+        string dummy = "";
+        for (int i = 0; i < n; i++) {
+            dummy += ".";
+        }
+
+        vector<string> currState(n, dummy);
+
+        unordered_set<int> rowVec, colVec;
+        unordered_set<int> dagVec, antiDagVec;
+
+        int r = 0;
+
+        for (int c = 0; c < n; c++) {
+            if (check(r, c, rowVec, colVec, dagVec, antiDagVec, n) == true) {
+                continue;
+            }
+
+            currState[r][c] = 'Q';
+            rowVec.insert(0);
+            colVec.insert(c);
+            dagVec.insert((n - 1 - r) + c);
+            antiDagVec.insert(r + c);
+
+            dfs(r + 1, n, rowVec, colVec, dagVec, antiDagVec, currState, ans);
+
+            currState[r][c] = '.';
+            rowVec.erase(0);
+            colVec.erase(c);
+            dagVec.erase((n - 1 - r) + c);
+            antiDagVec.erase(r + c);
+        }
+
+        return ans;
     }
 };
