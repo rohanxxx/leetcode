982e9306d0624c4682b2bfc8d33e5afcd5bed12d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-04-27 10:45:07 -0400

    Time: 17 ms (76.82%), Space: 81.2 MB (79.83%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,27 +1,37 @@
+/*
+    heights = [2,1,5,6,2,3]
+     0 1 2 3 4 5
+    [2,1,5,6,2,3]
+    2x 1 5x 6x 2 3
+
+     0 1 2 3
+    [2,1,2]
+     0 1 2 3 4 5
+    [4,2,0,3,2,5]
+    4x 2
+
+    max-> 4
+*/
 class Solution {
-    /*
-        [2,1,5,6,2,3]
-        [0,1,2,3,4,5]
-    */
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        int maxArea = 0;
-
         stack<int> stack;
-        for(int i = 0; i <= n; i++){
-            while(!stack.empty() && (i == n || heights[stack.top()] >= heights[i])){
-                int curHeight = heights[stack.top()]; stack.pop();
+        
+        int maxArea = 0;
+        int n = heights.size();
+        for(int i = 0; i < n+1; i++){
+            while(!stack.empty() && (i == n || heights[stack.top()] > heights[i])){
+                int height = heights[stack.top()]; stack.pop();
+                int currWidth = 0;
                 
-                int curWidth = 0;
-                if(!stack.empty()) curWidth = i - stack.top() - 1;
-                else curWidth = i;
-                
-                int curArea = curHeight * curWidth;
-                maxArea = max(maxArea, curArea);
+                if(!stack.empty()) currWidth = i-stack.top()-1;
+                else currWidth = i;
+        
+                maxArea = max(maxArea, currWidth*height);
             }
             stack.push(i);
-        }  
-        return maxArea; 
+        }
+
+        return maxArea;
     }
 };
