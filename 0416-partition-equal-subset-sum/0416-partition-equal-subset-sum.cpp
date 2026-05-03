fccc1fed6dc44cc6b88e3baa0c9e217028e6fbf8
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-03 09:14:32 -0400

    Time: 85 ms (73.42%), Space: 15.8 MB (70.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -7,30 +7,32 @@ public:
         if(sum % 2 == 1) return false;
         
         int target = sum / 2;
-        vector<vector<int>> dp(n, vector<int>(target+1, 0));
+        //vector<vector<int>> dp(n, vector<int>(target+1, 0));
+        vector<int> prev(target+1, 0), curr(target+1, 0);
         //return func(nums, dp, n-1, target);
         //if(target == 0) return true;
         for(int i = 0; i < n; i++){
-            dp[i][0] = true;
+            prev[0] = true;
         }
         //if(i == 0) return (nums[i] == target);
         //if(dp[i][target] != -1) return dp[i][target];
-        if(nums[0] == target){
-            dp[0][nums[0]] = true;
+        if(nums[0] <= target){
+            prev[nums[0]] = true;
         }
         for(int i = 1; i < n; i++){
             for(int t = 1; t <= target; t++){
-                bool notTake = dp[i-1][t];
+                bool notTake = prev[t];
                 bool take = false;
 
                 if(nums[i] <= t){
-                    take = dp[i-1][t-nums[i]];
+                    take = prev[t-nums[i]];
                 }
 
-                dp[i][t] = take || notTake;
+                curr[t] = take || notTake;
             }
+            prev = curr;
         }
 
-        return dp[n-1][target];
+        return prev[target];
     }
 };
