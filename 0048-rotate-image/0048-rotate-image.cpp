c0a98653e9ea825fed0dc6b889944810718855ca
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-04 11:13:16 -0400

    Time: 0 ms (100%), Space: 10.1 MB (68.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,42 @@
+/*
+      0 1 2
+    0[1,2,3]
+    1[4,5,6]
+    2[7,8,9]
+
+    1st iteration: (0,0) -> {(0,1),(1,0)} -> {(0,2),(2,0)}
+    2nd iteration: (0+1, 0+1) -> {1,1} -> {1,2}{2,1}
+    3rd iteration: (1+1, 1+1)
+
+    7 4 1
+    8 5 2
+    9 6 3
+
+    1 4 7.    1 4 7.   7 4 1
+    2 5 8.    2 5 8 -> 8 5 2
+    3 6 9.    3 6 9.   9 6 3
+*/
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size(), col = matrix[0].size();
-        for(int r = 0; r < row; r++){
-            for(int c = r; c < col; c++){ 
-                swap(matrix[r][c], matrix[c][r]);
+        //TC: O(1)
+        //n*n matrix
+        int n = matrix.size();
+        //TC: O(N*N)
+        //horizontal to vertical
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int tr = j;
+                int tc = i;
+                swap(matrix[i][j], matrix[tr][tc]);
             }
         }
-        for(int r = 0; r < row; r++){
-            reverse(matrix[r].begin(), matrix[r].end());
+
+        //TC: O(N*N)
+        //now we reverse each row
+        for(int i = 0; i < n; i++){
+            //TC: O(N)
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
