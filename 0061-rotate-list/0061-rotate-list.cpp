9e3d83fe5d52f2d622a2ea8bf58760bb0112af9a
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-05 08:30:14 -0400

    Time: 0 ms (100%), Space: 16.4 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,40 +8,77 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- /*
 
-    3 2 1 0
-    1 2 3 4 5 -> 1 2 3 4 5 
-    make it a cycle 
-    n-(k%n)
+ /*                0 1 2 3 4
+    Input: head = [1,2,3,4,5], k = 2
+    Output:       [4,5,1,2,3]
+
+    vector<ListNode*> store
+    vector<ListNode*> newVector
+    (index+k)%n  
+    0+2/5 -> 2
+    3/5 -> 3
+    4/5 -> 4
+    5/5 -> 0
+    
+    5-2->3
+    0 1 2 3
+    1 2 3 4 5 
+    |-------|
+
+    4 as our new head
+
+    newHeadIndex => n-(k%n);
+    
+
  */
 class Solution {
 public:
+    //TC: O(N)
+    //SC: O(1)
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL) return head;
-        
+        if(head == NULL){
+            return NULL;
+        }
+        //TC: O(N)
+        //step#1 make the list circular
         int n = 0;
-        ListNode* temp = head;
         ListNode* prev = NULL;
-        while(temp){
+        ListNode* curr = head;
+        while(curr){
             n++;
-            prev = temp;
-            temp = temp->next;
-        }
-        
-        prev->next = head; //makes it a cycle
-        
-        int newHeadIndex = n-(k%n);
-        temp = head;
-
-        while(newHeadIndex){
-            prev = temp;
-            temp = temp->next;
-            newHeadIndex--;
+            prev = curr;
+            curr = curr->next;
         }
 
-        head = temp;
+        if(n == 1){
+            return head;
+        }
+
+        //edge case
+        if(prev == NULL){
+            return NULL;
+        }
+
+        //otherwise connect 
+        //now the head is circular
+        prev->next = head;
+
+        curr = head;
+        prev = NULL;
+
+        int index = 0;
+        int newHeadIndex = n - (k%n);
+        //TC: O(N)
+        while(index < newHeadIndex && curr){
+            prev = curr;
+            curr = curr->next;
+            index++;
+        }
+
+        //detaches the circular property
         prev->next = NULL;
-        return head;
+
+        return curr;
     }
 };
