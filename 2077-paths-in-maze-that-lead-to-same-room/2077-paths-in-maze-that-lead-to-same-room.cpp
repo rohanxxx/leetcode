bce8ea8aabda03dc37afb782831e0c9ca2887508
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-09 10:56:29 -0400

    Time: 387 ms (38.89%), Space: 55.2 MB (25%) - LeetHub

--- 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.cpp
+++ 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.cpp
@@ -1,24 +1,61 @@
+/*
+    undirected graph
+
+    confused with with the confusion score cycle
+    -> the designer of the maze wants to know how confusing the maze is
+    -> the confusion score of the maze is the number of different cycle of length 3
+    -> two cycles are considered to be different if one or more of the rooms visited in the first cycle
+    is not in the second cycle
+
+    return the confusion score of the maze
+
+    3 - 1 - 2 - 5
+      \ | /
+        4
+
+    1x 3x 4 2 
+
+    count -> 1
+*/
 class Solution {
 public:
     int numberOfPaths(int n, vector<vector<int>>& corridors) {
-        vector<vector<int>> adj(n+1,vector<int>(n+1,0));
-        int result=0;
-        for(int i=0;i<corridors.size();i++) {
-            adj[corridors[i][0]][corridors[i][1]]=1;
-            adj[corridors[i][1]][corridors[i][0]]=1;
+        vector<unordered_set<int>> graph(n + 1);
+
+        for(auto& it : corridors){
+            graph[it[0]].insert(it[1]);
+            graph[it[1]].insert(it[0]);
         }
-        
-        for(int i=1;i<=n;i++) {
-            for(int j=i+1;j<=n;j++) {
-                if(adj[i][j]) {
-                    for(int k=j+1;k<=n;k++) {
-                        if(adj[j][k] && adj[k][i]) {
-                            result++;
-                        }
+
+        int count = 0;
+
+        for(int node = 1; node <= n; node++){
+
+            if(graph[node].size() < 2){
+                continue;
+            }
+
+            for(auto adjNode : graph[node]){
+
+                // enforce ordering
+                if(adjNode <= node){
+                    continue;
+                }
+
+                for(auto adj : graph[adjNode]){
+
+                    if(adj <= adjNode){
+                        continue;
+                    }
+
+                    // triangle exists
+                    if(graph[node].find(adj) != graph[node].end()){
+                        count++;
                     }
                 }
             }
         }
-        return result;
+
+        return count;
     }
 };
