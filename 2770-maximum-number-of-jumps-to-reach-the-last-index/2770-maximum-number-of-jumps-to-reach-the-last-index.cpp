d8d9a5e01819aa89c8f6ae456d0034f19747fb8f
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-10 09:53:44 -0400

    Time: 15 ms (81.06%), Space: 68.6 MB (19.88%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -10,30 +10,32 @@ class Solution {
 public:
     //TC: O(N*N)
     //SC: O(N*N)
-    int dfs(int i, int count, vector<int>& nums, int target, vector<vector<int>>& dp){
+    int dfs(int i, vector<int>& nums, int target, vector<int>& dp){
         int n = nums.size();
         
         if(i == n-1){
-            return count;
+            return 0;
         }
-        if(dp[i][count] != -1){
-            return dp[i][count];
+        if(dp[i] != -1){
+            return dp[i];
         }
         
         int take = INT_MIN;
         for(int j = i+1; j < n; j++){
             if(-target <= nums[j]-nums[i] && nums[j]-nums[i] <= target){
-                take = max(take, dfs(j, count+1, nums, target, dp));
+                int res = dfs(j, nums, target, dp);
+                if(res != INT_MIN){
+                    take = max(take, res+1);
+                }
             }
         }
-        return dp[i][count] = take;
+        return dp[i] = take;
     }
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<int> dp(n+1, -1);
         
-        int res = dfs(0, 0, nums, target, dp);
-        res = dp[0][0];
+        int res = dfs(0, nums, target, dp);
         if(res == INT_MIN){
             return -1;
         }
