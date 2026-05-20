ce795af29726a7799e4b13857f6f97439641b470
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-05-20 10:21:22 -0400

    Time: 0 ms (100%), Space: 10.5 MB (64.28%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-/*
-    void backtrack(vector<int> v, vector<int>&nums){
-        for(i->n){
-            v.push_back(nums[i]);
-            backtrack(v,nums);
-            v.pop_back();
-        }
-    }
-*/
-    void backtracking (set<vector<int>>& subsets, vector<int>& curSubSet,
+    void backtracking (vector<vector<int>>& subsets, vector<int>& curSubSet,
                         vector<int>& nums, int index){
-        subsets.insert(curSubSet);
+        subsets.push_back(curSubSet);
 
         for(int i = index; i < nums.size(); i++){
-            //if(i != index && nums[i] == nums[i-1]) continue;
+            if(i != index && nums[i] == nums[i-1]) continue;
             curSubSet.push_back(nums[i]);
             backtracking(subsets, curSubSet, nums, i+1);
             curSubSet.pop_back();
@@ -22,10 +13,10 @@ public:
 
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> subsets;
+        vector<vector<int>> subsets;
         vector<int> curSubSet;
         sort(nums.begin(), nums.end());
         backtracking(subsets, curSubSet, nums, 0);
-        return vector<vector<int>>(subsets.begin(), subsets.end());
+        return subsets;
     }
 };
