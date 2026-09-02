856835c9f2d880ad3d96f49ff09f4e6754c90dab
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-02 12:22:34 -0400

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,15 @@
+/*
+    odd - odd = even
+    even - odd = odd
+    even - even = even
+
+    approach count the odd and even elements
+
+    if odd % 2 == 0 || even % 2 == 0 || odd: 
+*/
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
