f34d10f8c0698d018a4bc0722ab2c9c095942841
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-03 19:19:50 -0400

    Time: 2247 ms (5.02%), Space: 505 MB (5.02%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,46 +1,42 @@
-/*
-    Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
-    
-    1 1 0
-    2 1 0
-    3 1 2
-    4 3 1
-    5 2 1
-    6 0 2
-    7 0 1
-    8 0 1
-    9 0 2
-    10 2 0 
-
-    hashmap[key, pair<winCount, loseCount>]
-    tc: O(N)
-    sc: O(N)
-*/
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int, pair<int, int>> map;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+
+        //TC: O(N)
+        for(auto it: matches){
+            maxi = max({maxi, it[0], it[1]});
+            mini = min({mini, it[0], it[1]});
+        }
+
+        int n = maxi-mini+1;
+        //SC: O(K+N)
+        vector<vector<int>> bucket(n, vector<int>(2, 0));
+
+        //TC: O(N)
         for(auto it: matches){
             int winner = it[0];
             int loser = it[1];
-            map[winner].first++;
-            map[loser].second++;
+            bucket[winner-mini][0]++;
+            bucket[loser-mini][1]++;
         }
 
         vector<vector<int>> ans(2);
-        for(auto it: map){
-            int player = it.first;
-            pair<int, int> result = it.second;
-            if(result.second == 0){
-                ans[0].push_back(player);
-            }
-            if(result.second == 1){
-                ans[1].push_back(player);
+
+        //TC: O(K)
+        for(int i = 0; i < n; i++){
+            if(bucket[i][0] > 0 || bucket[i][1] > 0){
+                int key = i + mini;
+                if(bucket[i][1] == 0){
+                    ans[0].push_back(key);
+                }
+                if(bucket[i][1] == 1){
+                    ans[1].push_back(key);
+                }
             }
         }
 
-        sort(ans[0].begin(), ans[0].end());
-        sort(ans[1].begin(), ans[1].end());
         return ans;
     }
 };
