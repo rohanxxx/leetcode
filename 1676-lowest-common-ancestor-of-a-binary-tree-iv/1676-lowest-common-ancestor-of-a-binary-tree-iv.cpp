40809aadf9bf32708e6dd64d5cdfca246495ad14
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-22 13:36:54 -0400

    Time: 59 ms (56.3%), Space: 44.2 MB (51.26%) - LeetHub

---
+++ 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ /*
+    
+ */
+
+ /*
+    1) both left and right has lca and set is empty left then return the cur node
+    2) both left and right has lca but the set isn't empty still return the cur node
+    3) if left or right lca case then return the left || right that has lca
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* node, unordered_set<int>& set){
+        if(node == NULL){
+            return NULL;
+        }
+
+        TreeNode* left = dfs(node->left, set);
+        TreeNode* right = dfs(node->right, set);
+        
+        if(set.find(node->val) != set.end()){
+            return node;
+        }
+        if(left && right){
+            return node;
+        }
+        /*if(left == NULL && right == NULL){
+            return NULL;
+        }*/
+        if(left == NULL){
+            return right;
+        }
+        return left;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, vector<TreeNode*> &nodes) {
+        unordered_set<int> set;
+        for(int i = 0; i < nodes.size(); i++){
+            set.insert(nodes[i]->val);
+        }
+        return dfs(root, set);
+    }
+};
