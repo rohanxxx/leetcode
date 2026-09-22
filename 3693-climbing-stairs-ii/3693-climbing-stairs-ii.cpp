18f70924f747a0d6a25cac94080dff447cf874dc
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-22 11:10:06 -0400

    Time: 49 ms (10.86%), Space: 188.6 MB (9.43%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -0,0 +1,26 @@
+/*
+    cost of jump costs[j] + (j-i)^2
+*/
+class Solution {
+public:
+    int dfs(int index, vector<int>& costs, vector<int>& dp){
+        if(index == 0){
+            return 0;
+        }
+        if(dp[index] != INT_MAX){
+            return dp[index];
+        }
+        int res = INT_MAX;
+        for(int i = 1; i <= 3; i++){
+            if(index-i < 0){
+                break;
+            }
+            res = min((costs[index-1] + (i*i)) + dfs(index-i, costs, dp), res);
+        }
+        return dp[index] = res;
+    }
+    int climbStairs(int n, vector<int>& costs) {
+        vector<int> dp(n+1, INT_MAX);
+        return dfs(n, costs, dp);
+    }
+};
