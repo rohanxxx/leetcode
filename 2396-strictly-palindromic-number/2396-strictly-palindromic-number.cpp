9f5a3e3bc086ed3d05bf03010064fe1a83988db3
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-24 19:38:25 -0400

    Time: 0 ms (100%), Space: 7.6 MB (99.76%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    /*bool isPalindrome(string& binary){
+        int left = 0, right = binary.size()-1;
+        while(left <= right){
+            if(binary[left] != binary[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }*/
+    bool isStrictlyPalindromic(int n) {
+        /*string binary = "";
+        while(n){
+            if((n & 1) == 1) binary.push_back('1');
+            else binary.push_back('0');
+
+            n >>= 1;
+        }
+
+        return isPalindrome*/
+        return false;
+    }
+};
