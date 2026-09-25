a52476da0aaa5a8a791d90a404dff4008252004d
rohanxxx <59073429+rohanxxx@users.noreply.github.com>
2026-09-24 22:27:35 -0400

    Time: 4 ms (33.33%), Space: 37.4 MB (8.33%) - LeetHub

---
+++ 3667-sort-array-by-absolute-value/3667-sort-array-by-absolute-value.cpp
@@ -0,0 +1,33 @@
+/*
+    rearrange elements of nums in non-decreasing order their abs value
+
+    return any rearranged array that satisfies this condition.
+
+    Input: nums = [3,-1,-4,1,5]
+    Output: [-1,1,3,-4,5]
+*/
+class Solution {
+public:
+    vector<int> sortByAbsoluteValue(vector<int>& nums) {
+        int mini = INT_MAX, maxi = INT_MIN;
+        for(auto it: nums){
+            mini = min(mini, (int)abs(it));
+            maxi = max(maxi, (int)abs(it));
+        }
+
+        vector<vector<int>> temp(maxi-mini+1);
+        for(int it: nums){
+            int index = (int)abs(it)-mini;
+            temp[index].push_back(it);
+        }
+
+        vector<int> ans;
+        for(vector<int> it: temp){
+            for(auto i: it){
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
